9399a0c2f2f4f64c0ea97ce0ece8d30cba441081
Matthew Ryan <matthew.j.ryan@colorado.edu>
2018-08-22 14:17:43 -0600

    Fixing Eigen in CMake.

--- share/cmake/eigen.cmake
+++ share/cmake/eigen.cmake
@@ -1,29 +1,28 @@
 # -------------------------------------------------------------------------
 # Eigen libraries ---------------------------------------------------------
 # -------------------------------------------------------------------------
-set(EIGEN_ENV_VARS
-    $ENV{EIGENDIR}
-    $ENV{EIGEN_DIR}
-    $ENV{Eigen_DIR}
-    $ENV{Eigen3_DIR}
-    $ENV{EIGEN_ROOT}
-    $ENV{Eigen_ROOT}
-    $ENV{Eigen3_ROOT}
-    $ENV{EIGEN_PATH}
-    $ENV{Eigen_PATH}
-    $ENV{Eigen3_PATH})
+if(NOT EIGEN_FOUND_ONCE)
+    set(EIGEN_ENV_VARS
+        $ENV{EIGENDIR}
+        $ENV{EIGEN_DIR}
+        $ENV{Eigen_DIR}
+        $ENV{Eigen3_DIR}
+        $ENV{EIGEN_ROOT}
+        $ENV{Eigen_ROOT}
+        $ENV{Eigen3_ROOT}
+        $ENV{EIGEN_PATH}
+        $ENV{Eigen_PATH}
+        $ENV{Eigen3_PATH})
 
-find_package(Eigen
-    REQUIRED
-    NAMES Eigen Eigen3
-    HINTS ${EIGEN_ENV_VARS})
+    find_package(Eigen
+        REQUIRED
+        NAMES Eigen3
+        HINTS ${EIGEN_ENV_VARS})
+message(${EIGEN3_INCLUDE_DIRS})
+    message(STATUS "EIGEN_LIBRARIES: ${EIGEN_LIBRARIES}")
+endif()
 
-message(STATUS "EIGEN_LIBRARIES: ${EIGEN_LIBRARIES}")
-
-# list(APPEND MORIS_DEFINITIONS "-DMORIS_HAVE_EIGEN")
-# list(APPEND MORIS_DEFINITIONS "-DMORIS_USE_EIGEN")
-# list(APPEND MORIS_INCDIRS ${EIGEN_DIRS})
 add_definitions("-DMORIS_USE_EIGEN")
-include_directories(/home/maute/tpls/eigen-3.3.3/include)
-
+include_directories(${EIGEN3_INCLUDE_DIRS})
+message(${EIGEN3_INCLUDE_DIRS})
 set(MORIS_ARMADILLO_EIGEN_LIBS "Eigen3::Eigen")
