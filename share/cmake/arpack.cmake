e48f8c69a67aec058d94f852a18dd7857787122a
Matthew Ryan <matthew.j.ryan@colorado.edu>
2018-08-24 10:10:51 -0600

    Separated ARPACK from MORIS_LDLIBS. ARPACK was not used in any moris code.

--- share/cmake/arpack.cmake
+++ share/cmake/arpack.cmake
@@ -2,8 +2,16 @@
 # ARPACK libraries --------------------------------------------------------
 # -------------------------------------------------------------------------
 
-find_package(ARPACK)
+if(NOT ARPACK_FOUND_ONCE)
+    find_package(ARPACK)
+    
+    if(ARPACK_FOUND)
+        set(ARPACK_FOUND_ONCE TRUE CACHE INTERNAL "ARPACK was found.")
+    endif()
+endif()
 
 message(STATUS "ARPACK_LIBRARIES: ${ARPACK_LIBRARIES}")
 
-list(APPEND MORIS_LDLIBS ${ARPACK_LIBRARIES})
+# list(APPEND MORIS_LDLIBS ${ARPACK_LIBRARIES})
+
+set(MORIS_ARPACK_LIBS ${ARPACK_LIBRARIES})
