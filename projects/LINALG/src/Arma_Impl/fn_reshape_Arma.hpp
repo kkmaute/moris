2dde2973679c08b48c7a0927bd7cd795dfc56f32
messe <masc0517@colorado.edu>
2018-09-06 21:18:57 -0600

    fixed reshape() for Armadillo

--- projects/LINALG/src/Arma_Impl/fn_reshape_Arma.hpp
+++ projects/LINALG/src/Arma_Impl/fn_reshape_Arma.hpp
@@ -7,32 +7,7 @@
 
 #ifndef PROJECTS_LINALG_SRC_ARMA_IMPL_FN_RESHAPE_ARMA_HPP_
 #define PROJECTS_LINALG_SRC_ARMA_IMPL_FN_RESHAPE_ARMA_HPP_
-#include <armadillo>
 
-namespace moris
-{
-template<  typename T1, typename T2, typename T3 >
-moris::Mat<T1>
-reshape(
-        moris::Mat< T1 > const & aA,
-        T2               const & aB,
-        T3               const & aC)
-        {
-    return arma::conv_to< arma::Mat<T1>>::from(reshape((arma::Mat<T1>)aA.data(),aB,aC));
-        }
-
-template<  typename T1, typename T2, typename T3 >
-moris::Mat<T1>
-reshape(
-        moris::Sp_Mat< T1 > const & aA,
-        T2               const & aB,
-        T3               const & aC)
-        {
-    return arma::conv_to< arma::Mat<T1>>::from(reshape((arma::Mat<T1>)aA.data(),aB,aC));
-        }
-}
-
-
-
+// nothing to do here
 
 #endif /* PROJECTS_LINALG_SRC_ARMA_IMPL_FN_RESHAPE_ARMA_HPP_ */
