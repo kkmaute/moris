952056d2f583802c72bd469623aac56d1e9e69f9
doble <doble@dorra.colorado.edu>
2018-09-12 14:20:21 -0600

    Added diag function, split into diag_mat and diag_vec from prev impl

---
+++ projects/LINALG/src/Arma_Impl/fn_diag_vec_Arma.hpp
@@ -0,0 +1,29 @@
+/*
+ * fn_diag_vec_Arma.hpp
+ *
+ *  Created on: Sep 12, 2018
+ *      Author: doble
+ */
+
+#ifndef PROJECTS_LINALG_SRC_ARMA_IMPL_FN_DIAG_VEC_ARMA_HPP_
+#define PROJECTS_LINALG_SRC_ARMA_IMPL_FN_DIAG_VEC_ARMA_HPP_
+
+#include "cl_Matrix.hpp"
+#include <armadillo>
+
+namespace moris
+{
+template< typename ET>
+auto
+diag_vec(
+     ET & aA,
+     size_t     const & ak = 0 )
+->decltype(arma::diagvec( aA, ak ))
+{
+    return arma::diagvec( aA, ak );
+}
+}
+
+
+
+#endif /* PROJECTS_LINALG_SRC_ARMA_IMPL_FN_DIAG_VEC_ARMA_HPP_ */
---
+++ projects/LINALG/src/fn_diag_mat.hpp
@@ -0,0 +1,54 @@
+/*
+ * fn_diag.hpp
+ *
+ *  Created on: Sep 6, 2018
+ *      Author: doble
+ */
+
+#ifndef PROJECTS_LINALG_SRC_FN_DIAG_MAT_HPP_
+#define PROJECTS_LINALG_SRC_FN_DIAG_MAT_HPP_
+
+#include "cl_Matrix.hpp"
+
+#ifdef MORIS_USE_EIGEN
+#include "Eigen_Impl/fn_diag_mat_Eigen.hpp"
+#endif
+
+#ifdef MORIS_USE_ARMA
+#include "Arma_Impl/fn_diag_mat_Arma.hpp"
+#endif
+
+namespace moris
+{
+/**
+ * @brief Extract the k-th diagonal from matrix A.
+ *
+ * @param[in] aA Matrix.
+ * @param[in] ak Diagonal index.
+ * The argument k is optional; by default the main diagonal
+ * is extracted (k=0).\n
+ * For k > 0, the k-th super-diagonal is extracted
+ * (top-right corner).\n
+ * For k < 0, the k-th sub-diagonal is extracted
+ * (bottom-left corner).\n
+ *
+ * @return Creates a vector from the diagonal of a matrix such that
+ * @f$ \mathbf{v}_{i}=\mathbf{A}_{ii}@f$ \n
+ * The extracted diagonal is interpreted as a column vector.
+ *
+ */
+
+template< typename Type, typename Matrix_Type >
+auto
+diag_mat(   Matrix< Type, Matrix_Type>  & aA,
+        size_t     const & ak = 0 )
+->decltype( diag_mat( aA.matrix_data(), ak ) )
+{
+    return diag_mat( aA.matrix_data(), ak );
+}
+
+}
+
+
+
+#endif /* PROJECTS_LINALG_SRC_FN_DIAG_MAT_HPP_ */
