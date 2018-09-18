72c925743dfaa9447dd07bdcd5ce666a5a56fc57
Mathia Schmidt <masc0517@colorado.edu>
2018-09-18 17:44:07 -0600

    Added MSI user tutorial

--- projects/FEM/MSI/test/cl_MSI_Model_Solver_Interface_Test.cpp
+++ projects/FEM/MSI/test/cl_MSI_Model_Solver_Interface_Test.cpp
@@ -34,113 +34,225 @@ namespace moris
 {
     namespace MSI
     {
+
     TEST_CASE("MSI_Test","[MSI],[MSI_Test]")
     {
         if ( par_size() == 1 )
         {
-            // Creating nodes and equation objects
-            // Create node obj
-            moris::uint tNodeId1 = 0;
-            moris::uint tNodeId2 = 1;
+            /*!
+             * <h1>User Tutorial: Usage of the MSI based on a two dof problem"</h1>
+             * This example creates a pdof and adofs for a simple 2 dop problem
+             */
+            SECTION( "MSI_Test" )
+            {
+            /*!
+             * <b> Step 1: Preperation: Create node objects </b>
+             */
 
+            /*!
+             * Create pointer to two FEM node objects
+             *
+             * \code{.cpp}
+             * fem::Node_Base * Node1;
+             * fem::Node_Base * Node2;
+             * \endcode
+             */
             fem::Node_Base * Node1;
             fem::Node_Base * Node2;
 
-            // Create generic adofs to this nodes pdof
+            /*!
+             * Create matrix with adof Ids for each FEM node
+             *
+             * \code{.cpp}
+             * moris::Mat< moris::sint> tAdofsId1( 2, 1 );
+             * moris::Mat< moris::sint> tAdofsId2( 2, 1 );
+             *
+             * tAdofsId1( 0, 0 ) = 0;         tAdofsId1( 1, 0 ) = 1;
+             * tAdofsId2( 0, 0 ) = 1;         tAdofsId2( 1, 0 ) = 0;
+             * \endcode
+             */
             moris::Mat< moris::sint> tAdofsId1( 2, 1 );
             moris::Mat< moris::sint> tAdofsId2( 2, 1 );
 
-            tAdofsId1( 0, 0 ) = 0;
-            tAdofsId1( 1, 0 ) = 1;
-            tAdofsId2( 0, 0 ) = 1;
-            tAdofsId2( 1, 0 ) = 0;
+            tAdofsId1( 0, 0 ) = 0;         tAdofsId1( 1, 0 ) = 1;
+            tAdofsId2( 0, 0 ) = 1;         tAdofsId2( 1, 0 ) = 0;
 
-            // Create generic adofs to this nodes pdof
+            /*!
+             * Create matrix with adof Indices for each FEM node
+             *
+             * \code{.cpp}
+             * moris::Mat< moris::sint> tAdofsInd1( 2, 1 );
+             * moris::Mat< moris::sint> tAdofsInd2( 2, 1 );
+             *
+             * tAdofsInd1( 0, 0 ) = 0;        tAdofsInd1( 1, 0 ) = 1;
+             * tAdofsInd2( 0, 0 ) = 1;        tAdofsInd2( 1, 0 ) = 0;
+             * \endcode
+             */
             moris::Mat< moris::sint> tAdofsInd1( 2, 1 );
             moris::Mat< moris::sint> tAdofsInd2( 2, 1 );
 
-            tAdofsInd1( 0, 0 ) = 0;
-            tAdofsInd1( 1, 0 ) = 1;
-            tAdofsInd2( 0, 0 ) = 1;
-            tAdofsInd2( 1, 0 ) = 0;
+            tAdofsInd1( 0, 0 ) = 0;        tAdofsInd1( 1, 0 ) = 1;
+            tAdofsInd2( 0, 0 ) = 1;        tAdofsInd2( 1, 0 ) = 0;
 
-            // Create generic T-matrices
+            /*!
+             * Create matrix with adof T-matrix values for each FEM node
+             *
+             * \code{.cpp}
+             * moris::Mat< moris::real> tMatrix1( 2, 1 );
+             * moris::Mat< moris::sint> tAdofsInd2( 2, 1 );
+             *
+             * tMatrix1( 0, 0 ) = 1.0;           tMatrix1( 1, 0 ) = 1.0;
+             * tMatrix2( 0, 0 ) = 1.0;           tMatrix2( 1, 0 ) = 2.0;
+             * \endcode
+             */
             moris::Mat< moris::real> tMatrix1( 2, 1 );
             moris::Mat< moris::real> tMatrix2( 2, 1 );
 
-            // Create generic T-matrices
-            tMatrix1( 0, 0 ) = 1.0;
-            tMatrix1( 1, 0 ) = 1.0;
-            tMatrix2( 0, 0 ) = 1.0;
-            tMatrix2( 1, 0 ) = 2.0;
+            tMatrix1( 0, 0 ) = 1.0;           tMatrix1( 1, 0 ) = 1.0;
+            tMatrix2( 0, 0 ) = 1.0;           tMatrix2( 1, 0 ) = 2.0;
 
-            // Create generic adof owning processor
+            /*!
+             * Create matrix with information about adof ownership for each FEM node
+             *
+             * \code{.cpp}
+             * moris::Mat< moris::uint> tAdofOwningProcessor1( 2, 1 );
+             * moris::Mat< moris::uint> tAdofOwningProcessor2( 2, 1 );
+             *
+             * tAdofOwningProcessor1( 0, 0 ) = 0;         tAdofOwningProcessor1( 1, 0 ) = 0;
+             * tAdofOwningProcessor2( 0, 0 ) = 0;         tAdofOwningProcessor2( 1, 0 ) = 0;
+             * \endcode
+             */
             moris::Mat< moris::uint> tAdofOwningProcessor1( 2, 1 );
             moris::Mat< moris::uint> tAdofOwningProcessor2( 2, 1 );
 
-            tAdofOwningProcessor1( 0, 0 ) = 0;
-            tAdofOwningProcessor1( 1, 0 ) = 0;
-            tAdofOwningProcessor2( 0, 0 ) = 0;
-            tAdofOwningProcessor2( 1, 0 ) = 0;
+            tAdofOwningProcessor1( 0, 0 ) = 0;         tAdofOwningProcessor1( 1, 0 ) = 0;
+            tAdofOwningProcessor2( 0, 0 ) = 0;         tAdofOwningProcessor2( 1, 0 ) = 0;
 
-            // Create generic Node Object
-            Node1 = new Node_Obj( tNodeId1, tAdofsId1, tAdofsInd1, tMatrix1, tAdofOwningProcessor1 );
-            Node2 = new Node_Obj( tNodeId2, tAdofsId2, tAdofsInd2, tMatrix2, tAdofOwningProcessor2 );
+            /*!
+             * Call constructor of node objects. First argument stands for the node Id, argument 1...5 were specified earlier.
+             *
+             * \code{.cpp}
+             * Node1 = new Node_Obj( 0, tAdofsId1, tAdofsInd1, tMatrix1, tAdofOwningProcessor1 );
+             * Node2 = new Node_Obj( 1, tAdofsId2, tAdofsInd2, tMatrix2, tAdofOwningProcessor2 );
+             * \endcode
+             */
+            Node1 = new Node_Obj( 0, tAdofsId1, tAdofsInd1, tMatrix1, tAdofOwningProcessor1 );
+            Node2 = new Node_Obj( 1, tAdofsId2, tAdofsInd2, tMatrix2, tAdofOwningProcessor2 );
 
-            moris::uint tNumEquationObjects = 2;
+            /*!
+             * Create list with node pointers which will be assigned to a equation object
+             *
+             * \code{.cpp}
+             * moris::Cell< fem::Node_Base* > tNodeIds_1( 2 );
+             * tNodeIds_1( 0 ) = Node1;       tNodeIds_1( 1 ) = Node2;
+             * \endcode
+             */
+            moris::Cell< fem::Node_Base* > tNodeIds_1( 2 );
+            tNodeIds_1( 0 ) = Node1;       tNodeIds_1( 1 ) = Node2;
 
-            moris::uint tNumNodes = 2;
+            moris::Cell< fem::Node_Base* > tNodeIds_2( 2 );
+            tNodeIds_2( 0 ) = Node1;       tNodeIds_2( 1 ) = Node2;
 
-            moris::Cell < Equation_Object* >tListEqnObj( tNumEquationObjects, nullptr );
+            /*!
+             * Create  equation objects. Equation objects have a list with nodes assigned
+             *
+             * \code{.cpp}
+             * moris::Cell < Equation_Object* > tListEqnObj( 2, nullptr );
+             * Equation_Object EquObj_1( tNodeIds_1 );    Equation_Object EquObj_2( tNodeIds_2 );
+             * \endcode
+             */
+            Equation_Object EquObj_1( tNodeIds_1 );    Equation_Object EquObj_2( tNodeIds_2 );
 
-            // Create List with node pointern correponding to generic equation object
-            moris::Cell< fem::Node_Base* > tNodeIds_1( tNumNodes );
-            tNodeIds_1( 0 ) = Node1;
-            tNodeIds_1( 1 ) = Node2;
-
-            moris::Cell< fem::Node_Base* > tNodeIds_2( tNumNodes );
-            tNodeIds_2( 0 ) = Node1;
-            tNodeIds_2( 1 ) = Node2;
-
-            // Create generic equation objects
-            Equation_Object EquObj_1( tNodeIds_1 );
-            Equation_Object EquObj_2( tNodeIds_2 );
-
+            /*!
+             * Set the equation object dof types. Jacobians and residuals
+             *
+             * \code{.cpp}
+             * EquObj_1.mEqnObjDofTypeList.resize( 1, Dof_Type::TEMP);
+             * EquObj_1.mJacobian.set_size( 2, 2, 0.0 );       EquObj_2.mJacobian.set_size( 2, 2, 0.0 );
+             * EquObj_1.mResidual.set_size( 2, 1, 0.0 );       EquObj_2.mResidual.set_size( 2, 1, 0.0 );
+             *
+             * EquObj_1.mJacobian.set_size( 2, 2, 0.0 );            EquObj_2.mJacobian.set_size( 2, 2, 0.0 );
+             * EquObj_1.mResidual.set_size( 2, 1, 0.0 );            EquObj_2.mResidual.set_size( 2, 1, 0.0 );
+             *
+             * EquObj_1.mJacobian( 0, 0 ) = 1;            EquObj_1.mJacobian( 0, 1 ) = 2;
+             * EquObj_2.mJacobian( 1, 0 ) = 1;            EquObj_2.mJacobian( 1, 1 ) = -3;
+             *
+             * EquObj_1.mResidual( 0, 0 ) = 5;
+             * \endcode
+             */
             EquObj_1.mEqnObjDofTypeList.resize( 1, Dof_Type::TEMP);
             EquObj_2.mEqnObjDofTypeList.resize( 1, Dof_Type::TEMP);
 
-            EquObj_1.mJacobian.set_size( 2, 2, 0.0 );
-            EquObj_2.mJacobian.set_size( 2, 2, 0.0 );
-            EquObj_1.mResidual.set_size( 2, 1, 0.0 );
-            EquObj_2.mResidual.set_size( 2, 1, 0.0 );
+            EquObj_1.mJacobian.set_size( 2, 2, 0.0 );            EquObj_2.mJacobian.set_size( 2, 2, 0.0 );
+            EquObj_1.mResidual.set_size( 2, 1, 0.0 );            EquObj_2.mResidual.set_size( 2, 1, 0.0 );
 
-            EquObj_1.mJacobian( 0, 0 ) = 1;
-            EquObj_1.mJacobian( 0, 1 ) = 2;
-            EquObj_2.mJacobian( 1, 0 ) = 1;
-            EquObj_2.mJacobian( 1, 1 ) = -3;
+            EquObj_1.mJacobian( 0, 0 ) = 1;            EquObj_1.mJacobian( 0, 1 ) = 2;
+            EquObj_2.mJacobian( 1, 0 ) = 1;            EquObj_2.mJacobian( 1, 1 ) = -3;
 
             EquObj_1.mResidual( 0, 0 ) = 5;
 
-            // Create List with equation objects
-            tListEqnObj( 0 ) = & EquObj_1;
-            tListEqnObj( 1 ) = & EquObj_2;
+            /*!
+             * Set a list with equation object pointers. This particular list has a size of 2 and 2 equation objects assigned
+             *
+             * \code{.cpp}
+             * moris::Cell < Equation_Object* > tListEqnObj( 2, nullptr );
+             * tListEqnObj( 0 ) = & EquObj_1;      tListEqnObj( 1 ) = & EquObj_2;
+             * \endcode
+             */
+            moris::Cell < Equation_Object* > tListEqnObj( 2, nullptr );
+            tListEqnObj( 0 ) = & EquObj_1;      tListEqnObj( 1 ) = & EquObj_2;
 
+            /*!
+             * Creating cammunication table
+             *
+             * \code{.cpp}
+             * moris::Mat< moris::uint > tCommTable( 1, 1, 0 );
+             * \endcode
+             */
             moris::Mat< moris::uint > tCommTable( 1, 1, 0 );
 
-            //End of creating equation object list and communication table
+            /*!
+             * <b> Step 2: Creating the Model Solver Interface and solve the problem </b>
+             */
 
+            /*!
+             * Create MSI. Input argument are the list of equation object pointers and the communication table
+             *
+             * \code{.cpp}
+             * Model_Solver_Interface tMSI( tListEqnObj, tCommTable );
+             * \endcode
+             */
             Model_Solver_Interface tMSI( tListEqnObj, tCommTable );
 
-            // create solver input object
-            moris::MSI::MSI_Solver_Interface *  tSolverInput;
-            tSolverInput = new moris::MSI::MSI_Solver_Interface( &tMSI, tMSI.get_dof_manager() );
+            /*!
+             * Create solver Interface
+             *
+             * \code{.cpp}
+             * moris::Solver_Input *  tSolverInput = new moris::MSI::MSI_Solver_Interface( &tMSI, tMSI.get_dof_manager() );
+             * \endcode
+             */
+            moris::Solver_Input *  tSolverInput = new moris::MSI::MSI_Solver_Interface( &tMSI, tMSI.get_dof_manager() );
 
-            // create solver factory
+            /*!
+             * Create solver
+             *
+             * \code{.cpp}
+             * moris::Solver_Factory  tSolFactory;
+             *
+             * std::shared_ptr< Linear_Solver > tLin = tSolFactory.create_solver( tSolverInput );
+             * \endcode
+             */
             moris::Solver_Factory  tSolFactory;
 
-            // create solver object
             std::shared_ptr< Linear_Solver > tLin = tSolFactory.create_solver( tSolverInput );
 
+            /*!
+             * Solve linear system
+             *
+             * \code{.cpp}
+             * tLin->solve_linear_system();
+             * \endcode
+             */
             tLin->solve_linear_system();
 
             moris::Mat< moris::real > tSolution;
@@ -152,6 +264,7 @@ namespace moris
             delete Node1;
             delete Node2;
             delete tSolverInput;
+            }
         }
     }
 
