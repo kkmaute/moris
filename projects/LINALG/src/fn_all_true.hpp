a329ecee48826c54c7e60891c9a8f64abdd5b48f
doble <doble@dorra.colorado.edu>
2018-09-19 15:22:11 -0600

    Added all true function

---
+++ projects/LINALG/src/fn_all_true.hpp
@@ -0,0 +1,48 @@
+/*
+ * fn_all_true.hpp
+ *
+ *  Created on: Sep 19, 2018
+ *      Author: doble
+ */
+
+#ifndef PROJECTS_LINALG_SRC_FN_ALL_TRUE_HPP_
+#define PROJECTS_LINALG_SRC_FN_ALL_TRUE_HPP_
+
+#include "cl_Matrix.hpp"
+#include "linalg_typedefs.hpp"
+
+namespace moris
+{
+
+    /*
+     * Checks whether all elements in a matrix evaluate to true.
+     * For example if the result of an tMat1 == tMat2 holds
+     * for all elements, this will return true.
+     */
+    bool
+    all_true(Matrix< DDBMat > const & aMatrix)
+    {
+        bool tAllTrue = false;
+        uint tNumRows = aMatrix.n_rows();
+        uint tNumCols = aMatrix.n_cols();
+
+        for(uint i = 0; i<tNumRows; i++)
+        {
+            for(uint j = 0; j<tNumCols; j++)
+            {
+                if(!aMatrix(i,j))
+                {
+                    return false;
+                }
+            }
+        }
+
+        tAllTrue = true;
+        return tAllTrue;
+    }
+
+}
+
+
+
+#endif /* PROJECTS_LINALG_SRC_FN_ALL_TRUE_HPP_ */
