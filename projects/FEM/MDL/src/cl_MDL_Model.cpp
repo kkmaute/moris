9438fd1ce1ee9a78293242cfd3d2dc248b9880ed
messe <christian.messe@colorado.edu>
2018-09-28 18:42:20 -0600

    HMR: merge with main stream

--- projects/FEM/MDL/src/cl_MDL_Model.cpp
+++ projects/FEM/MDL/src/cl_MDL_Model.cpp
@@ -36,18 +36,6 @@ namespace moris
             // how many cells exist on current proc
             auto tNumberOfElements = tBlock->get_number_of_cells();
 
-            // flag elements on this block
-            for( luint e=0; e<tNumberOfElements; ++e )
-            {
-                // flag cell
-                tBlock->get_cell_by_index( e )->set_t_matrix_flag();
-            }
-
-            // fixme: This turned out to be a bad idea!
-            //        This is HMR specific and needs to be moved outside the model
-            // finalize mesh ( calculate T-Matrices etc )
-            aMesh->finalize();
-
             // create nodes for these elements
             auto tNumberOfNodes = tBlock->get_number_of_vertices();
 
