9f33a7a67bd8ebeb5f158a52a24851dd1117b652
Matthew Ryan <matthew.j.ryan@colorado.edu>
2018-11-07 14:24:24 -0700

    Updated BUILD_ALL script.

--- share/cmake/utilities/build_all_exe.cmake
+++ share/cmake/utilities/build_all_exe.cmake
@@ -17,15 +17,13 @@ set(BUILD_EXC ON CACHE BOOL "Build the exceptions executable." FORCE)
 
 set(BUILD_FEM ON CACHE BOOL "Build the FEM executable." FORCE)
 
-#set(BUILD_GEN ON CACHE BOOL "Build the geometry engine executable." FORCE)
+set(BUILD_GEN ON CACHE BOOL "Build the geometry engine executable." FORCE)
 
 set(BUILD_HMR ON CACHE BOOL "Build the HMR executable." FORCE)
 
 set(BUILD_INT ON CACHE BOOL "Build the integration and interpolation executable." FORCE)
 
 set(BUILD_IOS ON CACHE BOOL "Build the IOS executable." FORCE)
-
-#set(BUILD_LNA ON CACHE BOOL "Build the linear algebra executable." FORCE)
 
 set(BUILD_LINALG ON CACHE BOOL "Build the linear algebra executable." FORCE)
 
@@ -34,18 +32,19 @@ set(BUILD_MOD ON CACHE BOOL "Build the model executable." FORCE)
 set(BUILD_MSI ON CACHE BOOL "Build the model solver interface executable." FORCE)
 
 set(BUILD_MTK ON CACHE BOOL "Build the MTK executable." FORCE)
+set(BUILD_MAP ON CACHE BOOL "Build the MTK mapper." FORCE)
 
 set(BUILD_NLA ON CACHE BOOL "Build the non-linear algebra executable." FORCE)
 
 set(BUILD_OPT ON CACHE BOOL "Build the optimization executable." FORCE)
 
-#set(BUILD_STK ON CACHE BOOL "Build the STK executable." FORCE)
+set(BUILD_STK ON CACHE BOOL "Build the STK executable." FORCE)
 
 set(BUILD_TIN ON CACHE BOOL "Build the TIN executable." FORCE)
 
 set(BUILD_TOL ON CACHE BOOL "Build the tools executable." FORCE)
 
-#set(BUILD_XTK ON CACHE BOOL "Build the XTK executable." FORCE)
+set(BUILD_XTK ON CACHE BOOL "Build the XTK executable." FORCE)
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
