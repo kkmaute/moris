56671d9374a3bbd8ec6588e1c916625de68857fd
Mathia Schmidt <masc0517@colorado.edu>
2019-01-21 17:09:50 -0700

    NLA NLBGS

--- projects/SOL/NLA/src/cl_NLA_DofTypeStruc.hpp
+++ projects/SOL/NLA/src/cl_NLA_DofTypeStruc.hpp
@@ -9,6 +9,7 @@
 
 #include "typedefs.hpp"
 #include "cl_Cell.hpp"
+#include "cl_Matrix.hpp"
 #include "cl_MSI_Dof_Type_Enums.hpp"
 
 namespace moris
@@ -18,13 +19,14 @@ namespace NLA
     class DofTypeStruc
     {
     private:
-        moris::sint                                      mLevel = -1;
+
         moris::Cell< moris::Cell< enum MSI::Dof_Type > > mStaggeredDofTypeList;
+        moris::sint                                      mLevel = -1;
 
     public:
         DofTypeStruc( const moris::Cell< moris::Cell< enum MSI::Dof_Type > > aStaggeredDofTypeList,
-                      const moris::sint                                      aLevel =  0 ) mStaggeredDofTypeList : aStaggeredDofTypeList,
-                                                                                           mLevel = aLevel
+                      const moris::sint                                      aLevel =  0 ) : mStaggeredDofTypeList( aStaggeredDofTypeList),
+                                                                                             mLevel (aLevel)
         {};
 
         ~DofTypeStruc()
@@ -41,23 +43,30 @@ namespace NLA
             return mLevel;
         };
 
-        moris::Cell< enum MSI::Dof_Type > get_dof_type_union()
-        {
-            moris::sint tCounter = 0;
-
-            for ( moris::sint Ik = 0; Ik <= mStaggeredDofTypeList.size(); ++Ik )
-            {
-                tCounter =+ mStaggeredDofTypeList( Ik ).size();
-            }
-
-            for ( moris::sint Ik = 0; Ik <= mStaggeredDofTypeList.size(); ++Ik )
-            {
-                for ( moris::sint Ik = 0; Ik <= mStaggeredDofTypeList.size(); ++Ik )
-                {
-                    tCounter =+ mStaggeredDofTypeList( Ik ).size();
-                }
-            }
-        };
+//        moris::Cell< enum MSI::Dof_Type > get_dof_type_union()
+//        {
+//            moris::sint tCounter = 0;
+//
+//            for ( moris::sint Ik = 0; Ik <= mStaggeredDofTypeList.size(); ++Ik )
+//            {
+//                tCounter =+ mStaggeredDofTypeList( Ik ).size();
+//            }
+//
+//            tCounter = 0;
+//            moris::Cell< enum MSI::Dof_Type > tUnionEnumList( tCounter );
+//
+//            for ( moris::sint Ik = 0; Ik < mStaggeredDofTypeList.size(); ++Ik )
+//            {
+//                for ( moris::sint Ii = 0; Ii < mStaggeredDofTypeList( Ik ).size(); ++Ii )
+//                {
+//                    tUnionEnumList( tCounter ) = mStaggeredDofTypeList( Ik )( Ii );
+//
+//                    tCounter++;;
+//                }
+//            }
+//
+//            return tUnionEnumList;
+//        };
     };
 }
 }
