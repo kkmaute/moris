15c15e24bbe25d04f3444f8b6ab8610fa5eef6a6
Mathia Schmidt <masc0517@colorado.edu>
2020-01-29 10:29:48 -0700

    - vis update

--- projects/FEM/VIS/test/UT_VIS_Output_Manager.cpp
+++ projects/FEM/VIS/test/UT_VIS_Output_Manager.cpp
@@ -339,7 +339,6 @@ TEST_CASE(" Output Data","[VIS],[Output_Data]")
                                          VIS_Mesh_Type::OVERLAPPING_INTERFACE,
                                          "Output_Vis_Mesh_overlapping.exo",
                                          { "HMR_dummy_c_p0", "HMR_dummy_c_p1", "HMR_dummy_n_p0", "HMR_dummy_n_p1"},
-                                         { 0, 1, 2, 3 },
                                          { "strain energy elemental", "strain energy global", "strain energy nodal IP" },
                                          { Field_Type::ELEMENTAL, Field_Type::GLOBAL,  Field_Type::NODAL },
                                          { Output_Type::STRAIN_ENERGY, Output_Type::STRAIN_ENERGY, Output_Type::STRAIN_ENERGY } );
