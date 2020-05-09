f5a7a91ce9c74cb29e27664521109debe05aca23
maute <maute@colorado.edu>
2020-05-08 19:31:28 -0600

    Renamed convergence parameters:
    NLA_tot_res_norm_drop -> NLA_rel_res_norm_drop
    NLA_max_res_norm_drop -> NLA_max_rel_res_norm
    
    Note the values are relative to the reference residual

--- projects/SOL/NLA/src/cl_NLA_Convergence.cpp
+++ projects/SOL/NLA/src/cl_NLA_Convergence.cpp
@@ -23,168 +23,170 @@ namespace moris
 {
     namespace NLA
     {
-//--------------------------------------------------------------------------------------------------------------------------
-    bool Convergence::check_for_convergence(         Nonlinear_Algorithm * tNonLinSolver,
-                                                     moris::sint & aIt,
-                                                     moris::real & aRefNorm,
-                                                     moris::real & aResNorm,
-                                               const moris::real & aAssemblyTime,
-                                               const moris::real & aSolvTime,
-                                                     bool        & aHartBreak )
-    {
-        bool tIsConverged = false;
-
-        moris::real solNorm = tNonLinSolver->mNonlinearProblem->get_full_vector()->vec_norm2();
-
-        aResNorm = tNonLinSolver->mNonlinearProblem->get_linearized_problem()->get_solver_RHS()->vec_norm2();
-
-        MORIS_LOG_SPEC( OutputSpecifier::ResidualNorm, aResNorm );
-        MORIS_LOG_SPEC( OutputSpecifier::SolutionNorm, solNorm );
-
-        if ( aIt <= 1)
+        //--------------------------------------------------------------------------------------------------------------------------
+        bool Convergence::check_for_convergence(
+                Nonlinear_Algorithm * tNonLinSolver,
+                moris::sint         & aIt,
+                moris::real         & aRefNorm,
+                moris::real         & aResNorm,
+                const moris::real   & aAssemblyTime,
+                const moris::real   & aSolvTime,
+                bool                & aHartBreak )
         {
-            aRefNorm = aResNorm;
-            MORIS_LOG( "--------------------------------------------------------------------------------");
-            MORIS_LOG( " Newton ... refNorm for pseudo-time step is %+1.15e", aRefNorm );
-            MORIS_LOG( "--------------------------------------------------------------------------------" );
+            bool tIsConverged = false;
 
-            if ( par_rank() == 0 )
+            moris::real solNorm = tNonLinSolver->mNonlinearProblem->get_full_vector()->vec_norm2();
+
+            aResNorm = tNonLinSolver->mNonlinearProblem->get_linearized_problem()->get_solver_RHS()->vec_norm2();
+
+            MORIS_LOG_SPEC( OutputSpecifier::ResidualNorm, aResNorm );
+            MORIS_LOG_SPEC( OutputSpecifier::SolutionNorm, solNorm );
+
+            if ( aIt <= 1)
             {
-                MORIS_LOG( "        NlinIt  |  NlinResNorm            |  NlinResDrop  |  SolVecNorm             ||  LinAsmTime  |  NewItrTime" );
-                MORIS_LOG( "         %-5i  |  %-15.15e  |  %-11.5e  |  %-10.15e  ||  %9.4e  |  %9.4e ", 1, aResNorm, 0.0, solNorm, aAssemblyTime, 0.0);
+                aRefNorm = aResNorm;
+                MORIS_LOG( "--------------------------------------------------------------------------------");
+                MORIS_LOG( " Newton ... refNorm for pseudo-time step is %+1.15e", aRefNorm );
+                MORIS_LOG( "--------------------------------------------------------------------------------" );
+
+                if ( par_rank() == 0 )
+                {
+                    MORIS_LOG( "        NlinIt  |  NlinResNorm            |  NlinResDrop  |  SolVecNorm             ||  LinAsmTime  |  NewItrTime" );
+                    MORIS_LOG( "         %-5i  |  %-15.15e  |  %-11.5e  |  %-10.15e  ||  %9.4e  |  %9.4e ", 1, aResNorm, 0.0, solNorm, aAssemblyTime, 0.0);
+                }
             }
+
+            MORIS_ERROR( !( std::isnan( aResNorm ) || std::isinf( aResNorm )), "Convergence::check_for_convergence(): Residual contains NAN or INF, exiting!");
+
+            MORIS_ERROR( !( aResNorm > 1e20 ), "Convergence::check_for_convergence(): Residual Norm has exceeded 1e20");
+
+            // Check for convergence
+            if ( ( aIt > 1 ) && ( aResNorm  < aRefNorm * tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_rel_res_norm_drop" ) ) )
+            {
+                if ( par_rank() == 0 )
+                {
+                    MORIS_LOG( "         %-5i  |  %-15.15e  |  %-11.5e  |  %-10.15e  ||  %-10.4e  |  (NlinResDrop < %6.1e)",
+                            aIt, aResNorm, ( aResNorm/aRefNorm ), solNorm,  aAssemblyTime, tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_rel_res_norm_drop" ) );
+                }
+
+                tIsConverged = true;
+            }
+            else if ( ( aIt > 1 ) && ( aResNorm < tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_tot_res_norm" ) ) )
+            {
+                if ( par_rank() == 0 )
+                {
+                    MORIS_LOG( "         %-5i  |  %-15.15e  |  %-11.5e  |  %-10.15e  ||  %-10.4e  |  (NlinResNorm < %6.1e)",
+                            aIt, aResNorm, (aResNorm/aRefNorm), solNorm, aAssemblyTime, tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_tot_res_norm" ) );
+                }
+
+                tIsConverged = true;
+            }
+            else if ( ( aIt > 1 ) && ( aResNorm > aRefNorm * tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_max_rel_res_norm" ) ) )
+            {
+                // case for residual drop getting too big, not converged, need to retry
+                if ( par_rank() == 0 )
+                {
+                    MORIS_LOG( "         %-5i  |  %-15.15e  |  %-11.5e  |  %-10.15e  ||  %-10.4e  |  (MaxRelResNorm > %6.1e)",
+                            aIt, aResNorm, ( aResNorm/aRefNorm ), solNorm, aAssemblyTime, tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_max_rel_res_norm" ) );
+                }
+
+                tIsConverged = false;
+
+                if ( tNonLinSolver->mParameterListNonlinearSolver.get< bool >( "NLA_hard_break" ) )
+                {
+                    aIt = tNonLinSolver->mParameterListNonlinearSolver.get< moris::sint >( "NLA_max_iter" );
+                    aHartBreak = true;
+                }
+            }
+            else if( ( aIt > 1 ) )
+            {
+                if ( par_rank() == 0 )
+                {
+                    MORIS_LOG( "         %-5i  |  %-15.15e  |  %-11.5e  |  %-10.15e  ||  %9.4e  |  %9.4e ", aIt, aResNorm, (aResNorm/aRefNorm), solNorm, aAssemblyTime, aSolvTime  );
+                }
+            }
+
+            return tIsConverged;
         }
 
-        MORIS_ERROR( !( std::isnan( aResNorm ) || std::isinf( aResNorm )), "Convergence::check_for_convergence(): Residual contains NAN or INF, exiting!");
+        //--------------------------------------------------------------------------------------------------------------------------
 
-        MORIS_ERROR( !( aResNorm > 1e20 ), "Convergence::check_for_convergence(): Residual Norm has exceeded 1e20");
-
-        // Check for convergence
-        if ( ( aIt > 1 ) && ( ( aResNorm/aRefNorm ) < tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_tot_res_norm_drop" ) ) )
+        bool Convergence::check_for_convergence(
+                Nonlinear_Algorithm * tNonLinSolver,
+                moris::sint         & aIt,
+                moris::real         & aRefNorm,
+                moris::real         & aResNorm,
+                const moris::real   & aSolvTime,
+                bool                & aHartBreak )
         {
-            if ( par_rank() == 0 )
+            bool tIsConverged = false;
+
+            if ( aIt <= 1)
             {
-                MORIS_LOG( "         %-5i  |  %-15.15e  |  %-11.5e  |  %-10.15e  ||  %-10.4e  |  (NlinResDrop < %6.1e)",
-                        aIt, aResNorm, ( aResNorm/aRefNorm ), solNorm,  aAssemblyTime, tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_tot_res_norm_drop" ) );
+                aRefNorm = aResNorm;
+                MORIS_LOG( "--------------------------------------------------------------------------------");
+                MORIS_LOG( " NLBGS ... refNorm for pseudo-time step is %+1.15e", aRefNorm );
+                MORIS_LOG( "--------------------------------------------------------------------------------" );
+
+                if ( par_rank() == 0 )
+                {
+                    MORIS_LOG( "        NlinIt  |  NlinResNorm            |  NlinResDrop  ||  NewItrTime" );
+                    MORIS_LOG( "         %-5i  |  %-15.15e  |  %-11.5e  ||  %9.4e ", 1, aResNorm, 0.0,  0.0  );
+                }
             }
 
-            tIsConverged = true;
+            MORIS_ERROR( !( std::isnan( aResNorm ) || std::isinf( aResNorm )), "Convergence::check_for_convergence(): Residual contains NAN or INF, exiting!");
+
+            MORIS_ERROR( !( aResNorm > 1e20 ), "Convergence::check_for_convergence(): Residual Norm has exceeded 1e20");
+
+            // Check for convergence
+            if ( ( aIt > 1 ) && ( aResNorm < aRefNorm * tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_rel_res_norm_drop" ) ) )
+            {
+                if ( par_rank() == 0 )
+                {
+                    MORIS_LOG( "         %-5i  |  %-15.15e  |  %-11.5e  ||  %-10.4e  |  (NlinResDrop < %6.1e)",
+                            aIt, aResNorm, ( aResNorm/aRefNorm ), aSolvTime, tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_rel_res_norm_drop" ) );
+                }
+
+                tIsConverged = true;
+            }
+            else if ( ( aIt > 1 ) && ( aResNorm < tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_tot_res_norm" ) ) )
+            {
+                if ( par_rank() == 0 )
+                {
+                    MORIS_LOG( "         %-5i  |  %-15.15e  |  %-11.5e  ||  %-10.4e  |  (NlinResNorm < %6.1e)",
+                            aIt, aResNorm, (aResNorm/aRefNorm), aSolvTime, tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_tot_res_norm" ) );
+                }
+
+                tIsConverged = true;
+            }
+            else if ( ( aIt > 1 ) && ( aResNorm > aRefNorm * tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_max_rel_res_norm" ) ) )
+            {
+                // case for residual drop getting too big, not converged, need to retry
+                if ( par_rank() == 0 )
+                {
+                    MORIS_LOG( "         %-5i  |  %-15.15e  |  %-11.5e  ||  %-10.4e  |  (MaxRelResNorm > %6.1e)",
+                            aIt, aResNorm, ( aResNorm/aRefNorm ), aSolvTime, tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_max_rel_res_norm" ) );
+                }
+
+                tIsConverged = false;
+
+                if ( tNonLinSolver->mParameterListNonlinearSolver.get< bool >( "NLA_hard_break" ) )
+                {
+                    aIt = tNonLinSolver->mParameterListNonlinearSolver.get< moris::sint >( "NLA_max_iter" );
+                    aHartBreak = true;
+                }
+            }
+            else if( ( aIt > 1 ) )
+            {
+                if ( par_rank() == 0 )
+                {
+                    MORIS_LOG( "         %-5i  |  %-15.15e  |  %-11.5e  ||  %9.4e  |  %9.4e ",
+                            aIt, aResNorm, (aResNorm/aRefNorm), aSolvTime, aSolvTime );
+                }
+            }
+
+            return tIsConverged;
         }
-        else if ( ( aIt > 1 ) && ( aResNorm < tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_tot_res_norm" ) ) )
-        {
-            if ( par_rank() == 0 )
-            {
-                MORIS_LOG( "         %-5i  |  %-15.15e  |  %-11.5e  |  %-10.15e  ||  %-10.4e  |  (NlinResNorm < %6.1e)",
-                        aIt, aResNorm, (aResNorm/aRefNorm), solNorm, aAssemblyTime, tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_tot_res_norm" ) );
-            }
-
-            tIsConverged = true;
-        }
-        else if ( ( aIt > 1 ) && ( ( aResNorm/aRefNorm ) > tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_max_res_norm_drop" ) ) )
-        {
-            // case for residual drop getting too big, not converged, need to retry
-            if ( par_rank() == 0 )
-            {
-                MORIS_LOG( "         %-5i  |  %-15.15e  |  %-11.5e  |  %-10.15e  ||  %-10.4e  |  (MaxResNormDrop > %6.1e)",
-                        aIt, aResNorm, ( aResNorm/aRefNorm ), solNorm, aAssemblyTime, tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_max_res_norm_drop" ) );
-            }
-
-            tIsConverged = false;
-
-            if ( tNonLinSolver->mParameterListNonlinearSolver.get< bool >( "NLA_hard_break" ) )
-            {
-                aIt = tNonLinSolver->mParameterListNonlinearSolver.get< moris::sint >( "NLA_max_iter" );
-                aHartBreak = true;
-            }
-        }
-        else if( ( aIt > 1 ) )
-        {
-            if ( par_rank() == 0 )
-            {
-                MORIS_LOG( "         %-5i  |  %-15.15e  |  %-11.5e  |  %-10.15e  ||  %9.4e  |  %9.4e ", aIt, aResNorm, (aResNorm/aRefNorm), solNorm, aAssemblyTime, aSolvTime  );
-            }
-        }
-
-        return tIsConverged;
-    }
-//--------------------------------------------------------------------------------------------------------------------------
-
-    bool Convergence::check_for_convergence(         Nonlinear_Algorithm * tNonLinSolver,
-                                                     moris::sint & aIt,
-                                                     moris::real & aRefNorm,
-                                                     moris::real & aResNorm,
-                                               const moris::real & aSolvTime,
-                                                     bool        & aHartBreak )
-    {
-        bool tIsConverged = false;
-
-        if ( aIt <= 1)
-        {
-            aRefNorm = aResNorm;
-            MORIS_LOG( "--------------------------------------------------------------------------------");
-            MORIS_LOG( " NLBGS ... refNorm for pseudo-time step is %+1.15e", aRefNorm );
-            MORIS_LOG( "--------------------------------------------------------------------------------" );
-
-            if ( par_rank() == 0 )
-            {
-                MORIS_LOG( "        NlinIt  |  NlinResNorm            |  NlinResDrop  ||  NewItrTime" );
-                MORIS_LOG( "         %-5i  |  %-15.15e  |  %-11.5e  ||  %9.4e ", 1, aResNorm, 0.0,  0.0  );
-            }
-        }
-
-        MORIS_ERROR( !( std::isnan( aResNorm ) || std::isinf( aResNorm )), "Convergence::check_for_convergence(): Residual contains NAN or INF, exiting!");
-
-        MORIS_ERROR( !( aResNorm > 1e20 ), "Convergence::check_for_convergence(): Residual Norm has exceeded 1e20");
-
-        // Check for convergence
-        if ( ( aIt > 1 ) && ( ( aResNorm/aRefNorm ) < tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_tot_res_norm_drop" ) ) )
-        {
-            if ( par_rank() == 0 )
-            {
-                MORIS_LOG( "         %-5i  |  %-15.15e  |  %-11.5e  ||  %-10.4e  |  (NlinResDrop < %6.1e)",
-                        aIt, aResNorm, ( aResNorm/aRefNorm ), aSolvTime, tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_tot_res_norm_drop" ) );
-            }
-
-            tIsConverged = true;
-        }
-        else if ( ( aIt > 1 ) && ( aResNorm < tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_tot_res_norm" ) ) )
-        {
-            if ( par_rank() == 0 )
-            {
-                MORIS_LOG( "         %-5i  |  %-15.15e  |  %-11.5e  ||  %-10.4e  |  (NlinResNorm < %6.1e)",
-                        aIt, aResNorm, (aResNorm/aRefNorm), aSolvTime, tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_tot_res_norm" ) );
-            }
-
-            tIsConverged = true;
-        }
-        else if ( ( aIt > 1 ) && ( ( aResNorm/aRefNorm ) > tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_max_res_norm_drop" ) ) )
-        {
-            // case for residual drop getting too big, not converged, need to retry
-            if ( par_rank() == 0 )
-            {
-                MORIS_LOG( "         %-5i  |  %-15.15e  |  %-11.5e  ||  %-10.4e  |  (MaxResNormDrop > %6.1e)",
-                        aIt, aResNorm, ( aResNorm/aRefNorm ), aSolvTime, tNonLinSolver->mParameterListNonlinearSolver.get< moris::real >( "NLA_max_res_norm_drop" ) );
-            }
-
-            tIsConverged = false;
-
-            if ( tNonLinSolver->mParameterListNonlinearSolver.get< bool >( "NLA_hard_break" ) )
-            {
-                aIt = tNonLinSolver->mParameterListNonlinearSolver.get< moris::sint >( "NLA_max_iter" );
-                aHartBreak = true;
-            }
-        }
-        else if( ( aIt > 1 ) )
-        {
-            if ( par_rank() == 0 )
-            {
-                MORIS_LOG( "         %-5i  |  %-15.15e  |  %-11.5e  ||  %9.4e  |  %9.4e ",
-                        aIt, aResNorm, (aResNorm/aRefNorm), aSolvTime, aSolvTime );
-            }
-        }
-
-        return tIsConverged;
-    }
-
     }
 }
