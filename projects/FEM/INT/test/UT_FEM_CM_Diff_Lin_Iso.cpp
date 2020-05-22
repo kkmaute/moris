96b28d2808f2d55166ebdde9bc290ea6a8c8ce73
wunsch <wunsch@tomcat>
2020-05-21 21:40:41 -0600

    Added unit tests for Diffusion-CM for quadratic and cubic elements

--- projects/FEM/INT/test/UT_FEM_CM_Diff_Lin_Iso.cpp
+++ projects/FEM/INT/test/UT_FEM_CM_Diff_Lin_Iso.cpp
@@ -35,19 +35,31 @@ namespace moris
 {
     namespace fem
     {
-        TEST_CASE( "CM_Diff_Lin_Iso", "[moris],[fem],[CM_Diff_Lin_Iso]" )
+        moris::Cell<bool> test_diffusion_constitutive_model(
+                Matrix< DDRMat > aXHat,
+                Matrix< DDRMat > aTHat,
+                Interpolation_Rule aGeomInterpRule,
+                Interpolation_Rule aIPRule,
+                Matrix< DDRMat > aUHat0,
+                Matrix< DDRMat > aParametricPoint)
         {
             // real for check
             real tEpsilon = 1E-6;
 
-            // create the properties
+            // initialize cell of checks
+            moris::Cell<bool> tChecks( 2, false );
+
+            // real for check
+            real tEpsilonRel = 1E-6;
+
+            // create the properties --------------------------------------------------------------------- //
             std::shared_ptr< fem::Property > tPropMasterConductivity = std::make_shared< fem::Property >();
             tPropMasterConductivity->set_parameters( {{{ 1.0}}, {{1.0 }}} );
             tPropMasterConductivity->set_dof_type_list( {{ MSI::Dof_Type::TEMP }} );
             tPropMasterConductivity->set_val_function( tValFunctionCM_Diff_Lin_Iso );
             tPropMasterConductivity->set_dof_derivative_functions( { tDerFunctionCM_Diff_Lin_Iso } );
 
-            // define constitutive models
+            // define constitutive models ---------------------------------------------------------------- //
             fem::CM_Factory tCMFactory;
 
             std::shared_ptr< fem::Constitutive_Model > tCMMasterDiffLinIso = tCMFactory.create_CM( fem::Constitutive_Type::DIFF_LIN_ISO );
@@ -55,49 +67,22 @@ namespace moris
             tCMMasterDiffLinIso->set_property( tPropMasterConductivity, "Conductivity" );
             tCMMasterDiffLinIso->set_space_dim( 2 );
 
-            //create a quad4 space element
-            Matrix< DDRMat > tXHat( 4, 2 );
-            tXHat( 0, 0 ) = 0.0; tXHat( 0, 1 ) = 0.0;
-            tXHat( 1, 0 ) = 1.0; tXHat( 1, 1 ) = 0.0;
-            tXHat( 2, 0 ) = 1.0; tXHat( 2, 1 ) = 1.0;
-            tXHat( 3, 0 ) = 0.0; tXHat( 3, 1 ) = 1.0;
-
-            //create a line time element
-            Matrix< DDRMat > tTHat( 2, 1 );
-            tTHat( 0 ) = 0.0;
-            tTHat( 1 ) = 5.0;
-
-            //create a space geometry interpolation rule
-            Interpolation_Rule tGeomInterpRule( mtk::Geometry_Type::QUAD,
-                                                Interpolation_Type::LAGRANGE,
-                                                mtk::Interpolation_Order::LINEAR,
-                                                Interpolation_Type::LAGRANGE,
-                                                mtk::Interpolation_Order::LINEAR );
-
             //create a space and a time geometry interpolator
-            Geometry_Interpolator tGI( tGeomInterpRule );
+            Geometry_Interpolator tGI( aGeomInterpRule );
 
             //set the coefficients xHat, tHat
-            tGI.set_coeff( tXHat, tTHat );
-            tGI.set_space_time({{0.0}, {0.0}, {-1.0}});
-
-            // create an interpolation rule
-            Interpolation_Rule tIPRule ( mtk::Geometry_Type::QUAD,
-                                         Interpolation_Type::LAGRANGE,
-                                         mtk::Interpolation_Order::LINEAR,
-                                         Interpolation_Type::LAGRANGE,
-                                         mtk::Interpolation_Order::LINEAR );
+            tGI.set_coeff( aXHat, aTHat );
+            tGI.set_space_time(aParametricPoint);
 
             // create a TEMP field interpolator
             Cell< Field_Interpolator* > tFIs( 1, nullptr );
-            tFIs( 0 ) = new Field_Interpolator ( 1, tIPRule, & tGI, { MSI::Dof_Type::TEMP } );
+            tFIs( 0 ) = new Field_Interpolator ( 1, aIPRule, & tGI, { MSI::Dof_Type::TEMP } );
 
             // set coefficients for field interpolators
-            Matrix< DDRMat > tUHat0 = {{1.0},{1.0},{2.0},{2.0},{1.0},{1.0},{2.0},{2.0}};
-            tFIs( 0 )->set_coeff( tUHat0 );
-            tFIs( 0 )->set_space_time({{0.0}, {0.0}, {-1.0}});
+            tFIs( 0 )->set_coeff( aUHat0 );
+            tFIs( 0 )->set_space_time(aParametricPoint);
 
-            // create a set
+            // create a fem set
             //MSI::Equation_Set * tSet = new fem::Set();
             fem::Set tSet;
 
@@ -120,6 +105,7 @@ namespace moris
 
             // set field interpolator manager
             tCMMasterDiffLinIso->set_field_interpolator_manager( &tFIManager );
+
 
             // check flux-------------------------------------------------------------------
             //------------------------------------------------------------------------------
@@ -145,7 +131,13 @@ namespace moris
                     tCheckdStress = tCheckdStress && ( tdFluxdDOF( iStress, jStress ) - tdFluxdDOF_FD( iStress, jStress ) < tEpsilon );
                 }
             }
-            REQUIRE( tCheckdStress );
+            //REQUIRE( tCheckdStress );
+            tChecks(0) = tCheckdStress;
+
+// debug
+//moris::print(tdFluxdDOF, "tdFluxdDOF");
+//moris::print(tdFluxdDOF_FD, "tdFluxdDOF_FD");
+
 
             // check strain-----------------------------------------------------------------
             //------------------------------------------------------------------------------
@@ -170,7 +162,13 @@ namespace moris
                     tCheckdStrain = tCheckdStrain && ( tdStraindDOF( iStress, jStress ) - tdStraindDOF_FD( iStress, jStress ) < tEpsilon );
                 }
             }
-            REQUIRE( tCheckdStrain );
+            //REQUIRE( tCheckdStrain );
+            tChecks(1) = tCheckdStrain;
+
+// debug
+//moris::print(tdStraindDOF, "tdStraindDOF");
+//moris::print(tdStraindDOF_FD, "tdStraindDOF_FD");
+
 
             // check constitutive matrix----------------------------------------------------
             //------------------------------------------------------------------------------
@@ -211,7 +209,199 @@ namespace moris
             //------------------------------------------------------------------------------
             tFIs.clear();
 
-        }/* TEST_CASE */
+            // return cell of checks
+            return tChecks;
+
+        }/* TEST Function */
+
+
+
+        // ------------------------------------------------------------------------------------- //
+        // ------------------------------------------------------------------------------------- //
+        TEST_CASE( "CM_Diff_Lin_Iso_Linear", "[moris],[fem],[CM_Diff_Lin_Iso_Linear]" )
+        {
+            //create a quad4 space element
+            Matrix< DDRMat > tXHat = {
+                    { 0.0, 0.0},
+                    { 1.0, 0.0},
+                    { 1.0, 1.0},
+                    { 0.0, 1.0}};
+
+            //create a line time element
+            Matrix< DDRMat > tTHat( 2, 1 );
+            tTHat( 0 ) = 1.0e-3;
+            tTHat( 1 ) = 1.1e-3;
+
+            //create a space geometry interpolation rule
+            Interpolation_Rule tGeomInterpRule(
+                    mtk::Geometry_Type::QUAD,
+                    Interpolation_Type::LAGRANGE,
+                    mtk::Interpolation_Order::LINEAR,
+                    Interpolation_Type::LAGRANGE,
+                    mtk::Interpolation_Order::LINEAR );
+
+            // create an interpolation rule
+            Interpolation_Rule tIPRule (
+                    mtk::Geometry_Type::QUAD,
+                    Interpolation_Type::LAGRANGE,
+                    mtk::Interpolation_Order::LINEAR,
+                    Interpolation_Type::LAGRANGE,
+                    mtk::Interpolation_Order::LINEAR );
+
+            // set coefficients for field interpolators
+            Matrix< DDRMat > tUHat0 = {{3.9},{4.4},{4.9},{4.2},{4.9},{5.4},{5.9},{6.0}};
+            Matrix< DDRMat > tParametricPoint = {{-0.4}, { 0.1}, {-0.6}};
+
+            // run test
+            moris::Cell<bool> tChecks = test_diffusion_constitutive_model(
+                            tXHat,
+                            tTHat,
+                            tGeomInterpRule,
+                            tIPRule,
+                            tUHat0,
+                            tParametricPoint);
+
+            // checks
+            bool tCheckdStress = tChecks(0);
+            bool tCheckdStrain = tChecks(1);
+            REQUIRE( tCheckdStress );
+            REQUIRE( tCheckdStrain );
+
+        }
+
+        // ------------------------------------------------------------------------------------- //
+        // ------------------------------------------------------------------------------------- //
+        TEST_CASE( "CM_Diff_Lin_Iso_Quadratic", "[moris],[fem],[CM_Diff_Lin_Iso_Quadratic]" )
+        {
+            //create a quad4 space element
+            Matrix< DDRMat > tXHat = {
+                    { 0.0, 0.0},
+                    { 1.0, 0.0},
+                    { 1.0, 1.0},
+                    { 0.0, 1.0},
+                    { 0.5, 0.0},
+                    { 1.0, 0.5},
+                    { 0.5, 1.0},
+                    { 0.0, 0.5},
+                    { 0.5, 0.5}};
+
+            //create a line time element
+            Matrix< DDRMat > tTHat( 3, 1 );
+            tTHat( 0 ) = 1.00e-3;
+            tTHat( 2 ) = 1.05e-3;
+            tTHat( 1 ) = 1.10e-3;
+
+            //create a space geometry interpolation rule
+            Interpolation_Rule tGeomInterpRule( mtk::Geometry_Type::QUAD,
+                    Interpolation_Type::LAGRANGE,
+                    mtk::Interpolation_Order::QUADRATIC,
+                    Interpolation_Type::LAGRANGE,
+                    mtk::Interpolation_Order::QUADRATIC );
+
+            // create an interpolation rule
+            Interpolation_Rule tIPRule (
+                    mtk::Geometry_Type::QUAD,
+                    Interpolation_Type::LAGRANGE,
+                    mtk::Interpolation_Order::QUADRATIC,
+                    Interpolation_Type::LAGRANGE,
+                    mtk::Interpolation_Order::QUADRATIC );
+
+
+            // set coefficients for field interpolators
+            Matrix< DDRMat > tUHat0 = {
+                    {4.1},{4.2},{4.3},{4.4},{4.5},{4.6},{4.7},{4.8},{4.9},
+                    {5.1},{5.2},{5.3},{5.4},{5.5},{5.6},{5.7},{5.8},{5.9},
+                    {6.1},{6.2},{6.3},{6.4},{6.5},{6.6},{6.7},{6.8},{6.9},};
+            Matrix< DDRMat > tParametricPoint = {{-0.4}, { 0.1}, {-0.6}};
+
+            // run test
+            moris::Cell<bool> tChecks = test_diffusion_constitutive_model(
+                            tXHat,
+                            tTHat,
+                            tGeomInterpRule,
+                            tIPRule,
+                            tUHat0,
+                            tParametricPoint);
+
+            // checks
+            bool tCheckdStress = tChecks(0);
+            bool tCheckdStrain = tChecks(1);
+            REQUIRE( tCheckdStress );
+            REQUIRE( tCheckdStrain );
+
+
+        }
+
+        // ------------------------------------------------------------------------------------- //
+        // ------------------------------------------------------------------------------------- //
+        TEST_CASE( "CM_Diff_Lin_Iso_Cubic", "[moris],[fem],[CM_Diff_Lin_Iso_Cubic]" )
+        {
+
+            //create a quad4 space element
+            Matrix< DDRMat > tXHat = {
+                    { 0.0, 0.0},
+                    { 3.0, 0.0},
+                    { 3.0, 3.0},
+                    { 0.0, 3.0},
+                    { 1.0, 0.0},
+                    { 2.0, 0.0},
+                    { 3.0, 1.0},
+                    { 3.0, 2.0},
+                    { 2.0, 3.0},
+                    { 1.0, 3.0},
+                    { 0.0, 2.0},
+                    { 0.0, 1.0},
+                    { 1.0, 1.0},
+                    { 2.0, 1.0},
+                    { 2.0, 2.0},
+                    { 1.0, 2.0}};
+
+            //create a line time element
+            Matrix< DDRMat > tTHat( 4, 1 );
+            tTHat( 0 ) = 1.00e-3;
+            tTHat( 2 ) = 1.05e-3;
+            tTHat( 3 ) = 1.10e-3;
+            tTHat( 1 ) = 1.15e-3;
+
+            //create a space geometry interpolation rule
+            Interpolation_Rule tGeomInterpRule( mtk::Geometry_Type::QUAD,
+                    Interpolation_Type::LAGRANGE,
+                    mtk::Interpolation_Order::CUBIC,
+                    Interpolation_Type::LAGRANGE,
+                    mtk::Interpolation_Order::CUBIC );
+
+            // create an interpolation rule
+            Interpolation_Rule tIPRule (
+                    mtk::Geometry_Type::QUAD,
+                    Interpolation_Type::LAGRANGE,
+                    mtk::Interpolation_Order::CUBIC,
+                    Interpolation_Type::LAGRANGE,
+                    mtk::Interpolation_Order::CUBIC );
+
+
+            // set coefficients for field interpolators
+            Matrix< DDRMat > tUHat0 = {
+                    {4.1},{4.2},{4.3},{4.4},{4.5},{4.6},{4.7},{4.8},{4.9},{4.1},{4.2},{4.3},{4.4},{4.5},{4.6},{4.7},
+                    {5.1},{5.2},{5.3},{5.4},{5.5},{5.6},{5.7},{5.8},{5.9},{5.1},{5.2},{5.3},{5.4},{5.5},{5.6},{5.7},
+                    {6.1},{6.2},{6.3},{6.4},{6.5},{6.6},{6.7},{6.8},{6.9},{6.1},{6.2},{6.3},{6.4},{6.5},{6.6},{6.7},
+                    {7.1},{7.2},{7.3},{7.4},{7.5},{7.6},{7.7},{7.8},{7.9},{7.1},{7.2},{7.3},{7.4},{7.5},{7.6},{7.7},};
+            Matrix< DDRMat > tParametricPoint = {{-0.4}, { 0.1}, {-0.6}};
+
+            // run test
+            moris::Cell<bool> tChecks = test_diffusion_constitutive_model(
+                            tXHat,
+                            tTHat,
+                            tGeomInterpRule,
+                            tIPRule,
+                            tUHat0,
+                            tParametricPoint);
+
+            // checks
+            bool tCheckdStress = tChecks(0);
+            bool tCheckdStrain = tChecks(1);
+            REQUIRE( tCheckdStress );
+            REQUIRE( tCheckdStrain );
+        }
 
 
     }/* namespace fem */
