ee6cc123f90590ca76866286eb70b8aa54de0901
wunsch <wunsch@tomcat>
2020-05-22 19:04:53 -0600

    Added modified GGLS parameter and its DoF derivative for phase change.

--- projects/FEM/INT/src/fn_FEM_CM_Phase_State_Functions.hpp
+++ projects/FEM/INT/src/fn_FEM_CM_Phase_State_Functions.hpp
@@ -87,77 +87,75 @@ namespace moris
             return tdfdT;
         }
 
-//        //------------------------------------------------------------------------------
-//        // 2nd derivative of phase state function wrt temperature
-//        moris::real eval_d2FdTemp2(
-//                moris::real aTmelt,
-//                moris::real aPCconst,
-//                moris::uint aPCfunc,
-//                Field_Interpolator * aFieldInterpolator)
-//        {
-//            // get temperature
-//            real tTemp = aFieldInterpolator->val()( 0 );
-//
-//            moris::real td2fdT2;
-//
-//            switch ( aPCfunc )
-//            {
-//                // linear phase change model
-//                case 1:
-//                {
-//                    td2fdT2 = 0;
-//                    break;
-//                }
-//
-//                // cubic phase change model
-//                case 2:
-//                {
-//                    real tTupper = (aTmelt + aPCconst/2);
-//                    real tTlower = (aTmelt - aPCconst/2);
-//
-//                    // cubic function: f(T)=( (tTemp - tTlower)^2 * (2*tTemp + tTlower - 3*tTupper))/(tTlower - tTupper)^3
-//                    if ( (tTemp > tTupper) || (tTemp < tTlower) )
-//                        td2fdT2 = 0;
-//                    else
-//                        td2fdT2 = ( 12.0 * tTemp + 6.0 * (tTlower + tTupper) )  / std::pow(aPCconst, 3.0);
-//                    break;
-//                }
-//
-//                // logistic function
-//                case 3:
-//                {
-//                    // logistic function parameter k
-//                    //  real tLogisticParam = ( 2 * std::log(1/aPCconst - 1) ) / ( aTupper - 3 * aTlower );
-//                    real tLogisticParam = aPCconst;
-//
-//                    real tExp = tLogisticParam * ( tTemp - aTmelt );
-//                    td2fdT2 = std::pow(tLogisticParam, 2) * (
-//                            ( std::exp(-tExp) - 1 ) / ( std::exp(tExp) + 3 + 3*std::exp(-tExp) + std::exp(- 2.0 * tExp) )    );
-//                    break;
-//                }
-//                default:
-//                    MORIS_ERROR(false,"wrong option for phase change function.");
-//            }
-//
-//            return td2fdT2;
-//        }
-//
-//        //------------------------------------------------------------------------------
-//        // derivative of the phase state function wrt temperature wrt the DoFs
-//        moris::Matrix<DDRMat> eval_dFdTempdDOF(
-//                moris::real aTmelt,
-//                moris::real aPCconst,
-//                moris::uint aPCfunc,
-//                Field_Interpolator * aFieldInterpolator)
-//        {
-//            // get second derivative of phase state function
-//            moris::real td2fdT2 = eval_d2FdTemp2( aTmelt, aPCconst, aPCfunc, aFieldInterpolator);
-//
-//            // multiply with shape functions
-//            return aFieldInterpolator->N() * td2fdT2;
-//        }
+        //------------------------------------------------------------------------------
+        // 2nd derivative of phase state function wrt temperature
+        moris::real eval_d2FdTemp2(
+                moris::real aTmelt,
+                moris::real aPCconst,
+                moris::uint aPCfunc,
+                Field_Interpolator * aFieldInterpolator)
+        {
+            // get temperature
+            real tTemp = aFieldInterpolator->val()( 0 );
 
+            moris::real td2fdT2;
 
+            switch ( aPCfunc )
+            {
+                // linear phase change model
+                case 1:
+                {
+                    td2fdT2 = 0;
+                    break;
+                }
+
+                // cubic phase change model
+                case 2:
+                {
+                    real tTupper = (aTmelt + aPCconst/2);
+                    real tTlower = (aTmelt - aPCconst/2);
+
+                    // cubic function: f(T)=( (tTemp - tTlower)^2 * (2*tTemp + tTlower - 3*tTupper))/(tTlower - tTupper)^3
+                    if ( (tTemp > tTupper) || (tTemp < tTlower) )
+                        td2fdT2 = 0;
+                    else
+                        td2fdT2 = ( 12.0 * tTemp + 6.0 * (tTlower + tTupper) )  / std::pow(aPCconst, 3.0);
+                    break;
+                }
+
+                // logistic function
+                case 3:
+                {
+                    // logistic function parameter k
+                    //  real tLogisticParam = ( 2 * std::log(1/aPCconst - 1) ) / ( aTupper - 3 * aTlower );
+                    real tLogisticParam = aPCconst;
+
+                    real tExp = tLogisticParam * ( tTemp - aTmelt );
+                    td2fdT2 = std::pow(tLogisticParam, 2) * (
+                            ( std::exp(-tExp) - 1 ) / ( std::exp(tExp) + 3 + 3*std::exp(-tExp) + std::exp(- 2.0 * tExp) )    );
+                    break;
+                }
+                default:
+                    MORIS_ERROR(false,"wrong option for phase change function.");
+            }
+
+            return td2fdT2;
+        }
+
+        //------------------------------------------------------------------------------
+        // derivative of the phase state function wrt temperature wrt the DoFs
+        moris::Matrix<DDRMat> eval_dFdTempdDOF(
+                moris::real aTmelt,
+                moris::real aPCconst,
+                moris::uint aPCfunc,
+                Field_Interpolator * aFieldInterpolator)
+        {
+            // get second derivative of phase state function
+            moris::real td2fdT2 = eval_d2FdTemp2( aTmelt, aPCconst, aPCfunc, aFieldInterpolator);
+
+            // multiply with shape functions
+            return aFieldInterpolator->N() * td2fdT2;
+        }
 
 
     } // namespace fem
