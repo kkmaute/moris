afd2de0c1bbc4c6b4291b75a60966e6d15f18857
Adam <adam.christopherson@colorado.edu>
2020-05-27 11:59:16 -0600

    Split Pdv Enums into header/definition and remove dummy file

--- projects/GEN/GEN_CORE/src/cl_GEN_Pdv_Enums.hpp
+++ projects/GEN/GEN_CORE/src/cl_GEN_Pdv_Enums.hpp
@@ -18,22 +18,6 @@ namespace moris
         UNDEFINED
     };
 
-    moris::map< std::string, PDV_Type > get_pdv_type_map()
-    {
-        moris::map< std::string, PDV_Type > tPdvTypeMap;
-
-        tPdvTypeMap["X_COORDINATE"]     = PDV_Type::X_COORDINATE;
-        tPdvTypeMap["Y_COORDINATE"]     = PDV_Type::Y_COORDINATE;
-        tPdvTypeMap["Z_COORDINATE"]     = PDV_Type::Z_COORDINATE;
-        tPdvTypeMap["DENSITY"]          = PDV_Type::DENSITY;
-        tPdvTypeMap["TEMPERATURE"]      = PDV_Type::TEMPERATURE;
-        tPdvTypeMap["ELASTIC_MODULUS"]  = PDV_Type::ELASTIC_MODULUS;
-        tPdvTypeMap["LS1"]              = PDV_Type::LS1;
-        tPdvTypeMap["LS2"]              = PDV_Type::LS2;
-        tPdvTypeMap[""]                 = PDV_Type::UNDEFINED;
-        return tPdvTypeMap;
-    }
-
     enum class Phase_Type
     {
         PHASE0,
@@ -42,7 +26,8 @@ namespace moris
         UNDEFINED,
         END_MAT_TYPE
     };
-}
 
+    moris::map< std::string, PDV_Type > get_pdv_type_map();
+}
 
 #endif /* MORIS_CL_PDV_ENUMS_HPP_ */
