acf75243fca797b90b49c60256d61780f87c15eb
wunsch <wunsch@tomcat>
2020-05-26 19:25:32 -0600

    testing of diffusion bulk terms added for higher order shape functions

--- projects/FEM/INT/test/UT_FEM_IWG_GGLS_Diffusion_Phase_Change.cpp
+++ projects/FEM/INT/test/UT_FEM_IWG_GGLS_Diffusion_Phase_Change.cpp
@@ -105,7 +105,7 @@ TEST_CASE( "IWG_GGLS_Diffusion_Phase_Change", "[moris],[fem],[IWG_GGLS_Diffusion
 
     // phase change constant
     std::shared_ptr< fem::Property > tPropMasterPCconst = std::make_shared< fem::Property >();
-    tPropMasterPCconst->set_parameters( {{{ 2.7 }}} );
+    tPropMasterPCconst->set_parameters( {{{ 2.8 }}} );
     //tPropMasterPCconst->set_dof_type_list( {{ MSI::Dof_Type::TEMP }} );
     tPropMasterPCconst->set_val_function( tConstValFunction_UTIWGGGLSDIFFBULK );
 
@@ -160,32 +160,50 @@ TEST_CASE( "IWG_GGLS_Diffusion_Phase_Change", "[moris],[fem],[IWG_GGLS_Diffusion
     // space and time geometry interpolators
     //------------------------------------------------------------------------------
     //create a space time interpolation rule
-    Interpolation_Rule tGIRule (
-            mtk::Geometry_Type::HEX,
-            Interpolation_Type::LAGRANGE,
-            mtk::Interpolation_Order::QUADRATIC,
-            Interpolation_Type::LAGRANGE,
-            mtk::Interpolation_Order::QUADRATIC );
+//    Interpolation_Rule tGIRule (
+//            mtk::Geometry_Type::HEX,
+//            Interpolation_Type::LAGRANGE,
+//            mtk::Interpolation_Order::QUADRATIC,
+//            Interpolation_Type::LAGRANGE,
+//            mtk::Interpolation_Order::QUADRATIC );
+
+    // create a space geometry interpolation rule
+    Interpolation_Rule tGIRule( mtk::Geometry_Type::HEX,
+                                Interpolation_Type::LAGRANGE,
+                                mtk::Interpolation_Order::LINEAR,
+                                Interpolation_Type::LAGRANGE,
+                                mtk::Interpolation_Order::LINEAR );
 
     // create a space time geometry interpolator
     Geometry_Interpolator tGI( tGIRule );
 
-    //create a quad4 space element
-    Matrix< DDRMat > tXHat = {
-            { 0.0, 0.0, 0.0}, { 1.0, 0.0, 0.0}, { 1.0, 1.0, 0.0}, { 0.0, 1.0, 0.0},
-            { 0.0, 0.0, 1.0}, { 1.0, 0.0, 1.0}, { 1.0, 1.0, 1.0}, { 0.0, 1.0, 1.0},
-            { 0.5, 0.0, 0.0}, { 1.0, 0.5, 0.0}, { 0.5, 1.0, 0.0}, { 0.0, 0.5, 0.0},
-            { 0.0, 0.0, 0.5}, { 1.0, 0.0, 0.5}, { 1.0, 1.0, 0.5}, { 0.0, 1.0, 0.5},
-            { 0.5, 0.0, 1.0}, { 1.0, 0.5, 1.0}, { 0.5, 1.0, 1.0}, { 0.0, 0.5, 1.0},
-            { 0.5, 0.5, 0.5}, { 0.5, 0.5, 0.0}, { 0.5, 0.5, 1.0},
-            { 0.5, 0.0, 0.5}, { 1.0, 0.5, 0.5}, { 0.5, 1.0, 0.5}, { 0.0, 0.5, 0.5}};
+    // create space coeff xHat
+    Matrix< DDRMat > tXHat = {{ 0.0, 0.0, 0.0 },
+                              { 1.0, 0.0, 0.0 },
+                              { 1.0, 1.0, 0.0 },
+                              { 0.0, 1.0, 0.0 },
+                              { 0.0, 0.0, 1.0 },
+                              { 1.0, 0.0, 1.0 },
+                              { 1.0, 1.0, 1.0 },
+                              { 0.0, 1.0, 1.0 }};
+
+//    //create a quad4 space element
+//    Matrix< DDRMat > tXHat = {
+//            { 0.0, 0.0, 0.0}, { 1.0, 0.0, 0.0}, { 1.0, 1.0, 0.0}, { 0.0, 1.0, 0.0},
+//            { 0.0, 0.0, 1.0}, { 1.0, 0.0, 1.0}, { 1.0, 1.0, 1.0}, { 0.0, 1.0, 1.0},
+//            { 0.5, 0.0, 0.0}, { 1.0, 0.5, 0.0}, { 0.5, 1.0, 0.0}, { 0.0, 0.5, 0.0},
+//            { 0.0, 0.0, 0.5}, { 1.0, 0.0, 0.5}, { 1.0, 1.0, 0.5}, { 0.0, 1.0, 0.5},
+//            { 0.5, 0.0, 1.0}, { 1.0, 0.5, 1.0}, { 0.5, 1.0, 1.0}, { 0.0, 0.5, 1.0},
+//            { 0.5, 0.5, 0.5}, { 0.5, 0.5, 0.0}, { 0.5, 0.5, 1.0},
+//            { 0.5, 0.0, 0.5}, { 1.0, 0.5, 0.5}, { 0.5, 1.0, 0.5}, { 0.0, 0.5, 0.5}};
 
 
     // create time coeff tHat
-    Matrix< DDRMat > tTHat( 3, 1 );
-    tTHat( 0 ) = 1.00e-3;
-    tTHat( 2 ) = 1.05e-3;
-    tTHat( 1 ) = 1.10e-3;
+//    Matrix< DDRMat > tTHat( 3, 1 );
+//    tTHat( 0 ) = 1.00e-3;
+//    tTHat( 2 ) = 1.05e-3;
+//    tTHat( 1 ) = 1.10e-3;
+    Matrix< DDRMat > tTHat = {{ 1.00e-3 }, { 1.10e-3 }};
 
 
     // set the coefficients xHat, tHat
@@ -197,19 +215,30 @@ TEST_CASE( "IWG_GGLS_Diffusion_Phase_Change", "[moris],[fem],[IWG_GGLS_Diffusion
     // field interpolators
     //------------------------------------------------------------------------------
     //create a space time interpolation rule
-    Interpolation_Rule tFIRule (
-            mtk::Geometry_Type::HEX,
-            Interpolation_Type::LAGRANGE,
-            mtk::Interpolation_Order::QUADRATIC,
-            Interpolation_Type::LAGRANGE,
-            mtk::Interpolation_Order::QUADRATIC );
+//    Interpolation_Rule tFIRule (
+//            mtk::Geometry_Type::HEX,
+//            Interpolation_Type::LAGRANGE,
+//            mtk::Interpolation_Order::QUADRATIC,
+//            Interpolation_Type::LAGRANGE,
+//            mtk::Interpolation_Order::QUADRATIC );
 
+    //create a space time interpolation rule
+    Interpolation_Rule tFIRule ( mtk::Geometry_Type::HEX,
+                                 Interpolation_Type::LAGRANGE,
+                                 mtk::Interpolation_Order::LINEAR,
+                                 Interpolation_Type::LAGRANGE,
+                                 mtk::Interpolation_Order::LINEAR );
 
     // create random coefficients
-    Matrix< DDRMat > tDOFHat = {
-            {4.1},{4.2},{4.3},{4.4},{4.5},{4.6},{4.7},{4.4},{4.9},{4.1},{4.2},{4.3},{4.4},{4.5},{4.6},{4.7},{4.8},{4.1},{4.3},{4.2},{4.3},{4.4},{4.5},{4.6},{4.7},{4.8},{4.9},
-            {5.1},{5.2},{5.3},{5.3},{5.5},{5.6},{5.7},{5.8},{5.9},{5.3},{5.2},{5.3},{5.4},{5.5},{5.2},{5.7},{5.8},{5.9},{5.1},{5.4},{5.3},{5.6},{5.5},{5.9},{5.7},{5.8},{5.9},
-            {6.4},{6.2},{6.3},{6.4},{6.2},{6.6},{6.7},{6.1},{6.9},{6.1},{6.1},{6.3},{6.4},{6.9},{6.8},{6.7},{6.6},{6.5},{6.6},{6.2},{6.3},{6.4},{6.5},{6.6},{6.7},{6.8},{6.9}};
+        Matrix< DDRMat > tDOFHat = {
+            {3.1},{4.2},{4.3},{3.4},{4.5},{2.6},{5.7},{6.4},
+            {4.9},{3.1},{7.2},{5.3},{5.4},{4.5},{6.6},{7.7}};
+
+//    // create random coefficients
+//    Matrix< DDRMat > tDOFHat = {
+//            {4.1},{4.2},{4.3},{4.4},{4.5},{4.6},{4.7},{4.4},{4.9},{4.1},{4.2},{4.3},{4.4},{4.5},{4.6},{4.7},{4.8},{4.1},{4.3},{4.2},{4.3},{4.4},{4.5},{4.6},{4.7},{4.8},{4.9},
+//            {5.1},{5.2},{5.3},{5.3},{5.5},{5.6},{5.7},{5.8},{5.9},{5.3},{5.2},{5.3},{5.4},{5.5},{5.2},{5.7},{5.8},{5.9},{5.1},{5.4},{5.3},{5.6},{5.5},{5.9},{5.7},{5.8},{5.9},
+//            {6.4},{6.2},{6.3},{6.4},{6.2},{6.6},{6.7},{6.1},{6.9},{6.1},{6.1},{6.3},{6.4},{6.9},{6.8},{6.7},{6.6},{6.5},{6.6},{6.2},{6.3},{6.4},{6.5},{6.6},{6.7},{6.8},{6.9}};
 
 
     // create a cell of field interpolators for IWG
@@ -239,18 +268,31 @@ TEST_CASE( "IWG_GGLS_Diffusion_Phase_Change", "[moris],[fem],[IWG_GGLS_Diffusion
     tIWG->mSet->mMasterDofTypeMap.set_size( static_cast< int >(MSI::Dof_Type::END_ENUM) + 1, 1, -1 );
     tIWG->mSet->mMasterDofTypeMap( static_cast< int >( MSI::Dof_Type::TEMP ) ) = 0;
 
+//    // set size and fill the set residual assembly map
+//    tIWG->mSet->mResDofAssemblyMap.resize( 1 );
+//    tIWG->mSet->mResDofAssemblyMap( 0 ) = { { 0, 80 } };
+//
+//    // set size and fill the set jacobian assembly map
+//    tIWG->mSet->mJacDofAssemblyMap.resize( 1 );
+//    tIWG->mSet->mJacDofAssemblyMap( 0 ) = { { 0, 80 } };
+//
+//    // set size and init the set residual and jacobian
+//    tIWG->mSet->mResidual.resize( 1 );
+//    tIWG->mSet->mResidual( 0 ).set_size( 81, 1, 0.0 );
+//    tIWG->mSet->mJacobian.set_size( 81, 81, 0.0 );
+
     // set size and fill the set residual assembly map
     tIWG->mSet->mResDofAssemblyMap.resize( 1 );
-    tIWG->mSet->mResDofAssemblyMap( 0 ) = { { 0, 80 } };
+    tIWG->mSet->mResDofAssemblyMap( 0 ) = { { 0, 15 } };
 
     // set size and fill the set jacobian assembly map
     tIWG->mSet->mJacDofAssemblyMap.resize( 1 );
-    tIWG->mSet->mJacDofAssemblyMap( 0 ) = { { 0, 80 } };
+    tIWG->mSet->mJacDofAssemblyMap( 0 ) = { { 0, 15 } };
 
     // set size and init the set residual and jacobian
     tIWG->mSet->mResidual.resize( 1 );
-    tIWG->mSet->mResidual( 0 ).set_size( 81, 1, 0.0 );
-    tIWG->mSet->mJacobian.set_size( 81, 81, 0.0 );
+    tIWG->mSet->mResidual( 0 ).set_size( 16, 1, 0.0 );
+    tIWG->mSet->mJacobian.set_size( 16, 16, 0.0 );
 
     // build global dof type list
     tIWG->get_global_dof_type_list();
@@ -291,8 +333,9 @@ TEST_CASE( "IWG_GGLS_Diffusion_Phase_Change", "[moris],[fem],[IWG_GGLS_Diffusion
     // require check is true
     REQUIRE( tCheckJacobian );
 
-//    print( tJacobians( 0 ),   "tJacobians" );
-//    print( tJacobiansFD( 0 ), "tJacobiansFD" );
+    // debug
+    //print( tJacobian,   "tJacobian" );
+    //print( tJacobianFD, "tJacobianFD" );
 
 }/*END_TEST_CASE*/
 
