867e0fad78819174401c23a6650f2df50c9154f4
lisenoel <lino8536@colorado.edu>
2020-06-23 12:22:21 +0200

    New function for getting data for FD scheme

---
+++ projects/FEM/INT/src/fn_FEM_FD_Scheme.hpp
@@ -0,0 +1,96 @@
+/*
+ * fn_FEM_FD_Scheme.hpp
+ *
+ *  Created on: Jun 18, 2020
+ *      Author: noel
+ */
+
+#ifndef SRC_FEM_FN_FEM_FD_SCHEME_HPP_
+#define SRC_FEM_FN_FEM_FD_SCHEME_HPP_
+
+#include "assert.hpp"
+#include "cl_Matrix.hpp"
+#include "cl_FEM_Enums.hpp"
+
+namespace moris
+{
+//------------------------------------------------------------------------------
+    namespace fem
+    {
+//------------------------------------------------------------------------------
+
+        void fd_scheme(
+                enum fem::FDScheme_Type              aFDSchemeType,
+                moris::Cell< moris::Cell< real > > & aFDScheme )
+        {
+            // set size for aFDScheme
+            aFDScheme.resize( 3 );
+
+            // switch on geometry type
+            switch( aFDSchemeType )
+            {
+                case fem::FDScheme_Type::POINT_1_FORWARD :
+                {
+                    // fill points
+                    aFDScheme( 0 ) = { { 0.0 }, { 1.0 } };
+
+                    // fill coeffs
+                    aFDScheme( 1 ) = { { -1.0 }, { 1.0 } };
+
+                    // fill deno
+                    aFDScheme( 2 ) = { 1.0 };
+
+                    break;
+                }
+                case fem::FDScheme_Type::POINT_1_BACKWARD :
+                {
+                    // fill points
+                    aFDScheme( 0 ) = { { -1.0 }, { 0.0 } };
+
+                    // fill coeffs
+                    aFDScheme( 1 ) = { { -1.0 }, { 1.0 } };
+
+                    // fill deno
+                    aFDScheme( 2 ) = { 1.0 };
+
+                    break;
+                }
+                case fem::FDScheme_Type::POINT_3_CENTRAL :
+                {
+                    // fill points
+                    aFDScheme( 0 ) = { { -1.0 }, { 1.0 } };
+
+                    // fill coeffs
+                    aFDScheme( 1 ) = { { -1.0 }, { 1.0 } };
+
+                    // fill deno
+                    aFDScheme( 2 ) = { 2.0 };
+
+                    break;
+                }
+                case fem::FDScheme_Type::POINT_5 :
+                {
+                    // fill points
+                    aFDScheme( 0 ) = { { -2.0 }, { -1.0 }, { 1.0 }, { 2.0 } };
+
+                    // fill coeffs
+                    aFDScheme( 1 ) = { { 1.0 }, { -8.0 }, { 8.0 }, { -1.0 } };
+
+                    // fill deno
+                    aFDScheme( 2 ) = { 12.0 };
+
+                    break;
+                }
+                default:
+                {
+                    MORIS_ERROR( false, " fd_scheme - unknown fd scheme type ");
+                    break;
+                }
+            }
+        }
+
+//------------------------------------------------------------------------------
+    } /* namespace fem */
+} /* namespace moris */
+
+#endif /* SRC_FEM_FN_FEM_FD_SCHEME_HPP_ */
