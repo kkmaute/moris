3d9e59430cc6960a93cc400adf3a46eea50fec12
Adam <adam.christopherson@colorado.edu>
2020-07-01 12:16:19 -0600

    Epsilon in test made less strict for checks

--- projects/FEM/INT/test/UT_FEM_CM_Diff_Lin_Iso.cpp
+++ projects/FEM/INT/test/UT_FEM_CM_Diff_Lin_Iso.cpp
@@ -53,7 +53,7 @@ namespace moris
                 uint aSpatialDim = 2)
         {
             // real for check
-            real tEpsilonRel = 1.0E-6;
+            real tEpsilonRel = 2.0E-6;
 
             // initialize cell of checks
             moris::Cell<bool> tChecks( 6, false );
