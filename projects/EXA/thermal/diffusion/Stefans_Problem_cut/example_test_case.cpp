0ae2e463a5cc8ce922bcc0ff6d2910fc10bbb088
wunsch <wunsch@tomcat>
2020-07-08 22:40:17 -0600

    Examples for thermal diffusion added

---
+++ projects/EXA/thermal/diffusion/Stefans_Problem_cut/example_test_case.cpp
@@ -0,0 +1,31 @@
+//
+// example specific interface to moris
+//
+
+#include <catch.hpp>
+
+#include "cl_Logger.hpp" // MRS/IOS/src
+
+//---------------------------------------------------------------
+
+int fn_WRK_Workflow_Main_Interface( int argc, char * argv[] );
+
+//---------------------------------------------------------------
+
+TEST_CASE("Comsol_cut_mesh",
+        "[moris],[example],[thermal],[diffusion]")
+{
+    // define command line call
+    int argc = 2;
+
+    char tString1[] = "";
+    char tString2[] = "./bin/Comsol_cut_mesh.so";
+
+    char * argv[2] = {tString1,tString2};
+
+    // call to performance manager main interface
+    int tRet = fn_WRK_Workflow_Main_Interface( argc, argv );
+
+    // catch test statements should follow
+    REQUIRE( tRet ==  0 );
+}
