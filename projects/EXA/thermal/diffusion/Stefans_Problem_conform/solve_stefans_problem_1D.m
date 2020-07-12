c570cc93d74c400b986461c5bf2bab22d323239a
wunsch <wunsch@tomcat>
2020-07-12 15:28:44 -0600

    matlab code for analytical solution to Stefan's problem

---
+++ projects/EXA/thermal/diffusion/Stefans_Problem_conform/solve_stefans_problem_1D.m
@@ -0,0 +1,42 @@
+function [T] = solve_stefans_problem_1D(x, t, Tw, MatParams)
+%SOLVE_STEFANS_PROBLEM_1D Solves for the temperatur the 1D phase change 
+%Stefan's problem at a point x at time t with wall temperature Tw for a 
+%given material with parameters MatParams
+
+%% Solve
+
+% get alpha
+alpha = MatParams.k / ( MatParams.rho * MatParams.cp );
+
+% get eta
+eta = x / ( 2 * sqrt(alpha*t) );
+
+% compute erf(eta)
+erfEta = erf(eta);
+
+% compute Stefan's number
+Ste = ( MatParams.cp / MatParams.Lh ) * (Tw - MatParams.Tm);
+
+% solve for beta
+beta_eqn = @(beta) ( beta * exp(beta^2) * erf(beta) - 0.5 * Ste );
+beta = fzero( beta_eqn, 1.0 );
+
+% compute erf(beta)
+erfBeta = erf(beta);
+
+% get position of melting front
+delta = 2 * eta * sqrt(alpha * t);
+
+% debug
+% fprintf( 'Melting Front at: %f \n x = %f, t = %f \n', delta, x, t);
+
+% compute result  
+T = Tw + (MatParams.Tm - Tw) * (erfEta/erfBeta);
+
+% clean results behind melting front
+if (T < MatParams.Tm)
+    T = MatParams.Tm;
+end  
+
+end
+
