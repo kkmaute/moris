e40dc91a02a0912aecdda84fd01b3aebc6d817cd
wunsch <wunsch@tomcat>
2020-08-10 11:56:46 -0600

    Momentum conservation IWG for compressible NS

---
+++ projects/FEM/INT/src/cl_FEM_IWG_Compressible_NS_Velocity_Bulk.hpp
@@ -0,0 +1,182 @@
+/*
+ * cl_FEM_IWG_Compressible_NS_Velocity_Bulk.hpp
+ *
+ *  Created on: Jul 22, 2020
+ *      Author: wunsch
+ */
+
+#ifndef SRC_FEM_CL_FEM_IWG_COMPRESSIBLE_NS_VELOCITY_BULK_HPP_
+#define SRC_FEM_CL_FEM_IWG_COMPRESSIBLE_NS_VELOCITY_BULK_HPP_
+
+#include <map>
+#include "typedefs.hpp"                     //MRS/COR/src
+#include "cl_Cell.hpp"                      //MRS/CON/src
+
+#include "cl_Matrix.hpp"                    //LINALG/src
+#include "linalg_typedefs.hpp"              //LINALG/src
+
+#include "cl_FEM_IWG.hpp"                   //FEM/INT/src
+
+namespace moris
+{
+    namespace fem
+    {
+        //------------------------------------------------------------------------------
+
+        class IWG_Compressible_NS_Velocity_Bulk : public IWG
+        {
+                //------------------------------------------------------------------------------
+
+            private:
+
+                // default dof types
+                MSI::Dof_Type mDofDensity     = MSI::Dof_Type::RHO;
+                MSI::Dof_Type mDofVelocity    = MSI::Dof_Type::VX;
+                MSI::Dof_Type mDofTemperature = MSI::Dof_Type::TEMP;
+
+                // multiplication matrix for double dot product based on number of dimensions
+                Matrix< DDRMat > mMultipMat;
+
+                //------------------------------------------------------------------------------
+
+            public:
+
+                // local property enums
+                enum class IWG_Property_Type
+                {
+                    BODY_FORCE,
+                    MAX_ENUM
+                };
+
+                // local string to property enum map
+                std::map< std::string, IWG_Property_Type > mPropertyMap;
+
+                // local constitutive enums
+                enum class IWG_Constitutive_Type
+                {
+                    FLUID,
+                    MAX_ENUM
+                };
+
+                // local string to constitutive enum map
+                std::map< std::string, IWG_Constitutive_Type > mConstitutiveMap;
+
+                //------------------------------------------------------------------------------
+                /*
+                 *  constructor
+                 */
+                IWG_Compressible_NS_Velocity_Bulk();
+
+                //------------------------------------------------------------------------------
+                /**
+                 * trivial destructor
+                 */
+                ~IWG_Compressible_NS_Velocity_Bulk(){};
+
+                //------------------------------------------------------------------------------
+                /**
+                 * set property
+                 * @param[ in ] aProperty       a property pointer
+                 * @param[ in ] aPropertyString a string defining the property
+                 * @param[ in ] aIsMaster       an enum for master or slave
+                 */
+                void set_property(
+                        std::shared_ptr< Property > aProperty,
+                        std::string                 aPropertyString,
+                        mtk::Master_Slave           aIsMaster = mtk::Master_Slave::MASTER );
+
+                //------------------------------------------------------------------------------
+                /**
+                 * set constitutive model
+                 * @param[ in ] aConstitutiveModel  a constitutive model pointer
+                 * @param[ in ] aConstitutiveString a string defining the constitutive model
+                 * @param[ in ] aIsMaster           an enum for master or slave
+                 */
+                void set_constitutive_model(
+                        std::shared_ptr< Constitutive_Model > aConstitutiveModel,
+                        std::string                           aConstitutiveString,
+                        mtk::Master_Slave                     aIsMaster = mtk::Master_Slave::MASTER );
+
+                //------------------------------------------------------------------------------
+                /**
+                 * set stabilization parameter
+                 * @param[ in ] aStabilizationParameter a stabilization parameter pointer
+                 * @param[ in ] aStabilizationString    a string defining the stabilization parameter
+                 */
+                void set_stabilization_parameter(
+                        std::shared_ptr< Stabilization_Parameter > aStabilizationParameter,
+                        std::string                                aStabilizationString );
+
+                //------------------------------------------------------------------------------
+                /**
+                 * compute the residual
+                 * @param[ in ] aWStar weight associated to the evaluation point
+                 */
+                void compute_residual( real aWStar );
+
+                //------------------------------------------------------------------------------
+                /**
+                 * compute the jacobian
+                 * @param[ in ] aWStar weight associated to the evaluation point
+                 */
+                void compute_jacobian( real aWStar );
+
+                //------------------------------------------------------------------------------
+                /**
+                 * compute the residual and the jacobian
+                 * @param[ in ] aWStar weight associated to the evaluation point
+                 */
+                void compute_jacobian_and_residual( real aWStar );
+
+                //------------------------------------------------------------------------------
+                /**
+                 * compute the derivative of the residual wrt design variables
+                 * @param[ in ] aWStar weight associated to the evaluation point
+                 */
+                void compute_dRdp( real aWStar );
+
+            private:
+                //------------------------------------------------------------------------------
+                /**
+                 * compute the residual strong form
+                 * @param[ in ] aRM a matrix to fill with RM
+                 * @param[ in ] aRC a matrix to fill with RC
+                 */
+                void compute_residual_strong_form(
+                        Matrix< DDRMat > & aRM,
+                        real             & aRC );
+
+                //------------------------------------------------------------------------------
+                /**
+                 * compute the residual strong form
+                 * @param[ in ] aDofTypes a list of dof type wrt which
+                 *                        the derivative is requested
+                 * @param[ in ] aJM       a matrix to fill with dRMdDof
+                 * @param[ in ] aJC       a matrix to fill with dRCdDof
+                 */
+                void compute_jacobian_strong_form(
+                        moris::Cell< MSI::Dof_Type >   aDofTypes,
+                        Matrix< DDRMat >             & aJM,
+                        Matrix< DDRMat >             & aJC );
+
+                //------------------------------------------------------------------------------
+                /**
+                 * compute the term ui uj
+                 * @param[ in ] auiuj a matrix to fill with ui uj
+                 */
+                void compute_uiuj(Matrix< DDRMat > & auiuj);
+
+                //------------------------------------------------------------------------------
+                /**
+                 * computes the term d(ui uj)/duhat
+                 * @param[ in ] aduiujdDOF a matrix to fill with result
+                 */
+                void compute_duiujdDOF(Matrix< DDRMat > & aduiujdDOF);
+
+                //------------------------------------------------------------------------------
+        };
+        //------------------------------------------------------------------------------
+    } /* namespace fem */
+} /* namespace moris */
+
+#endif /* SRC_FEM_CL_FEM_IWG_COMPRESSIBLE_NS_VELOCITY_BULK_HPP_ */
