c78de7004f8a34e505a205ee23fd4965658084e6
lisenoel <lino8536@colorado.edu>
2020-08-21 19:41:24 +0200

    New integration rules for tet

--- projects/FEM/INT/src/cl_FEM_Integration_Coeffs_Tet_1.hpp
+++ projects/FEM/INT/src/cl_FEM_Integration_Coeffs_Tet_1.hpp
@@ -8,66 +8,79 @@
 #ifndef SRC_FEM_CL_FEM_INTEGRATION_COEFFS_TET_1_HPP_
 #define SRC_FEM_CL_FEM_INTEGRATION_COEFFS_TET_1_HPP_
 
+//MRS/COR/src
+#include "typedefs.hpp"
+//LINALG/src
+#include "cl_Matrix.hpp"
+#include "linalg_typedefs.hpp"
+//FEM/INT/src
+#include "cl_FEM_Enums.hpp"
 #include "cl_FEM_Integration_Coeffs.hpp"
-#include "typedefs.hpp" //MRS/COR/src
-#include "cl_Matrix.hpp" //LNA/src
-#include "linalg_typedefs.hpp" //LNA/src
-#include "cl_FEM_Enums.hpp" //FEM/INT/src
 
 namespace moris
 {
     namespace fem
     {
 
-//------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------
+
         template<>
         uint
         Integration_Coeffs<
-            Integration_Type::GAUSS,
-            Integration_Order::TET_1>::get_number_of_dimensions()
+        Integration_Type::GAUSS,
+        Integration_Order::TET_1>::get_number_of_dimensions()
         {
             return 4;
         }
-//------------------------------------------------------------------------------
+
+        //------------------------------------------------------------------------------
 
         template<>
         uint
         Integration_Coeffs<
-            Integration_Type::GAUSS,
-            Integration_Order::TET_1>::get_number_of_points()
-            {
-                return 1;
-            }
+        Integration_Type::GAUSS,
+        Integration_Order::TET_1>::get_number_of_points()
+        {
+            return 1;
+        }
 
-//------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------
 
         template<>
         void
         Integration_Coeffs<
-                Integration_Type::GAUSS,
-                Integration_Order::TET_1>::get_points( Matrix< DDRMat > & aIntegrationPoints )
+        Integration_Type::GAUSS,
+        Integration_Order::TET_1>::get_points( Matrix< DDRMat > & aIntegrationPoints )
         {
             aIntegrationPoints =
             {
-                {0.250000000000000},
-                {0.250000000000000},
-                {0.250000000000000},
-                {0.250000000000000}
+                    {
+                            0.250000000000000
+                    },
+                    {
+                            0.250000000000000
+                    },
+                    {
+                            0.250000000000000
+                    },
+                    {
+                            0.250000000000000
+                    }
             };
-          }
+        }
 
-//------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------
 
-            template<>
-            void
-            Integration_Coeffs<
-                Integration_Type::GAUSS,
-                Integration_Order::TET_1 >::get_weights( Matrix< DDRMat > & aIntegrationWeights )
-            {
-                aIntegrationWeights.set_size( 1, 1, 1.0 );
-            }
+        template<>
+        void
+        Integration_Coeffs<
+        Integration_Type::GAUSS,
+        Integration_Order::TET_1 >::get_weights( Matrix< DDRMat > & aIntegrationWeights )
+        {
+            aIntegrationWeights.set_size( 1, 1, 1.0 );
+        }
 
-//------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------
     } /* namespace fem */
 } /* namespace moris */
 #endif /* SRC_FEM_CL_FEM_INTEGRATION_COEFFS_TET_1_HPP_ */
