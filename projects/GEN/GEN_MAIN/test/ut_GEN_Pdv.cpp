e1ac2736af1434126c277bcad3c5436a806f687e
schmidt <schmidt@blueflame>
2020-09-21 23:08:22 -0600

    - GEN test update

--- projects/GEN/GEN_MAIN/test/ut_GEN_Pdv.cpp
+++ projects/GEN/GEN_MAIN/test/ut_GEN_Pdv.cpp
@@ -32,98 +32,101 @@ namespace moris
 
         TEST_CASE("Interpolation PDV creation test", "[gen], [pdv], [interpolation pdv]")
         {
-            // Create PDV_Type host manager
-            Pdv_Host_Manager tPdvHostManager;
-
-            tPdvHostManager.mPdvTypeList = {PDV_Type::DENSITY, PDV_Type::TEMPERATURE, PDV_Type::ELASTIC_MODULUS};
-            tPdvHostManager.mPdvTypeMap.set_size(10, 1, -1);
-            tPdvHostManager.mPdvTypeMap( 3 ) = 0;
-            tPdvHostManager.mPdvTypeMap( 4 ) = 1;
-            tPdvHostManager.mPdvTypeMap( 5 ) = 2;
-
-            // ----------------- Interpolation PDVs ---------------------- //
-            // Node indices per set
-            Cell<Matrix<DDSMat>> tIpNodeIndicesPerSet(2);
-            tIpNodeIndicesPerSet(0).resize(4, 1);
-            tIpNodeIndicesPerSet(1).resize(4, 1);
-            tIpNodeIndicesPerSet(0) = {{0, 1, 2, 3}};
-            tIpNodeIndicesPerSet(1) = {{2, 3, 4, 5}};
-
-            Cell<Matrix<DDSMat>> tIpNodeIdsPerSet(2);
-            tIpNodeIdsPerSet(0).resize(4, 1);
-            tIpNodeIdsPerSet(1).resize(4, 1);
-            tIpNodeIdsPerSet(0) = {{0, 1, 2, 3}};
-            tIpNodeIdsPerSet(1) = {{2, 3, 4, 5}};
-
-            Cell<Matrix<DDSMat>> tIpNodeOwnersPerSet(2);
-            tIpNodeOwnersPerSet(0).resize(4, 1);
-            tIpNodeOwnersPerSet(1).resize(4, 1);
-            tIpNodeOwnersPerSet(0) = {{0, 0, 0, 0}};
-            tIpNodeOwnersPerSet(1) = {{0, 0, 0, 0}};
-
-            // PDV_Type types per set
-            Cell<Cell<Cell<PDV_Type>>> tIpPdvTypes(2);
-            tIpPdvTypes(0).resize(2);
-            tIpPdvTypes(1).resize(2);
-            tIpPdvTypes(0)(0).resize(1);
-            tIpPdvTypes(0)(1).resize(1);
-            tIpPdvTypes(1)(0).resize(1);
-            tIpPdvTypes(1)(1).resize(1);
-            tIpPdvTypes(0)(0)(0) = PDV_Type::DENSITY;
-            tIpPdvTypes(0)(1)(0) = PDV_Type::TEMPERATURE;
-            tIpPdvTypes(1)(0)(0) = PDV_Type::TEMPERATURE;
-            tIpPdvTypes(1)(1)(0) = PDV_Type::ELASTIC_MODULUS;
-
-            // Create PDV_Type hosts
-            tPdvHostManager.create_interpolation_pdv_hosts(
-                    tIpNodeIndicesPerSet,
-                    tIpNodeIdsPerSet,
-                    tIpNodeOwnersPerSet,
-                    Cell<Matrix<F31RMat>>(6),
-                    tIpPdvTypes);
-
-            // Set PDVs
-            for (uint tMeshSetIndex = 0; tMeshSetIndex < 2; tMeshSetIndex++)
+            if( par_size() == 1)
             {
-                for (uint tNodeIndex = 0; tNodeIndex < 4; tNodeIndex++)
+                // Create PDV_Type host manager
+                Pdv_Host_Manager tPdvHostManager;
+
+                tPdvHostManager.mPdvTypeList = {PDV_Type::DENSITY, PDV_Type::TEMPERATURE, PDV_Type::ELASTIC_MODULUS};
+                tPdvHostManager.mPdvTypeMap.set_size(10, 1, -1);
+                tPdvHostManager.mPdvTypeMap( 3 ) = 0;
+                tPdvHostManager.mPdvTypeMap( 4 ) = 1;
+                tPdvHostManager.mPdvTypeMap( 5 ) = 2;
+
+                // ----------------- Interpolation PDVs ---------------------- //
+                // Node indices per set
+                Cell<Matrix<DDSMat>> tIpNodeIndicesPerSet(2);
+                tIpNodeIndicesPerSet(0).resize(4, 1);
+                tIpNodeIndicesPerSet(1).resize(4, 1);
+                tIpNodeIndicesPerSet(0) = {{0, 1, 2, 3}};
+                tIpNodeIndicesPerSet(1) = {{2, 3, 4, 5}};
+
+                Cell<Matrix<DDSMat>> tIpNodeIdsPerSet(2);
+                tIpNodeIdsPerSet(0).resize(4, 1);
+                tIpNodeIdsPerSet(1).resize(4, 1);
+                tIpNodeIdsPerSet(0) = {{0, 1, 2, 3}};
+                tIpNodeIdsPerSet(1) = {{2, 3, 4, 5}};
+
+                Cell<Matrix<DDSMat>> tIpNodeOwnersPerSet(2);
+                tIpNodeOwnersPerSet(0).resize(4, 1);
+                tIpNodeOwnersPerSet(1).resize(4, 1);
+                tIpNodeOwnersPerSet(0) = {{0, 0, 0, 0}};
+                tIpNodeOwnersPerSet(1) = {{0, 0, 0, 0}};
+
+                // PDV_Type types per set
+                Cell<Cell<Cell<PDV_Type>>> tIpPdvTypes(2);
+                tIpPdvTypes(0).resize(2);
+                tIpPdvTypes(1).resize(2);
+                tIpPdvTypes(0)(0).resize(1);
+                tIpPdvTypes(0)(1).resize(1);
+                tIpPdvTypes(1)(0).resize(1);
+                tIpPdvTypes(1)(1).resize(1);
+                tIpPdvTypes(0)(0)(0) = PDV_Type::DENSITY;
+                tIpPdvTypes(0)(1)(0) = PDV_Type::TEMPERATURE;
+                tIpPdvTypes(1)(0)(0) = PDV_Type::TEMPERATURE;
+                tIpPdvTypes(1)(1)(0) = PDV_Type::ELASTIC_MODULUS;
+
+                // Create PDV_Type hosts
+                tPdvHostManager.create_interpolation_pdv_hosts(
+                        tIpNodeIndicesPerSet,
+                        tIpNodeIdsPerSet,
+                        tIpNodeOwnersPerSet,
+                        Cell<Matrix<F31RMat>>(6),
+                        tIpPdvTypes);
+
+                // Set PDVs
+                for (uint tMeshSetIndex = 0; tMeshSetIndex < 2; tMeshSetIndex++)
+                {
+                    for (uint tNodeIndex = 0; tNodeIndex < 4; tNodeIndex++)
+                    {
+                        for (uint tPdvIndex = 0; tPdvIndex < 2; tPdvIndex++)
+                        {
+                            tPdvHostManager.create_interpolation_pdv(
+                                    (uint)tIpNodeIndicesPerSet(tMeshSetIndex)(tNodeIndex),
+                                    tIpPdvTypes(tMeshSetIndex)(tPdvIndex)(0),
+                                    (real)tMeshSetIndex);
+                        }
+                    }
+                }
+
+                tPdvHostManager.create_pdv_ids();
+
+                // Check PDVs
+                Cell<Matrix<DDRMat>> tPdvValues;
+                for (uint tMeshSetIndex = 0; tMeshSetIndex < 2; tMeshSetIndex++)
                 {
                     for (uint tPdvIndex = 0; tPdvIndex < 2; tPdvIndex++)
                     {
-                        tPdvHostManager.create_interpolation_pdv(
-                                (uint)tIpNodeIndicesPerSet(tMeshSetIndex)(tNodeIndex),
-                                tIpPdvTypes(tMeshSetIndex)(tPdvIndex)(0),
-                                (real)tMeshSetIndex);
+                        tPdvValues.clear();
+                        tPdvHostManager.get_ip_pdv_value(tIpNodeIndicesPerSet(tMeshSetIndex), tIpPdvTypes(tMeshSetIndex)(tPdvIndex), tPdvValues);
+
+                        for (uint tNodeIndex = 0; tNodeIndex < 4; tNodeIndex++)
+                        {
+                            CHECK(tPdvValues(0)(tNodeIndex) == tMeshSetIndex +
+                                    (tMeshSetIndex == 0) * (tNodeIndex > 1) *
+                                    (tIpPdvTypes(tMeshSetIndex)(tPdvIndex)(0) == PDV_Type::TEMPERATURE));
+                        }
                     }
                 }
-            }
 
-            tPdvHostManager.create_pdv_ids();
+                // ------------------- Check global map ----------------------- //
+                const Matrix<DDSMat> & tLocalGlobalMap = tPdvHostManager.get_my_local_global_map();
 
-            // Check PDVs
-            Cell<Matrix<DDRMat>> tPdvValues;
-            for (uint tMeshSetIndex = 0; tMeshSetIndex < 2; tMeshSetIndex++)
-            {
-                for (uint tPdvIndex = 0; tPdvIndex < 2; tPdvIndex++)
+                REQUIRE(tLocalGlobalMap.length() == 14);
+                for (int tGlobalPdvIndex = 0; tGlobalPdvIndex < 14; tGlobalPdvIndex++)
                 {
-                    tPdvValues.clear();
-                    tPdvHostManager.get_ip_pdv_value(tIpNodeIndicesPerSet(tMeshSetIndex), tIpPdvTypes(tMeshSetIndex)(tPdvIndex), tPdvValues);
-
-                    for (uint tNodeIndex = 0; tNodeIndex < 4; tNodeIndex++)
-                    {
-                        CHECK(tPdvValues(0)(tNodeIndex) == tMeshSetIndex +
-                                (tMeshSetIndex == 0) * (tNodeIndex > 1) *
-                                (tIpPdvTypes(tMeshSetIndex)(tPdvIndex)(0) == PDV_Type::TEMPERATURE));
-                    }
+                    CHECK(tLocalGlobalMap(tGlobalPdvIndex) == tGlobalPdvIndex);
                 }
-            }
-
-            // ------------------- Check global map ----------------------- //
-            const Matrix<DDSMat> & tLocalGlobalMap = tPdvHostManager.get_my_local_global_map();
-
-            REQUIRE(tLocalGlobalMap.length() == 14);
-            for (int tGlobalPdvIndex = 0; tGlobalPdvIndex < 14; tGlobalPdvIndex++)
-            {
-                CHECK(tLocalGlobalMap(tGlobalPdvIndex) == tGlobalPdvIndex);
             }
         }
 
