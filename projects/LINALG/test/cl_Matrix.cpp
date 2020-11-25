2b60ae0ae7a0bb80f5be96a99282bfb0227feba3
maute <maute@colorado.edu>
2020-11-24 23:15:58 -0700

    added checks for block operators

--- projects/LINALG/test/cl_Matrix.cpp
+++ projects/LINALG/test/cl_Matrix.cpp
@@ -202,28 +202,60 @@ namespace moris
             REQUIRE( moris::equal_to( tMatrix7(0,0)    , 44.0 ) );
             REQUIRE( moris::equal_to( tMatrix7Copy(0,0),  1.0 ) );
 
-
             // check sub-matrix functionality
+
+            // create test matrix
             Matrix< DDRMat > a( 3, 3 );
             a( 0, 0 ) = 1.0; a( 0, 1 ) = 2.0; a( 0, 2 ) = 3.0;
             a( 1, 0 ) = 4.0; a( 1, 1 ) = 5.0; a( 1, 2 ) = 6.0;
             a( 2, 0 ) = 9.0; a( 2, 1 ) = 8.0; a( 2, 2 ) = 7.0;
 
-            Matrix< DDRMat > aSubMatrix = a( {1, 2}, {1, 2} );
+            // create sub-matrix view
+            auto aSubMatrix = a( {1, 2}, {1, 2} );
             REQUIRE( moris::equal_to( aSubMatrix( 0, 0 ), 5.0 ) );
             REQUIRE( moris::equal_to( aSubMatrix( 0, 1 ), 6.0 ) );
             REQUIRE( moris::equal_to( aSubMatrix( 1, 0 ), 8.0 ) );
             REQUIRE( moris::equal_to( aSubMatrix( 1, 1 ), 7.0 ) );
 
-            Matrix< DDRMat > aSubVec1 = a( {0, 2} );
-            REQUIRE( moris::equal_to( aSubVec1( 0 ), 1.0 ) );
-            REQUIRE( moris::equal_to( aSubVec1( 1 ), 4.0 ) );
-            REQUIRE( moris::equal_to( aSubVec1( 2 ), 9.0 ) );
+            // create test sub-matrix
+            Matrix< DDRMat > as = { { 1, 2 },{3, 4} };
 
-            Matrix< DDRMat > aSubVec2 = a( {0, 2}, 2 );
-            REQUIRE( moris::equal_to( aSubVec2( 0 ), 3.0 ) );
-            REQUIRE( moris::equal_to( aSubVec2( 1 ), 6.0 ) );
-            REQUIRE( moris::equal_to( aSubVec2( 2 ), 7.0 ) );
+            // copy test sub-matrix onto sub-matrix view
+            aSubMatrix = as.matrix_data();
+            REQUIRE( moris::equal_to( a( 1, 1 ), 1.0 ) );
+            REQUIRE( moris::equal_to( a( 1, 2 ), 2.0 ) );
+            REQUIRE( moris::equal_to( a( 2, 1 ), 3.0 ) );
+            REQUIRE( moris::equal_to( a( 2, 2 ), 4.0 ) );
+
+            // copy sub-matrix view onto moris::matrix
+            Matrix< DDRMat > aSubMatrixCopy = aSubMatrix;
+            REQUIRE( moris::equal_to( aSubMatrixCopy( 0, 0 ), 1.0 ) );
+            REQUIRE( moris::equal_to( aSubMatrixCopy( 0, 1 ), 2.0 ) );
+            REQUIRE( moris::equal_to( aSubMatrixCopy( 1, 0 ), 3.0 ) );
+            REQUIRE( moris::equal_to( aSubMatrixCopy( 1, 1 ), 4.0 ) );
+
+            // Reset test matrix
+            a( 0, 0 ) = 1.0; a( 0, 1 ) = 2.0; a( 0, 2 ) = 3.0;
+            a( 1, 0 ) = 4.0; a( 1, 1 ) = 5.0; a( 1, 2 ) = 6.0;
+            a( 2, 0 ) = 9.0; a( 2, 1 ) = 8.0; a( 2, 2 ) = 7.0;
+
+            // create sub-matrix view of first column and check sub-matrix view
+            auto aSubVec = a( {0, 2} );
+            REQUIRE( moris::equal_to( aSubVec( 0 ), 1.0 ) );
+            REQUIRE( moris::equal_to( aSubVec( 1 ), 4.0 ) );
+            REQUIRE( moris::equal_to( aSubVec( 2 ), 9.0 ) );
+
+            // copy second column of test matrix on sub-matrix view and check test matrix
+            aSubVec = a( {0, 2}, 2 );
+            REQUIRE( moris::equal_to( a( 0, 0 ), 3.0 ) );
+            REQUIRE( moris::equal_to( a( 1, 0 ), 6.0 ) );
+            REQUIRE( moris::equal_to( a( 2, 0 ), 7.0 ) );
+
+            // create moris::mat from sub-view and check for correctness
+            Matrix< DDRMat > aSubVecCopy = aSubVec;
+            REQUIRE( moris::equal_to( aSubVecCopy( 0 ), 3.0 ) );
+            REQUIRE( moris::equal_to( aSubVecCopy( 1 ), 6.0 ) );
+            REQUIRE( moris::equal_to( aSubVecCopy( 2 ), 7.0 ) );
 
             //        // Index out of bounds
             //        REQUIRE_THROWS(tMatrix7(3,3) = 0);
@@ -241,7 +273,7 @@ namespace moris
         uint tDimX = rand() % 40 + 130;
         uint tDimY = rand() % 40 + 130;
 
-        uint tNumRepetitions = 10000+tDimX;
+        uint tNumRepetitions = 5000+tDimX;
 
         moris::Matrix<DDRMat> tMorisMat(tDimX,tDimY);
         arma::Mat<double>     tArmaMat(tDimX,tDimY);
@@ -379,6 +411,14 @@ namespace moris
 
         // time Moris Matrix involving transpose --------------------------------------------------
 
+        tNumRepetitions = 10000+tDimX;
+
+        // convert into square matrices
+        uint minDim = std::min( tMorisMat.n_rows(), tMorisMat.n_cols() );
+
+        tMorisMat.resize(minDim,minDim);
+        tResultMatMoris.resize(minDim,minDim);
+
         tTimeStamp = std::clock();
 
          tResultMatMoris = tMorisMat;
@@ -390,7 +430,7 @@ namespace moris
 
          real tTimeForSummationNoTransMoris = 1000 * (moris::real) ( clock() - tTimeStamp ) / CLOCKS_PER_SEC;
          MORIS_LOG_INFO( "Time to 'plus without transpose'         %i %i x %i MORIS-matrices: %5.2f milliseconds (result: %e).",
-                 tNumRepetitions, tDimX, tDimY, ( double ) tTimeForSummationNoTransMoris, norm(tResultMatMoris) );
+                 tNumRepetitions, minDim, minDim, ( double ) tTimeForSummationNoTransMoris, norm(tResultMatMoris) );
 
         tTimeStamp = std::clock();
 
@@ -402,10 +442,13 @@ namespace moris
         }
 
         real tTimeForSummationTransMoris = 1000 * (moris::real) ( clock() - tTimeStamp ) / CLOCKS_PER_SEC;
-        MORIS_LOG_INFO( "Time to 'plus with transpose'         %i %i x %i MORIS-matrices: %5.2f milliseconds (result: %e).",
-                tNumRepetitions, tDimX, tDimY, ( double ) tTimeForSummationTransMoris, norm(tResultMatMoris) );
+        MORIS_LOG_INFO( "Time to 'plus with transpose'            %i %i x %i MORIS-matrices: %5.2f milliseconds (result: %e).",
+                tNumRepetitions, minDim, minDim, ( double ) tTimeForSummationTransMoris, norm(tResultMatMoris) );
 
         // time Arma Matrix involving transpose --------------------------------------------------
+        tArmaMat.resize(minDim,minDim);
+        tResultMatArma.resize(minDim,minDim);
+
         tTimeStamp = std::clock();
 
         tResultMatArma = tArmaMat;
@@ -417,7 +460,7 @@ namespace moris
 
         real tTimeForSummationNoTransArma = 1000 * (moris::real) ( clock() - tTimeStamp ) / CLOCKS_PER_SEC;
         MORIS_LOG_INFO( "Time to 'plus without transpose'         %i %i x %i Arma-matrices: %5.2f milliseconds (result: %e).",
-                tNumRepetitions, tDimX, tDimY, ( double ) tTimeForSummationNoTransArma, norm(tResultMatArma) );
+                tNumRepetitions, minDim, minDim, ( double ) tTimeForSummationNoTransArma, norm(tResultMatArma) );
 
         tTimeStamp = std::clock();
 
@@ -429,9 +472,8 @@ namespace moris
         }
 
         real tTimeForSummationTransArma = 1000 * (moris::real) ( clock() - tTimeStamp ) / CLOCKS_PER_SEC;
-        MORIS_LOG_INFO( "Time to 'plus with transpose'         %i %i x %i Arma-matrices: %5.2f milliseconds (result: %e).",
-                tNumRepetitions, tDimX, tDimY, ( double ) tTimeForSummationTransArma, norm(tResultMatArma) );
-
+        MORIS_LOG_INFO( "Time to 'plus with transpose'            %i %i x %i Arma-matrices: %5.2f milliseconds (result: %e).",
+                tNumRepetitions, minDim, minDim, ( double ) tTimeForSummationTransArma, norm(tResultMatArma) );
     }
 
     TEST_CASE( "matrix_solve", "[moris],[matrix_solve]" )
