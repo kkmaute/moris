68998bf528f352a9e23b7ad0e0e9ba31289b739c
maute <maute@colorado.edu>
2020-11-28 23:06:17 -0700

    formating

--- projects/SOL/SOL_CORE/src/cl_SOL_Dist_Matrix.hpp
+++ projects/SOL/SOL_CORE/src/cl_SOL_Dist_Matrix.hpp
@@ -38,91 +38,97 @@ namespace moris
         class Dist_Matrix
         {
 
-        protected:
-            Epetra_FECrsMatrix   * mEpetraMat;
-            Dist_Map* mMap;
-            Mat                    mPETScMat;
+            protected:
+                Epetra_FECrsMatrix   * mEpetraMat;
+                Dist_Map* mMap;
+                Mat                    mPETScMat;
 
-        public:
-            Dist_Matrix( ) : mEpetraMat( NULL ),
-                             mMap( NULL ),
-                             mPETScMat( NULL )
-            {};
+            public:
 
-            Dist_Matrix( Dist_Map* aMap ) : mEpetraMat( NULL ),
-                    mMap( aMap ),
-                    mPETScMat( NULL )
-            {};
+                Dist_Matrix( )
+                : mEpetraMat( NULL ),
+                  mMap( NULL ),
+                  mPETScMat( NULL )
+                {};
 
-            virtual ~Dist_Matrix(){};
+                Dist_Matrix( Dist_Map* aMap ) : mEpetraMat( NULL ),
+                        mMap( aMap ),
+                        mPETScMat( NULL )
+                {};
 
-            virtual void fill_matrix(const moris::uint             & anumDofs,
-                    const moris::Matrix< DDRMat > & aA_val,
-                    const moris::Matrix< DDSMat > & aEleDofConectivity) = 0;
+                virtual ~Dist_Matrix(){};
 
-            /**
-             * Inserts values into the matrix at locations corresponding to the given row and column IDs.
-             *
-             * @param aRowIDs Row IDs
-             * @param aColumnIDs Column IDs
-             * @param aMatrixValues Values to be inserted
-             */
-            virtual void insert_values(
-                    const Matrix<DDSMat>& aRowIDs,
-                    const Matrix<DDSMat>& aColumnIDs,
-                    const Matrix<DDRMat>& aMatrixValues) = 0;
+                virtual void fill_matrix(
+                        const moris::uint             & anumDofs,
+                        const moris::Matrix< DDRMat > & aA_val,
+                        const moris::Matrix< DDSMat > & aEleDofConectivity) = 0;
 
-            /**
-             * Sums values into the matrix at locations corresponding to the given row and column IDs.
-             *
-             * @param aRowIDs Row IDs
-             * @param aColumnIDs Column IDs
-             * @param aMatrixValues Values to be summed into the existing matrix values
-             */
-            virtual void sum_into_values(
-                    const Matrix<DDSMat>& aRowIDs,
-                    const Matrix<DDSMat>& aColumnIDs,
-                    const Matrix<DDRMat>& aMatrixValues) = 0;
+                /**
+                 * Inserts values into the matrix at locations corresponding to the given row and column IDs.
+                 *
+                 * @param aRowIDs Row IDs
+                 * @param aColumnIDs Column IDs
+                 * @param aMatrixValues Values to be inserted
+                 */
+                virtual void insert_values(
+                        const Matrix<DDSMat>& aRowIDs,
+                        const Matrix<DDSMat>& aColumnIDs,
+                        const Matrix<DDRMat>& aMatrixValues) = 0;
 
-            virtual void get_matrix_values( const moris::Matrix< DDSMat > & aRequestedIds,
-                    moris::Matrix< DDRMat > & aValues ) = 0;
+                /**
+                 * Sums values into the matrix at locations corresponding to the given row and column IDs.
+                 *
+                 * @param aRowIDs Row IDs
+                 * @param aColumnIDs Column IDs
+                 * @param aMatrixValues Values to be summed into the existing matrix values
+                 */
+                virtual void sum_into_values(
+                        const Matrix<DDSMat>& aRowIDs,
+                        const Matrix<DDSMat>& aColumnIDs,
+                        const Matrix<DDRMat>& aMatrixValues) = 0;
 
-            virtual void matrix_global_assembly() = 0;
+                virtual void get_matrix_values(
+                        const moris::Matrix< DDSMat > & aRequestedIds,
+                        moris::Matrix< DDRMat >       & aValues ) = 0;
 
-            virtual void dirichlet_BC_vector(      moris::Matrix< DDUMat > & aDirichletBCVec,
-                    const moris::Matrix< DDUMat > & aMyConstraintDofs) = 0;
+                virtual void matrix_global_assembly() = 0;
 
-            virtual void build_graph(const moris::uint       & anumDofs,
-                    const moris::Matrix< DDSMat > & aEleDofConectivity) = 0;
+                virtual void dirichlet_BC_vector(
+                        moris::Matrix< DDUMat >       & aDirichletBCVec,
+                        const moris::Matrix< DDUMat > & aMyConstraintDofs) = 0;
 
-            virtual void get_diagonal( moris::sol::Dist_Vector & aDiagVec ) const = 0;
+                virtual void build_graph(
+                        const moris::uint             & anumDofs,
+                        const moris::Matrix< DDSMat > & aEleDofConectivity) = 0;
 
-            virtual void mat_put_scalar( const moris::real & aValue ) = 0;
+                virtual void get_diagonal( moris::sol::Dist_Vector & aDiagVec ) const = 0;
 
-            virtual void sparse_mat_left_scale( const moris::sol::Dist_Vector & aScaleVector ) = 0;
+                virtual void mat_put_scalar( const moris::real & aValue ) = 0;
 
-            virtual void sparse_mat_right_scale( const moris::sol::Dist_Vector & aScaleVector ) = 0;
+                virtual void sparse_mat_left_scale( const moris::sol::Dist_Vector & aScaleVector ) = 0;
 
-            virtual void replace_diagonal_values( const moris::sol::Dist_Vector & aDiagVec ) = 0;
+                virtual void sparse_mat_right_scale( const moris::sol::Dist_Vector & aScaleVector ) = 0;
 
-            virtual void mat_vec_product(
-                    const moris::sol::Dist_Vector & aInputVec,
-                          moris::sol::Dist_Vector & aResult,
-                    const bool                      aUseTranspose ) = 0;
+                virtual void replace_diagonal_values( const moris::sol::Dist_Vector & aDiagVec ) = 0;
 
-            virtual void print() const = 0;
+                virtual void mat_vec_product(
+                        const moris::sol::Dist_Vector & aInputVec,
+                        moris::sol::Dist_Vector & aResult,
+                        const bool                      aUseTranspose ) = 0;
 
-            virtual void save_matrix_to_matlab_file( const char* aFilename ) = 0;
+                virtual void print() const = 0;
 
-            virtual void save_matrix_to_matrix_market_file( const char* aFilename ) = 0;
+                virtual void save_matrix_to_matlab_file( const char* aFilename ) = 0;
 
-            virtual void save_matrix_map_to_matrix_market_file( const char* aFilename ) = 0;
+                virtual void save_matrix_to_matrix_market_file( const char* aFilename ) = 0;
 
-            //---------------------------------------------------------------------------------
+                virtual void save_matrix_map_to_matrix_market_file( const char* aFilename ) = 0;
 
-            Epetra_FECrsMatrix* get_matrix() { return mEpetraMat; }
+                //---------------------------------------------------------------------------------
 
-            Mat get_petsc_matrix() { return mPETScMat; }
+                Epetra_FECrsMatrix* get_matrix() { return mEpetraMat; }
+
+                Mat get_petsc_matrix() { return mPETScMat; }
         };
     }
 }
