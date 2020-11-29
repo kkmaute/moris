cd43d4e1f0934ee09bd476083e61850b9fb252fc
maute <maute@colorado.edu>
2020-11-28 21:37:17 -0700

    Nitshce for Heat Element (in progress; likely to be removed soon)

---
+++ projects/FEM/INT/src/cl_FEM_IWG_Heat_Method_Dirichlet_Nitsche.cpp
@@ -0,0 +1,193 @@
+
+#include "cl_FEM_IWG_Heat_Method_Dirichlet_Nitsche.hpp"
+#include "cl_FEM_Set.hpp"
+#include "cl_FEM_Field_Interpolator_Manager.hpp"
+
+#include "fn_trans.hpp"
+#include "fn_eye.hpp"
+#include "fn_dot.hpp"
+
+namespace moris
+{
+    namespace fem
+    {
+
+        //------------------------------------------------------------------------------
+
+        IWG_Heat_Method_Dirichlet_Nitsche::IWG_Heat_Method_Dirichlet_Nitsche( sint aBeta )
+        {
+            // set sign for symmetric/unsymmetric Nitsche
+            mBeta = aBeta;
+
+            // set size for the property pointer cell
+            mMasterProp.resize( static_cast< uint >( IWG_Property_Type::MAX_ENUM ), nullptr );
+
+            // populate the property map
+            mPropertyMap[ "Dirichlet" ] = static_cast< uint >( IWG_Property_Type::DIRICHLET );
+
+            // set size for the constitutive model pointer cell
+            mMasterCM.resize( static_cast< uint >( IWG_Constitutive_Type::MAX_ENUM ), nullptr );
+
+            // populate the constitutive map
+            mConstitutiveMap[ "Diffusion" ] = static_cast< uint >( IWG_Constitutive_Type::DIFFUSION );
+
+            // set size for the stabilization parameter pointer cell
+            mStabilizationParam.resize( static_cast< uint >( IWG_Stabilization_Type::MAX_ENUM ), nullptr );
+
+            // populate the stabilization map
+            mStabilizationMap[ "DirichletNitsche" ] = static_cast< uint >( IWG_Stabilization_Type::DIRICHLET_NITSCHE );
+        }
+
+        //------------------------------------------------------------------------------
+
+        void IWG_Heat_Method_Dirichlet_Nitsche::compute_residual( real aWStar )
+        {
+#ifdef DEBUG
+            // check field interpolators, properties, constitutive models
+            this->check_field_interpolators();
+#endif
+
+            // get master index for residual dof type, indices for assembly
+            uint tMasterDofIndex      = mSet->get_dof_index_for_type( mResidualDofType( 0 ), mtk::Master_Slave::MASTER );
+            uint tMasterResStartIndex = mSet->get_res_dof_assembly_map()( tMasterDofIndex )( 0, 0 );
+            uint tMasterResStopIndex  = mSet->get_res_dof_assembly_map()( tMasterDofIndex )( 0, 1 );
+
+            // get field interpolator for a given dof type
+            Field_Interpolator * tFI =
+                    mMasterFIManager->get_field_interpolators_for_type( mResidualDofType( 0 ) );
+
+            // get imposed temperature property
+            const std::shared_ptr< Property > & tPropDirichlet =
+                    mMasterProp( static_cast< uint >( IWG_Property_Type::DIRICHLET ) );
+
+            // get the elasticity CM
+            const std::shared_ptr< Constitutive_Model > & tCMDiffusion =
+                    mMasterCM( static_cast< uint >( IWG_Constitutive_Type::DIFFUSION ) );
+
+            // get the elasticity CM
+            const std::shared_ptr< Stabilization_Parameter > & tSPNitsche =
+                    mStabilizationParam( static_cast< uint >( IWG_Stabilization_Type::DIRICHLET_NITSCHE ) );
+
+            // compute jump
+            Matrix< DDRMat > tJump = tFI->val() - tPropDirichlet->val();
+
+            // compute the residual
+            mSet->get_residual()( 0 )(
+                    { tMasterResStartIndex, tMasterResStopIndex } ) += aWStar * (
+                            - tFI->N_trans() * tCMDiffusion->traction( mNormal )
+                            + mBeta * tCMDiffusion->testTraction( mNormal, mResidualDofType ) * tJump
+                            + tSPNitsche->val()( 0 ) * tFI->N_trans() * tJump );
+
+            // check for nan, infinity
+            MORIS_ASSERT( isfinite( mSet->get_residual()( 0 ) ),
+                    "IWG_Heat_Method_Dirichlet_Nitsche::compute_residual - Residual contains NAN or INF, exiting!");
+        }
+
+        //------------------------------------------------------------------------------
+
+        void IWG_Heat_Method_Dirichlet_Nitsche::compute_jacobian( real aWStar )
+        {
+#ifdef DEBUG
+            // check field interpolators, properties, constitutive models
+            this->check_field_interpolators();
+#endif
+
+            // get master index for residual dof type, indices for assembly
+            uint tMasterDofIndex      = mSet->get_dof_index_for_type( mResidualDofType( 0 ), mtk::Master_Slave::MASTER );
+            uint tMasterResStartIndex = mSet->get_res_dof_assembly_map()( tMasterDofIndex )( 0, 0 );
+            uint tMasterResStopIndex  = mSet->get_res_dof_assembly_map()( tMasterDofIndex )( 0, 1 );
+
+            // get field interpolator for residual dof type
+            Field_Interpolator * tFI =
+                    mMasterFIManager->get_field_interpolators_for_type( mResidualDofType( 0 ) );
+
+            // get imposed temperature property
+            const std::shared_ptr< Property > & tPropDirichlet =
+                    mMasterProp( static_cast< uint >( IWG_Property_Type::DIRICHLET ) );
+
+            // get the elasticity CM
+            const std::shared_ptr< Constitutive_Model > & tCMDiffusion =
+                    mMasterCM( static_cast< uint >( IWG_Constitutive_Type::DIFF_LIN_ISO ) );
+
+            // get the elasticity CM
+            const std::shared_ptr< Stabilization_Parameter > & tSPNitsche =
+                    mStabilizationParam( static_cast< uint >( IWG_Stabilization_Type::DIRICHLET_NITSCHE ) );
+
+            // compute jump
+            Matrix< DDRMat > tJump = tFI->val() - tPropDirichlet->val();
+
+            // compute the Jacobian for indirect dof dependencies through properties
+            uint tNumDofDependencies = mRequestedMasterGlobalDofTypes.size();
+
+            for( uint iDOF = 0; iDOF < tNumDofDependencies; iDOF++ )
+            {
+                // get the dof type
+                const Cell< MSI::Dof_Type > & tDofType = mRequestedMasterGlobalDofTypes( iDOF );
+
+                // get the index for dof type, indices for assembly
+                sint tDofDepIndex         = mSet->get_dof_index_for_type( tDofType( 0 ), mtk::Master_Slave::MASTER );
+                uint tMasterDepStartIndex = mSet->get_jac_dof_assembly_map()( tMasterDofIndex )( tDofDepIndex, 0 );
+                uint tMasterDepStopIndex  = mSet->get_jac_dof_assembly_map()( tMasterDofIndex )( tDofDepIndex, 1 );
+
+                // get sub-matrix
+                auto tJac = mSet->get_jacobian()(
+                        { tMasterResStartIndex, tMasterResStopIndex },
+                        { tMasterDepStartIndex, tMasterDepStopIndex } );
+
+                // if dof type is residual dof type
+                if( tDofType( 0 ) == mResidualDofType( 0 ) )
+                {
+                    tJac += aWStar * (
+                            mBeta * tCMDiffusion->testTraction( mNormal, mResidualDofType ) * tM * tFI->N()
+                            + tSPNitsche->val()( 0 ) * tFI->N_trans() * tM * tFI->N() ) ;
+                }
+
+                // if dependency on the dof type
+                if ( tPropDirichlet->check_dof_dependency( tDofType ) )
+                {
+                    // add contribution to Jacobian
+                    tJac += aWStar * (
+                            - mBeta  * tCMDiffusion->testTraction( mNormal, mResidualDofType ) * tM * tPropDirichlet->dPropdDOF( tDofType )
+                            - tSPNitsche->val()( 0 ) * tFI->N_trans() * tM * tPropDirichlet->dPropdDOF( tDofType ) );
+                }
+
+                // if dependency on the dof type
+                if ( tCMDiffusion->check_dof_dependency( tDofType ) )
+                {
+                    // add contribution to Jacobian
+                    tJac += aWStar * (
+                            - tFI->N_trans() * tM * tCMDiffusion->dTractiondDOF( tDofType, mNormal )
+                            + mBeta * tCMDiffusion->dTestTractiondDOF( tDofType, mNormal, mResidualDofType ) * tM( 0 ) * tJump( 0 ) );
+                }
+
+                // if dependency on the dof type
+                if ( tSPNitsche->check_dof_dependency( tDofType ) )
+                {
+                    // add contribution to Jacobian
+                    tJac += aWStar * (
+                            tFI->N_trans() * tM * tJump( 0 ) * tSPNitsche->dSPdMasterDOF( tDofType ) );
+                }
+            }
+
+            // check for nan, infinity
+            MORIS_ASSERT( isfinite( mSet->get_jacobian() ) ,
+                    "IWG_Heat_Method_Dirichlet_Nitsche::compute_jacobian - Jacobian contains NAN or INF, exiting!");
+        }
+
+        //------------------------------------------------------------------------------
+
+        void IWG_Heat_Method_Dirichlet_Nitsche::compute_jacobian_and_residual( real aWStar )
+        {
+            MORIS_ERROR( false, "IWG_Heat_Method_Dirichlet_Nitsche::compute_jacobian_and_residual - Not implemented." );
+        }
+
+        //------------------------------------------------------------------------------
+
+        void IWG_Heat_Method_Dirichlet_Nitsche::compute_dRdp( real aWStar )
+        {
+            MORIS_ERROR( false, "IWG_Heat_Method_Dirichlet_Nitsche::compute_dRdp - Not implemented.");
+        }
+
+        //------------------------------------------------------------------------------
+    } /* namespace fem */
+} /* namespace moris */
