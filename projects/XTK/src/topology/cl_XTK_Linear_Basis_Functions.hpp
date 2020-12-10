9013caad508b4877dbb1b343f1f12b39db5498c7
Adam <adam.christopherson@colorado.edu>
2020-12-09 23:56:51 -0700

    Basis functions can work with either a row vector or a column vector of parametric coordinates

--- projects/XTK/src/topology/cl_XTK_Linear_Basis_Functions.hpp
+++ projects/XTK/src/topology/cl_XTK_Linear_Basis_Functions.hpp
@@ -10,29 +10,25 @@
 
 namespace xtk
 {
-class Linear_Basis_Function: public Basis_Function
-{
-public:
-    Linear_Basis_Function()
+    class Linear_Basis_Function: public Basis_Function
     {
 
-    }
+    public:
+        Linear_Basis_Function()
+        {
+        }
 
-    void evaluate_basis_function(moris::Matrix< moris::DDRMat > const & aLocalCoordinate,
-                                 moris::Matrix< moris::DDRMat > & aBasisFunctionValues) const
-    {
-        aBasisFunctionValues.resize(1,2);
+        void evaluate_basis_function(moris::Matrix< moris::DDRMat > const & aLocalCoordinate,
+                                     moris::Matrix< moris::DDRMat > & aBasisFunctionValues) const
+        {
+            aBasisFunctionValues.resize(1,2);
 
-        moris::real tXi = aLocalCoordinate(0,0);
+            moris::real tXi = aLocalCoordinate(0);
 
-        aBasisFunctionValues(0,0) = 0.5*(1-tXi);
-        aBasisFunctionValues(0,1) = 0.5*(1+tXi);
-    }
-
-
-};
+            aBasisFunctionValues(0) = 0.5*(1-tXi);
+            aBasisFunctionValues(1) = 0.5*(1+tXi);
+        }
+    };
 }
-
-
 
 #endif /* SRC_TOPOLOGY_CL_XTK_LINEAR_BASIS_FUNCTIONS_HPP_ */
--- projects/XTK/src/topology/cl_XTK_Quad_4_Basis_Function.hpp
+++ projects/XTK/src/topology/cl_XTK_Quad_4_Basis_Function.hpp
@@ -14,34 +14,30 @@
 #include "cl_Matrix.hpp"
 #include "cl_XTK_Basis_Function.hpp"
 
-
-
 namespace xtk
 {
-class Quad_4_Basis_Function: public Basis_Function
-{
-public:
-    Quad_4_Basis_Function()
+    class Quad_4_Basis_Function: public Basis_Function
     {
 
-    }
+    public:
+        Quad_4_Basis_Function()
+        {
+        }
 
-    void evaluate_basis_function(moris::Matrix< moris::DDRMat > const & aLocalCoordinate,
-                                 moris::Matrix< moris::DDRMat > & aBasisFunctionValues) const
-    {
-        aBasisFunctionValues.resize(1,4);
+        void evaluate_basis_function(moris::Matrix< moris::DDRMat > const & aLocalCoordinate,
+                                     moris::Matrix< moris::DDRMat > & aBasisFunctionValues) const
+        {
+            aBasisFunctionValues.resize(1,4);
 
-        moris::real tXi = aLocalCoordinate(0,0);
-        moris::real tEta = aLocalCoordinate(0,1);
+            moris::real tXi = aLocalCoordinate(0);
+            moris::real tEta = aLocalCoordinate(1);
 
-        aBasisFunctionValues(0,0) = 0.25*(1.0-tXi)*(1.0-tEta);
-        aBasisFunctionValues(0,1) = 0.25*(1.0+tXi)*(1.0-tEta);
-        aBasisFunctionValues(0,2) = 0.25*(1.0+tXi)*(1.0+tEta);
-        aBasisFunctionValues(0,3) = 0.25*(1.0-tXi)*(1.0+tEta);
-    }
-
-
-};
+            aBasisFunctionValues(0) = 0.25*(1.0-tXi)*(1.0-tEta);
+            aBasisFunctionValues(1) = 0.25*(1.0+tXi)*(1.0-tEta);
+            aBasisFunctionValues(2) = 0.25*(1.0+tXi)*(1.0+tEta);
+            aBasisFunctionValues(3) = 0.25*(1.0-tXi)*(1.0+tEta);
+        }
+    };
 }
 
 
