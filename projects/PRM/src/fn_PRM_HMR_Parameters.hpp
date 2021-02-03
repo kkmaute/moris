1456d7c2a05716cfb74cf5016ebd8da83d86ae6e
schmidt <schmidt@blueflame>
2021-02-03 11:58:12 -0700

    - added new HMR input parameter

--- projects/PRM/src/fn_PRM_HMR_Parameters.hpp
+++ projects/PRM/src/fn_PRM_HMR_Parameters.hpp
@@ -111,6 +111,11 @@ namespace moris
 
         tParameterList.insert( "refinement_function_names", "" );
 
+        //! Expert functionality. This function is only for developers. It is not tested in all use cases and will not work in all use cases. T
+        //! When using this function the user has to now the limitations and unexpected behaviors
+        tParameterList.insert( "use_refine_low_level_elements", false );
+
+
         return tParameterList;
     }
 //------------------------------------------------------------------------------
