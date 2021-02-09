9888c01e6306f773e2a44ca33fbc34bed32cbad8
schmidt <schmidt@blueflame>
2021-02-08 22:49:52 -0700

    - fixed mtk field

--- projects/MTK/src/cl_MTK_Field.cpp
+++ projects/MTK/src/cl_MTK_Field.cpp
@@ -27,53 +27,151 @@ namespace moris
 
         //------------------------------------------------------------------------------
 
-        Field::Field(sint        aDiscretizationMeshIndex,
-                     std::string aName)
-                : mDiscretizationMeshIndex(aDiscretizationMeshIndex)
-                , mName(aName)
-        {
-        }
-
-        //------------------------------------------------------------------------------
-
         Field::~Field()
         {
+
         }
 
         //------------------------------------------------------------------------------
 
-        const Matrix<DDRMat>& Field::get_nodal_values(mtk::Mesh* aMesh)
+        void Field::save_field_to_hdf5(
+                const std::string & aFilePath,
+                const bool          aCreateNewFile )
         {
-            mNodalValues.set_size(aMesh->get_num_nodes(), 1);
-            for (uint tNodeIndex = 0; tNodeIndex < aMesh->get_num_nodes(); tNodeIndex++)
+            // test if file exists
+            std::string tFilePath = make_path_parallel( aFilePath );
+
+            // test if file exists
+            std::ifstream tFile( tFilePath );
+            bool tFileExists;
+            if( tFile )
             {
-                mNodalValues(tNodeIndex) = this->get_field_value(tNodeIndex, aMesh->get_node_coordinate(tNodeIndex));
+                tFileExists = true;
             }
-            return mNodalValues;
+            else
+            {
+                tFileExists = false;
+            }
+
+            tFile.close();
+
+            // delete file if it exists and user does not want to keep it
+            if( aCreateNewFile && tFileExists )
+            {
+                std::remove( tFilePath.c_str() );
+                tFileExists = false;
+            }
+
+            hid_t tFileID;
+
+            if( tFileExists )
+            {
+                tFileID = open_hdf5_file( aFilePath );
+            }
+            else
+            {
+                tFileID = create_hdf5_file( aFilePath );
+            }
+
+            herr_t tStatus;
+
+            save_matrix_to_hdf5_file( tFileID,
+                    this->get_label(),
+                    this->get_coefficients(),
+                    tStatus );
+
+            // close file
+            tStatus = close_hdf5_file( tFileID );
         }
 
         //------------------------------------------------------------------------------
 
-        uint Field::get_number_of_dimensions()
+        void Field::save_node_values_to_hdf5(
+                const std::string & aFilePath,
+                const bool          aCreateNewFile )
         {
-            return mNumberOfDimensions;
+            // test if file exists
+            std::string tFilePath = make_path_parallel( aFilePath );
+
+            // test if file exists
+            std::ifstream tFile( tFilePath );
+            bool tFileExists;
+            if( tFile )
+            {
+                tFileExists = true;
+            }
+            else
+            {
+                tFileExists = false;
+            }
+
+            tFile.close();
+
+            // delete file if it exists and user does not want to keep it
+            if( aCreateNewFile && tFileExists )
+            {
+                std::remove( tFilePath.c_str() );
+                tFileExists = false;
+            }
+
+            hid_t tFileID;
+
+            if( tFileExists )
+            {
+                tFileID = open_hdf5_file( aFilePath );
+            }
+            else
+            {
+                tFileID = create_hdf5_file( aFilePath );
+            }
+
+            herr_t tStatus;
+
+            save_matrix_to_hdf5_file( tFileID,
+                    this->get_label(),
+                    this->get_node_values(),
+                    tStatus );
+
+            // close file
+            tStatus = close_hdf5_file( tFileID );
         }
 
         //------------------------------------------------------------------------------
 
-        uint Field::get_discretization_mesh_index()
+        void Field::load_field_from_hdf5(
+                const std::string & aFilePath,
+                const uint          aBSplineOrder )
         {
-            return mDiscretizationMeshIndex;
+            hid_t tFile    = open_hdf5_file( aFilePath );
+            herr_t tStatus = 0;
+            load_matrix_from_hdf5_file( tFile,
+                    this->get_label(),
+                    this->get_coefficients(),
+                    tStatus );
+
+            tStatus = close_hdf5_file( tFile );
         }
 
         //------------------------------------------------------------------------------
 
-        std::string Field::get_name()
+        void Field::save_node_values_to_binary( const std::string & aFilePath )
         {
-            return mName;
+            // make path parallel
+            std::string tFilePath = parallelize_path( aFilePath );
+
+            save_matrix_to_binary_file( this->get_node_values(), tFilePath );
         }
 
         //------------------------------------------------------------------------------
 
+        void Field::save_bspline_coeffs_to_binary( const std::string & aFilePath )
+        {
+            // make path parallel
+            std::string tFilePath = parallelize_path( aFilePath );
+
+            save_matrix_to_binary_file( this->get_coefficients(), tFilePath );
+        }
+
+        //------------------------------------------------------------------------------
     } /* namespace hmr */
 } /* namespace moris */
