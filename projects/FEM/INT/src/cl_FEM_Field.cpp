fdbe01ec98c3a7ef3cdf6e4eed5e729e12736597
schmidt <schmidt@blueflame>
2021-02-09 23:59:26 -0700

    - FEM field

---
+++ projects/FEM/INT/src/cl_FEM_Field.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+
+#include "cl_FEM_Field.hpp"
+// HD5 c-interface
+
+
+namespace moris
+{
+    namespace fem
+    {
+
+        //------------------------------------------------------------------------------
+
+        Field::~Field()
+        {
+
+        }
+
+        //------------------------------------------------------------------------------
+    } /* namespace hmr */
+} /* namespace moris */
