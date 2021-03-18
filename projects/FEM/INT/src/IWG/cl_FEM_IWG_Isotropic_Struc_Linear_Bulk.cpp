27102115149f15eeb7942087a96a057241815d3b
maute <maute@colorado.edu>
2021-03-18 14:15:14 -0600

    replace MORIS_ERROR with MORIS_ASSERT in GP level function

--- projects/FEM/INT/src/IWG/cl_FEM_IWG_Isotropic_Struc_Linear_Bulk.cpp
+++ projects/FEM/INT/src/IWG/cl_FEM_IWG_Isotropic_Struc_Linear_Bulk.cpp
@@ -94,7 +94,7 @@ namespace moris
             }
 
             // check for nan, infinity
-            MORIS_ERROR( isfinite( mSet->get_residual()( 0 ) ),
+            MORIS_ASSERT( isfinite( mSet->get_residual()( 0 ) ),
                     "IWG_Isotropic_Struc_Linear_Bulk::compute_residual - Residual contains NAN or INF, exiting!");
         }
 
