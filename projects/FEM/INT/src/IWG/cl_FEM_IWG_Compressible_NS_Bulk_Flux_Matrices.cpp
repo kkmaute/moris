9b75b52cff7cdd0b0e2dc13cc2fe34452dee0455
wunsch <wunsch@tomcat>
2021-03-21 19:26:36 -0600

    Comp flow bulk and boundary IWGs make use of free functions
    flux matrix computations were moved to centrelized
    free functions in the previous commit

--- projects/FEM/INT/src/IWG/cl_FEM_IWG_Compressible_NS_Bulk_Flux_Matrices.cpp
+++ projects/FEM/INT/src/IWG/cl_FEM_IWG_Compressible_NS_Bulk_Flux_Matrices.cpp
@@ -8,6 +8,7 @@
 #include "cl_FEM_Set.hpp"
 #include "cl_FEM_Field_Interpolator_Manager.hpp"
 #include "cl_FEM_IWG_Compressible_NS_Bulk.hpp"
+#include "fn_FEM_IWG_Compressible_NS.hpp"
 
 #include "fn_trans.hpp"
 #include "fn_norm.hpp"
@@ -31,7 +32,8 @@ namespace moris
             mVarVecEval = false;
 
             // check residual DoF types
-            MORIS_ASSERT( this->check_residual_dof_types(), "IWG_Compressible_NS_Bulk::assemble_variable_set() - check for residual DoF types failed." );
+            MORIS_ASSERT( check_residual_dof_types( mResidualDofType ), 
+                    "IWG_Compressible_NS_Bulk::assemble_variable_set() - check for residual DoF types failed." );
 
             //FIXME: only density and pressure primitive variable sets supported in this function
 
@@ -83,7 +85,8 @@ namespace moris
             mVarDofVecEval = false;
 
             // check residual DoF types
-            MORIS_ASSERT( this->check_residual_dof_types(), "IWG_Compressible_NS_Bulk::assemble_variable_DOF_set() - check for residual DoF types failed." );
+            MORIS_ASSERT( check_residual_dof_types( mResidualDofType ), 
+                    "IWG_Compressible_NS_Bulk::assemble_variable_DOF_set() - check for residual DoF types failed." );
 
             //FIXME: only density and pressure primitive variable sets supported so far
 
@@ -148,109 +151,8 @@ namespace moris
             std::shared_ptr< Material_Model > tMM = mMasterMM( static_cast< uint >( IWG_Material_Type::FLUID_MM ) );
             std::shared_ptr< Constitutive_Model > tCM = mMasterCM( static_cast< uint >( IWG_Constitutive_Type::FLUID_CM ) );
 
-            // get the velocity FI
-            Field_Interpolator * tFIVelocity =  mMasterFIManager->get_field_interpolators_for_type( mDofVelocity );
-            
-            // get number of Space dimensions
-            uint tNumSpaceDims = tFIVelocity->get_number_of_fields();
-
-            // get commonly used values
-            real tRho = tMM->density()( 0 );
-            real tUx = tFIVelocity->val()( 0 );
-            real tUy = tFIVelocity->val()( 1 );
-            real tAlphaP = tMM->AlphaP()( 0 );
-            real tBetaT = tMM->BetaT()( 0 );
-            real tEtot = tCM->Energy()( 0 );
-
-            // constants computed as in M. Polner's 2005 PhD thesis
-            real tE1p = tBetaT * tEtot;
-            real tE2p = tE1p + 1.0;
-            real tE3p = tEtot + tMM->pressure()( 0 );
-            real tE4p = -1.0 * tAlphaP * tEtot + tRho * tMM->Cv()( 0 );         
-
-            // reset A matrices
-            Matrix< DDRMat > tEmptyA( tNumSpaceDims + 2, tNumSpaceDims + 2, 0.0 );
-            mA.assign( tNumSpaceDims + 2, tEmptyA );
-            //mA.resize( tNumSpaceDims + 2 );          
-
-            // assemble matrices based on 
-            switch ( tNumSpaceDims )
-            {
-                // for 2D
-                case 2 :
-                {
-                    // evaluate A_0
-                    mA( 0 ) = { 
-                        {       tRho * tBetaT,        0.0,        0.0,       -tRho * tAlphaP }, 
-                        { tUx * tRho * tBetaT,       tRho,        0.0, tUx * -tRho * tAlphaP }, 
-                        { tUy * tRho * tBetaT,        0.0,       tRho, tUy * -tRho * tAlphaP }, 
-                        {                tE1p, tUx * tRho, tUy * tRho,                  tE4p } };
-
-                    // evaluate A_1
-                    mA( 1 ) = { 
-                        {             tRho * tBetaT * tUx,                    tRho,              0.0,       -tRho * tAlphaP * tUx }, 
-                        { 1.0 + tUx * tRho * tBetaT * tUx,        2.0 * tUx * tRho,              0.0, tUx * -tRho * tAlphaP * tUx }, 
-                        {       tUy * tRho * tBetaT * tUx,              tUy * tRho,       tUx * tRho, tUy * -tRho * tAlphaP * tUx },  
-                        {                      tE2p * tUx, tE3p + tUx * tUx * tRho, tUy * tUx * tRho,                  tE4p * tUx } };
-
-                    // evaluate A_2
-                    mA( 2 ) = { 
-                        {             tRho * tBetaT * tUy,              0.0,                    tRho,       -tRho * tAlphaP * tUy }, 
-                        {       tUx * tRho * tBetaT * tUy,       tUy * tRho,              tUx * tRho, tUx * -tRho * tAlphaP * tUy }, 
-                        { 1.0 + tUy * tRho * tBetaT * tUy,              0.0,        2.0 * tUy * tRho, tUy * -tRho * tAlphaP * tUy }, 
-                        {                      tE2p * tUy, tUx * tUy * tRho, tE3p + tUy * tUy * tRho,                  tE4p * tUy } };
-
-                    break;
-                }
-
-                // for 3D
-                case 3 :
-                {
-                    // get the z-velocity
-                    real tUz = tFIVelocity->val()( 2 );
-
-                    // evaluate A_0
-                    mA( 0 ) = { 
-                        {       tRho * tBetaT,        0.0,        0.0,        0.0,       -tRho * tAlphaP }, 
-                        { tUx * tRho * tBetaT,       tRho,        0.0,        0.0, tUx * -tRho * tAlphaP }, 
-                        { tUy * tRho * tBetaT,        0.0,       tRho,        0.0, tUy * -tRho * tAlphaP }, 
-                        { tUz * tRho * tBetaT,        0.0,        0.0,       tRho, tUz * -tRho * tAlphaP }, 
-                        {                tE1p, tUx * tRho, tUy * tRho, tUz * tRho,                  tE4p } };      
-
-                    // evaluate A_1
-                    mA( 1 ) = { 
-                        {             tRho * tBetaT * tUx,                    tRho,              0.0,              0.0,       -tRho * tAlphaP * tUx }, 
-                        { 1.0 + tUx * tRho * tBetaT * tUx,        2.0 * tUx * tRho,              0.0,              0.0, tUx * -tRho * tAlphaP * tUx }, 
-                        {       tUy * tRho * tBetaT * tUx,              tUy * tRho,       tUx * tRho,              0.0, tUy * -tRho * tAlphaP * tUx }, 
-                        {       tUz * tRho * tBetaT * tUx,              tUz * tRho,              0.0,       tUx * tRho, tUz * -tRho * tAlphaP * tUx }, 
-                        {                      tE2p * tUx, tE3p + tUx * tUx * tRho, tUy * tUx * tRho, tUz * tUx * tRho,                  tE4p * tUx } };
-
-                    // evaluate A_2
-                    mA( 2 ) = { 
-                        {             tRho * tBetaT * tUy,              0.0,                    tRho,              0.0,       -tRho * tAlphaP * tUy }, 
-                        {       tUx * tRho * tBetaT * tUy,       tUy * tRho,              tUx * tRho,              0.0, tUx * -tRho * tAlphaP * tUy }, 
-                        { 1.0 + tUy * tRho * tBetaT * tUy,              0.0,        2.0 * tUy * tRho,              0.0, tUy * -tRho * tAlphaP * tUy }, 
-                        {       tUz * tRho * tBetaT * tUy,              0.0,              tUz * tRho,       tUy * tRho, tUz * -tRho * tAlphaP * tUy }, 
-                        {                      tE2p * tUy, tUx * tUy * tRho, tE3p + tUy * tUy * tRho, tUz * tUy * tRho,                  tE4p * tUy } };
-
-
-                    // evaluate A_3
-                    mA( 3 ) = { 
-                        {             tRho * tBetaT * tUz,              0.0,              0.0,                    tRho,       -tRho * tAlphaP * tUz }, 
-                        {       tUx * tRho * tBetaT * tUz,       tUz * tRho,              0.0,              tUx * tRho, tUx * -tRho * tAlphaP * tUz }, 
-                        {       tUy * tRho * tBetaT * tUz,              0.0,       tUz * tRho,              tUy * tRho, tUy * -tRho * tAlphaP * tUz }, 
-                        { 1.0 + tUz * tRho * tBetaT * tUz,              0.0,              0.0,        2.0 * tUz * tRho, tUz * -tRho * tAlphaP * tUz }, 
-                        {                      tE2p * tUz, tUx * tUz * tRho, tUy * tUz * tRho, tE3p + tUz * tUz * tRho,                  tE4p * tUz } };                        
-
-                    break;
-                }                
-            
-                // unknown number of spatial dimensions
-                default:
-                {
-                    MORIS_ERROR( false, "IWG_Compressible_NS_Bulk::assemble_A_matrices() - Number of space dimensions must be 2 or 3" );
-                };
-            }
+            // evaluate A-matrices and store them
+            eval_A( tMM, tCM, mMasterFIManager, mResidualDofType, mA );  
         }
 
        //------------------------------------------------------------------------------
@@ -264,7 +166,11 @@ namespace moris
             }
             
             // set the eval flag
-            mFluxADofMatEval = false;         
+            mFluxADofMatEval = false;
+
+            // get the material and constitutive models
+            std::shared_ptr< Material_Model > tMM = mMasterMM( static_cast< uint >( IWG_Material_Type::FLUID_MM ) );
+            std::shared_ptr< Constitutive_Model > tCM = mMasterCM( static_cast< uint >( IWG_Constitutive_Type::FLUID_CM ) );
 
             // get the velocity FI
             Field_Interpolator * tFIVelocity =  mMasterFIManager->get_field_interpolators_for_type( mDofVelocity );
@@ -283,16 +189,16 @@ namespace moris
             for ( uint iMat = 0; iMat < tNumSpaceDims + 1; iMat++)
             {
                 mADOF( iMat ).assign( tNumSpaceDims + 2, tEmptyADof );
-            }   
+            }      
 
-            // evaluate the derivatives for each of the matrices
-            this->eval_A0_DOF();
-            this->eval_A1_DOF();
-            this->eval_A2_DOF();
+            // evaluate the derivatives for each of the matrices and store them
+            eval_A0_DOF( tMM, tCM, mMasterFIManager, mResidualDofType, mADOF( 0 ) ); 
+            eval_A1_DOF( tMM, tCM, mMasterFIManager, mResidualDofType, mADOF( 1 ) );  
+            eval_A2_DOF( tMM, tCM, mMasterFIManager, mResidualDofType, mADOF( 2 ) ); 
             if ( tNumSpaceDims == 3 )
             {
-                this->eval_A3_DOF();
-            }
+                eval_A3_DOF( tMM, tCM, mMasterFIManager, mResidualDofType, mADOF( 3 ) );  
+            } 
         }
 
         //------------------------------------------------------------------------------
@@ -312,157 +218,8 @@ namespace moris
             std::shared_ptr< Property > tPropDynamicViscosity = mMasterProp( static_cast< uint >( IWG_Property_Type::DYNAMIC_VISCOSITY ) );
             std::shared_ptr< Property > tPropThermalConductivity = mMasterProp( static_cast< uint >( IWG_Property_Type::THERMAL_CONDUCTIVITY ) );
 
-            // get the velocity FI
-            Field_Interpolator * tFIVelocity =  mMasterFIManager->get_field_interpolators_for_type( mDofVelocity );
-            
-            // get number of Space dimensions
-            uint tNumSpaceDims = tFIVelocity->get_number_of_fields();
-
-            // get commonly used values
-            real tKa = tPropThermalConductivity->val()( 0 );
-            real tMu = tPropDynamicViscosity->val()( 0 );
-            real tLa = -2.0 * tMu / 3.0;
-            real tCh = 2.0 * tMu + tLa;
-            real tUx = tFIVelocity->val()( 0 );
-            real tUy = tFIVelocity->val()( 1 );
-
-            // set number of K matrices
-            mK.resize( tNumSpaceDims );
-            for ( uint iDim = 0; iDim < tNumSpaceDims ; iDim++)
-            {
-                mK( iDim ).resize( tNumSpaceDims );
-            }            
-
-            // assemble matrices based on 
-            switch ( tNumSpaceDims )
-            {
-                // for 2D
-                case 2 :
-                {
-                    // evaluate K_11
-                    mK( 0 )( 0 ) = { 
-                        { 0.0,       0.0,       0.0, 0.0 }, 
-                        { 0.0,       tCh,       0.0, 0.0 }, 
-                        { 0.0,       0.0,       tMu, 0.0 }, 
-                        { 0.0, tUx * tCh, tUy * tMu, tKa } };
-
-                    // evaluate K_12
-                    mK( 0 )( 1 ) = { 
-                        { 0.0,       0.0,       0.0, 0.0 }, 
-                        { 0.0,       0.0,       tLa, 0.0 }, 
-                        { 0.0,       tMu,       0.0, 0.0 }, 
-                        { 0.0, tUy * tMu, tUx * tLa, 0.0 } };
-
-                    // =======================
-
-                    // evaluate K_21
-                    mK( 1 )( 0 ) = { 
-                        { 0.0,       0.0,       0.0, 0.0 }, 
-                        { 0.0,       0.0,       tMu, 0.0 }, 
-                        { 0.0,       tLa,       0.0, 0.0 }, 
-                        { 0.0, tUy * tLa, tUx * tMu, 0.0 } };
-
-                        // evaluate K_22
-                    mK( 1 )( 1 ) = { 
-                        { 0.0,       0.0,       0.0, 0.0 }, 
-                        { 0.0,       tMu,       0.0, 0.0 }, 
-                        { 0.0,       0.0,       tCh, 0.0 }, 
-                        { 0.0, tUx * tMu, tUy * tCh, tKa } };
-
-                    break;
-                }
-
-                // for 3D
-                case 3 :
-                {
-                    // get the z-velocity
-                    real tUz = tFIVelocity->val()( 2 );
-
-                    // evaluate K_11
-                    mK( 0 )( 0 ) = { 
-                        { 0.0,       0.0,       0.0,       0.0, 0.0 }, 
-                        { 0.0,       tCh,       0.0,       0.0, 0.0 }, 
-                        { 0.0,       0.0,       tMu,       0.0, 0.0 }, 
-                        { 0.0,       0.0,       0.0,       tMu, 0.0 }, 
-                        { 0.0, tUx * tCh, tUy * tMu, tUz * tMu, tKa } };
-
-                    // evaluate K_12
-                    mK( 0 )( 1 ) = { 
-                        { 0.0,       0.0,       0.0, 0.0, 0.0 }, 
-                        { 0.0,       0.0,       tLa, 0.0, 0.0 }, 
-                        { 0.0,       tMu,       0.0, 0.0, 0.0 }, 
-                        { 0.0,       0.0,       0.0, 0.0, 0.0 }, 
-                        { 0.0, tUy * tMu, tUx * tLa, 0.0, 0.0 } };
-
-                    // evaluate K_13
-                    mK( 0 )( 2 ) = { 
-                        { 0.0,       0.0, 0.0,       0.0, 0.0 }, 
-                        { 0.0,       0.0, 0.0,       tLa, 0.0 }, 
-                        { 0.0,       0.0, 0.0,       0.0, 0.0 }, 
-                        { 0.0,       tMu, 0.0,       0.0, 0.0 }, 
-                        { 0.0, tUz * tMu, 0.0, tUx * tLa, 0.0 } };
-
-                    // =======================
-
-                    // evaluate K_21
-                    mK( 1 )( 0 ) = { 
-                        { 0.0,       0.0,       0.0, 0.0, 0.0 }, 
-                        { 0.0,       0.0,       tMu, 0.0, 0.0 }, 
-                        { 0.0,       tLa,       0.0, 0.0, 0.0 }, 
-                        { 0.0,       0.0,       0.0, 0.0, 0.0 }, 
-                        { 0.0, tUy * tLa, tUx * tMu, 0.0, 0.0 } };
-
-                        // evaluate K_22
-                    mK( 1 )( 1 ) = { 
-                        { 0.0,       0.0,       0.0,       0.0, 0.0 }, 
-                        { 0.0,       tMu,       0.0,       0.0, 0.0 }, 
-                        { 0.0,       0.0,       tCh,       0.0, 0.0 }, 
-                        { 0.0,       0.0,       0.0,       tMu, 0.0 }, 
-                        { 0.0, tUx * tMu, tUy * tCh, tUz * tMu, tKa } };
-
-                        // evaluate K_23
-                    mK( 1 )( 2 ) = { 
-                        { 0.0, 0.0,       0.0,       0.0, 0.0 }, 
-                        { 0.0, 0.0,       0.0,       0.0, 0.0 }, 
-                        { 0.0, 0.0,       0.0,       tLa, 0.0 }, 
-                        { 0.0, 0.0,       tMu,       0.0, 0.0 }, 
-                        { 0.0, 0.0, tUy * tMu, tUy * tLa, 0.0 } };
-
-                    // =======================
-
-                    // evaluate K_31
-                    mK( 2 )( 0 ) = { 
-                        { 0.0,       0.0, 0.0,       0.0, 0.0 }, 
-                        { 0.0,       0.0, 0.0,       tMu, 0.0 }, 
-                        { 0.0,       0.0, 0.0,       0.0, 0.0 }, 
-                        { 0.0,       tLa, 0.0,       0.0, 0.0 }, 
-                        { 0.0, tUz * tLa, 0.0, tUx * tMu, 0.0 } };
-
-                    // evaluate K_32
-                    mK( 2 )( 1 ) = { 
-                        { 0.0, 0.0,       0.0,       0.0, 0.0 }, 
-                        { 0.0, 0.0,       0.0,       0.0, 0.0 }, 
-                        { 0.0, 0.0,       0.0,       tMu, 0.0 }, 
-                        { 0.0, 0.0,       tLa,       0.0, 0.0 }, 
-                        { 0.0, 0.0, tUz * tLa, tUy * tMu, 0.0 } };
-
-                    // evaluate K_33
-                    mK( 2 )( 2 ) = { 
-                        { 0.0,       0.0,       0.0,       0.0, 0.0 }, 
-                        { 0.0,       tMu,       0.0,       0.0, 0.0 }, 
-                        { 0.0,       0.0,       tMu,       0.0, 0.0 }, 
-                        { 0.0,       0.0,       0.0,       tCh, 0.0 }, 
-                        { 0.0, tUx * tMu, tUy * tMu, tUz * tCh, tKa } };
-
-                    break;
-                }                
-            
-                // unknown number of spatial dimensions
-                default:
-                {
-                    MORIS_ERROR( false, "IWG_Compressible_NS_Bulk::assemble_A_matrices() - Number of space dimensions must be 2 or 3" );
-                };
-            }
+            // eval K matrices and store them
+            eval_K( tPropDynamicViscosity, tPropThermalConductivity, mMasterFIManager, mK );
         }                    
 
         //------------------------------------------------------------------------------
