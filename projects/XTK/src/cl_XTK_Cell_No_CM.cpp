3d893377a7feb9027aded82a3efd2ebb5a79d197
Keenan Doble <kdoble48@gmail.com>
2021-03-29 15:36:37 -0600

    Merge issue resolved

--- projects/XTK/src/cl_XTK_Cell_No_CM.cpp
+++ projects/XTK/src/cl_XTK_Cell_No_CM.cpp
@@ -43,14 +43,6 @@ namespace xtk
         }
         return tVertexCoords;
     }
-
-
-    moris::real
-    Cell_XTK_No_CM::compute_cell_side_measure_deriv(moris_index const & aSideOrdinal, uint aLocalVertexID, uint aDirection) const
-    {
-        return mCellInfo->compute_cell_side_size_deriv(this, aSideOrdinal, aLocalVertexID, aDirection);
-    }
-
 }
 
 
