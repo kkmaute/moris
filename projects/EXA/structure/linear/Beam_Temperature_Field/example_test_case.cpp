9c50c4a74665bb29976daf2f636ff6427593cf89
Sam <gates@falcon.colorado.edu>
2021-03-29 22:45:09 -0600

    Updating beam deflection due to temperature field example

--- projects/EXA/structure/linear/Beam_Temperature_Field/example_test_case.cpp
+++ projects/EXA/structure/linear/Beam_Temperature_Field/example_test_case.cpp
@@ -18,21 +18,81 @@ using namespace moris;
 
 //---------------------------------------------------------------
 
-//// global variable for interpolation order
-//uint gInterpolationOrder;
-//
-//// problem dimension: 2D or 3D
-//uint gDim;
-//
-//// test case index
-//uint gTestCaseIndex;
-
 // flag to print reference values
 bool gPrintReferenceValues = false;
 
 //---------------------------------------------------------------
 
 int fn_WRK_Workflow_Main_Interface( int argc, char * argv[] );
+
+//---------------------------------------------------------------
+
+extern "C"
+void check_linear_results(moris::mtk::Exodus_IO_Helper & aExoIO,uint aNodeId)
+{
+    if (gPrintReferenceValues)
+    {
+        // coordinates of reference point
+        moris::print( aExoIO.get_nodal_coordinate( aNodeId ), "Coordinates of reference point");
+
+        std::cout<<"Ux: "<<          aExoIO.get_nodal_field_value( aNodeId, 2, 0 ) << std::endl;
+        std::cout<<"Uy: "<<          aExoIO.get_nodal_field_value( aNodeId, 3, 0 ) << std::endl;
+        std::cout<<"Temperature: "<< aExoIO.get_nodal_field_value( aNodeId, 4, 0 ) << std::endl;
+        return;
+    }
+
+    // define error threshold.
+    // larger deflection epsilon used since the
+    real tEpsilon            = 1.0e-6;
+    real tEpsilon_Deflection = 1.0e-3;
+
+    // check nodal coordinates
+    Matrix< DDRMat > tReferenceCoordinate = { {10.0},{0.5} };
+    real tRelDiffNorm = moris::norm( aExoIO.get_nodal_coordinate( aNodeId ) - tReferenceCoordinate )/ moris::norm(tReferenceCoordinate);
+    REQUIRE( tRelDiffNorm <  tEpsilon );
+
+    // check beam deflection from the temperature field
+    // see matlab script or https://nptel.ac.in/content/storage2/courses/105101085/downloads/lec-26.pdf
+    real tReferenceUy = -2.0;
+    real tUy =  aExoIO.get_nodal_field_value( aNodeId, 3, 0 );
+    REQUIRE( ( tUy - tReferenceUy ) <  tEpsilon_Deflection );
+
+    // check field temperature at this node
+    real tTemperatureField = 1.0;
+    REQUIRE(  aExoIO.get_nodal_field_value( aNodeId, 4, 0 ) - tTemperatureField < tEpsilon);
+}
+
+//---------------------------------------------------------------
+
+extern "C"
+void check_linear_results_serial()
+{
+    // open and query exodus output file (set verbose to true to get basic mesh information)
+    moris::mtk::Exodus_IO_Helper tExoIO("Beam_Temperature_Field_Problem.exo",0,false,false);
+
+    // check dimension, number of nodes and number of elements
+    uint tNumDims  = tExoIO.get_number_of_dimensions();
+    uint tNumNodes = tExoIO.get_number_of_nodes();
+    uint tNumElems = tExoIO.get_number_of_elements();
+
+    if (gPrintReferenceValues)
+    {
+        std::cout << "Number of dimensions: " << tNumDims  << std::endl;
+        std::cout << "Number of nodes     : " << tNumNodes << std::endl;
+        std::cout << "Number of elements  : " << tNumElems << std::endl;
+    }
+    else
+    {
+        REQUIRE( tNumDims  ==  2 );
+        REQUIRE( tNumNodes ==  3975 );
+        REQUIRE( tNumElems ==  924 );
+    }
+
+    // check results at node with most deformation
+    uint tNodeId = 3973;
+
+    check_linear_results(tExoIO,tNodeId);
+}
 
 //---------------------------------------------------------------
 
@@ -47,20 +107,23 @@ TEST_CASE("Field_example_write",
 
     char * argv[2] = {tString1,tString2};
 
-//    // set interpolation order
-//    gInterpolationOrder = 1;
-//
-//    MORIS_LOG_INFO("");
-//    MORIS_LOG_INFO("Executing Field_Example_Write - 2D: Interpolation order 1 - %i Processors.",par_size());
-//    MORIS_LOG_INFO("");
-//
-//    // set dimension: 2D
-//    gDim = 2;
-//
-//    // set test case index
-//    gTestCaseIndex = 0;
-
     // call to performance manager main interface
-    fn_WRK_Workflow_Main_Interface( argc, argv );
+    int tRet = fn_WRK_Workflow_Main_Interface( argc, argv );
 
+    // catch test statements should follow
+    REQUIRE( tRet ==  0 );
+
+    // check results
+    switch ( par_size() )
+    {
+        case 1:
+        {
+            check_linear_results_serial();
+            break;
+        }
+        default:
+        {
+            MORIS_ERROR(false,"This 2D Example can only be run in serial.",par_size());
+        }
+    }
 }
