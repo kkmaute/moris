6568eb3b124face8f1b01b531f8d7d78e32e9e8a
Sam <gates@falcon.colorado.edu>
2021-03-31 18:46:23 -0600

    Fixing memory leak

--- projects/FEM/INT/src/IP/cl_FEM_Geometry_Interpolator.cpp
+++ projects/FEM/INT/src/IP/cl_FEM_Geometry_Interpolator.cpp
@@ -86,8 +86,8 @@ namespace moris
             mTimeInterpolation  = aInterpolationRule.create_time_interpolation_function();
 
             // number of space bases and dimensions
-            mNumSpaceParamDim = aInterpolationRule.create_space_interpolation_function()
-                                    ->get_number_of_param_dimensions();
+            mSpaceInterpolation = aInterpolationRule.create_space_interpolation_function();
+            mNumSpaceParamDim = mSpaceInterpolation->get_number_of_param_dimensions();
 
             // number of time bases and dimensions
             mNumTimeBases = mTimeInterpolation->get_number_of_bases();
@@ -104,14 +104,22 @@ namespace moris
 
         Geometry_Interpolator::~Geometry_Interpolator()
         {
-            if( mTimeInterpolation != NULL )
+            if( mSpaceInterpolation )
             {
-                delete mTimeInterpolation;
+                delete mSpaceInterpolation;
+                mSpaceInterpolation = nullptr;
             }
 
-            if( mSpaceInterpolator != nullptr )
+            if( mTimeInterpolation )
+            {
+                delete mTimeInterpolation;
+                mTimeInterpolation = nullptr;
+            }
+
+            if( mSpaceInterpolator )
             {
                 delete mSpaceInterpolator;
+                mSpaceInterpolator = nullptr;
             }
         }
 
