59332568540ad2013065ca809e89ffe9fd0b1558
maute <maute@colorado.edu>
2021-05-05 16:05:48 -0600

    Augmented DOF_IQI to extract spatial and temporal derivatives; trigger through function_parameters; documentation of options is needed

--- projects/FEM/INT/src/IQI/cl_FEM_IQI_Dof.cpp
+++ projects/FEM/INT/src/IQI/cl_FEM_IQI_Dof.cpp
@@ -18,14 +18,46 @@ namespace moris
 
         //------------------------------------------------------------------------------
 
+        void IQI_Dof::initialize()
+        {
+            if ( ! mIsInitialized )
+            {
+                // size of parameter list
+                uint tParamSize = mParameters.size();
+
+                // extract spatial derivative order
+                if ( tParamSize > 0 )
+                {
+                    MORIS_ERROR( mParameters( 0 ).numel() == 2,
+                            "IQI_Dof::initialize - Spatial gradient definition requires exactly two coefficients.\n");
+
+                    mSpatialDerivativeDirection = mParameters( 0 )( 0 );
+                    mSpatialDerivativeOrder     = mParameters( 0 )( 1 );
+                }
+
+                // extract time derivative order
+                if ( tParamSize > 1 )
+                {
+                    MORIS_ERROR( mParameters( 1 ).numel() == 1,
+                            "IQI_Dof::initialize - Time gradient definition requires exactly one coefficient.\n");
+
+                    mTimeDerivativeOrder = mParameters( 0 )( 0 );
+                }
+
+                // set initialize flag to true
+                mIsInitialized = true;
+            }
+        }
+
+        //------------------------------------------------------------------------------
+
         void IQI_Dof::compute_QI( real aWStar )
         {
+            // initialize if needed
+            this->initialize();
+
             // get index for QI
             sint tQIIndex = mSet->get_QI_assembly_index( mName );
-
-            // get field interpolator for a given dof type
-            Field_Interpolator * tFI =
-                    mMasterFIManager->get_field_interpolators_for_type( mQuantityDofType( 0 ) );
 
             // check if dof index was set (for the case of vector field)
             if( mQuantityDofType.size() > 1 )
@@ -37,27 +69,53 @@ namespace moris
                 mIQITypeIndex = 0;
             }
 
-            // evaluate the QI
-            mSet->get_QI()( tQIIndex ) += aWStar * ( tFI->val()( mIQITypeIndex ) );
+            mSet->get_QI()( tQIIndex ) += aWStar * this->evaluate_QI( mIQITypeIndex );
         }
 
         //------------------------------------------------------------------------------
 
         void IQI_Dof::compute_QI( Matrix< DDRMat > & aQI )
         {
+            // initialize if needed
+            this->initialize();
+
+            // check if dof index was set (for the case of vector field)
+            sint tQIIndex = 0;
+            if( mQuantityDofType.size() > 1 && mIQITypeIndex != -1 )
+            {
+                tQIIndex = mIQITypeIndex;
+            }
+
+            // evaluate QI
+            aQI = { this->evaluate_QI( tQIIndex ) };
+        }
+
+        //------------------------------------------------------------------------------
+
+        real IQI_Dof::evaluate_QI( sint aIQITypeIndex )
+        {
             // get field interpolator for a given dof type
             Field_Interpolator * tFI =
                     mMasterFIManager->get_field_interpolators_for_type( mQuantityDofType( 0 ) );
 
-            // check if dof index was set (for the case of vector field)
-            if( mQuantityDofType.size() > 1 && mIQITypeIndex != -1 )
+            // evaluate spatial derivative of dof
+            if ( mSpatialDerivativeOrder > 0 )
             {
-                aQI = { tFI->val()( mIQITypeIndex ) };
+                const Matrix<DDRMat> & tSpatialGradient = tFI->gradx( mSpatialDerivativeOrder );
+
+                return tSpatialGradient( mSpatialDerivativeDirection, aIQITypeIndex );
             }
-            else
+
+            // evaluate time derivative of dof
+            if ( mTimeDerivativeOrder > 0 )
             {
-                aQI = tFI->val();
+                const Matrix<DDRMat> & tTemporalGradient = tFI->gradt( mTimeDerivativeOrder );
+
+                return tTemporalGradient( aIQITypeIndex );
             }
+
+            // evaluate DOF value
+            return tFI->val()( aIQITypeIndex );
         }
 
         //------------------------------------------------------------------------------
