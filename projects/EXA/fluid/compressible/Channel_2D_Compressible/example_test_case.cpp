4f6d1850c36ead43cc28bd3ee5bc0eb9242a7b70
wunsch <wunsch@tomcat>
2021-05-06 01:57:17 -0600

    prepared compressible channel flow example

---
+++ projects/EXA/fluid/compressible/Channel_2D_Compressible/example_test_case.cpp
@@ -0,0 +1,36 @@
+//
+// example specific interface to moris
+//
+
+#include <catch.hpp>
+
+#include "cl_Logger.hpp" // MRS/IOS/src
+
+//---------------------------------------------------------------
+
+// global variable for interpolation order
+uint gInterpolationOrder;
+
+// flag to print reference values
+bool gPrintReferenceValues = false;
+
+//---------------------------------------------------------------
+
+int fn_WRK_Workflow_Main_Interface( int argc, char * argv[] );
+
+//---------------------------------------------------------------
+
+TEST_CASE("Channel_2D_Compressible",
+        "[moris],[example],[fluid],[compressible],[Channel_2D_Compressible]")
+{
+    // define command line call
+    int argc = 2;
+
+    char tString1[] = "";
+    char tString2[] = "./Channel_2D_Compressible.so";
+
+    char * argv[2] = {tString1,tString2};
+
+    // call to performance manager main interface
+    int tRet = fn_WRK_Workflow_Main_Interface( argc, argv );
+}
