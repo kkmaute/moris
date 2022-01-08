b217e19fc94f800915ab1a244b2e40390cd6ce16
maute <maute@colorado.edu>
2022-01-07 17:36:47 -0700

    synced with master branch: added option to delay start to attach debugging process; use command line option -p or --pause

--- projects/mains/main.cpp
+++ projects/mains/main.cpp
@@ -1,3 +1,7 @@
+#include<iostream>
+#include<ios>
+#include<limits>
+
 #include "../MRS/IOS/src/cl_Git_info.hpp"
 #include "cl_Communication_Manager.hpp" // COM/src
 #include "cl_Logger.hpp"                // MRS/IOS/src
@@ -12,6 +16,30 @@ using namespace moris;
 
 int fn_WRK_Workflow_Main_Interface( int argc, char * argv[] );
 
+//---------------------------------------------------------------
+
+void moris_pause( int  & argc, char * argv[] )
+{
+    // go through user arguments and look for flags
+    for( int k=0; k<argc; ++k )
+    {
+        // user requests delayed start
+        if ( std::string( argv[ k ] ) == "--pause" || std::string( argv[ k ] ) == "-p" )
+        {
+            if ( par_rank() == 0 )
+            {
+                std::string dummy;
+
+                std::cout << "Press enter to continue . . .\n" << std::flush;
+                std::getline(std::cin, dummy);
+            }
+        }
+    }
+
+    // wait until all processors are ready to continue
+    barrier();
+}
+
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -20,23 +48,56 @@ int main( int argc, char * argv[] )
     // initialize MORIS global communication manager
     gMorisComm = moris::Comm_Manager( &argc, &argv );
 
-    // Kokkos::initialize(argc, argv);
+    // delay start of execution for attaching debugger to process
+    moris_pause( argc, argv );
 
+    // Kokkos::initialize(argc, argv);
+    barrier();
+    int one=1;
+
+    int sumup=sum_all(one);
+    if (par_rank() == 0)
+    {
+        std::cout << " in main 0 " << sumup << std::endl << std::flush ;
+    }
     // set severity level 0 - all outputs
     gLogger.initialize( argc, argv );
 
+    barrier();
+    sumup=sum_all(one);
+    if (par_rank() == 0)
+    {
+        std::cout << " in main 1 " << sumup << std::endl << std::flush ;
+    }
     // print banner
     moris::print_banner( argc, argv );
+
+    barrier();
+    sumup=sum_all(one);
+    if (par_rank() == 0)
+    {
+        std::cout << " in main 2 " << sumup << std::endl << std::flush ;
+    }
 
     // print git branch and hash
     if ( par_rank() == 0 )
     {
         git_info tGitInfo;
+
         std::fprintf( stdout, "\n     GIT branch    : %s\n",  tGitInfo.get_git_branch().c_str() );
         std::fprintf( stdout,   "     GIT revision  : %s\n\n",tGitInfo.get_git_hash().c_str() );
+
         MORIS_LOG_SPEC("Par Rank",par_rank());
         MORIS_LOG_SPEC("Par Size",par_size());
     }
+
+    barrier();
+    sumup=sum_all(one);
+    if (par_rank() == 0)
+    {
+        std::cout << " in main 3 " << sumup << std::endl << std::flush ;
+    }
+
 
     int tRet = fn_WRK_Workflow_Main_Interface( argc, argv );
 
