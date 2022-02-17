c2005be80027f6b6b3450ea90c87af41e18f9fd9
noel <lino8536@colorado.edu>
2022-02-17 22:46:32 +0100

    Revert change to IQI stabilization

--- projects/FEM/INT/src/IQI/cl_FEM_IQI_Stabilization.cpp
+++ projects/FEM/INT/src/IQI/cl_FEM_IQI_Stabilization.cpp
@@ -62,14 +62,43 @@ namespace moris
             }
 
             // evaluate the QI
-            mSet->get_QI()( tQIIndex ) += aWStar * ( tSP->val()( mIQITypeIndex ) );
+            mSet->get_QI()( tQIIndex ) += {{ aWStar * tSP->val()( mIQITypeIndex ) }};
         }
 
         //------------------------------------------------------------------------------
 
         void IQI_Stabilization::compute_dQIdu( real aWStar )
         {
-            MORIS_ERROR(false, "IQI_Stabilization::compute_dQIdu - not implemented\n.");
+            // get the column index to assemble in residual
+            sint tQIIndex = mSet->get_QI_assembly_index( mName );
+
+            // get the stabilization parameter
+            const std::shared_ptr< Stabilization_Parameter > & tSP =
+                    mStabilizationParam( static_cast< uint >( IQI_Stabilization_Type::STABILIZATION ) );
+
+            // get the number of master dof type dependencies
+            uint tNumDofDependencies = mRequestedMasterGlobalDofTypes.size();
+
+            // compute dQIdu for indirect dof dependencies
+            for( uint iDof = 0; iDof < tNumDofDependencies; iDof++ )
+            {
+                // get the treated dof type
+                Cell< MSI::Dof_Type > & tDofType = mRequestedMasterGlobalDofTypes( iDof );
+
+                // get master index for residual dof type, indices for assembly
+                uint tMasterDofIndex      = mSet->get_dof_index_for_type( tDofType( 0 ), mtk::Master_Slave::MASTER );
+                uint tMasterDepStartIndex = mSet->get_res_dof_assembly_map()( tMasterDofIndex )( 0, 0 );
+                uint tMasterDepStopIndex  = mSet->get_res_dof_assembly_map()( tMasterDofIndex )( 0, 1 );
+
+                // Dof dependency
+                if ( tSP != nullptr && tSP->check_dof_dependency( tDofType ) )
+                {
+                    // compute dQIdu
+                    mSet->get_residual()( tQIIndex )(
+                            { tMasterDepStartIndex, tMasterDepStopIndex },
+                            { 0, 0 } ) += aWStar * trans( tSP->dSPdMasterDOF( tDofType ) );
+                }
+            }
         }
 
         //------------------------------------------------------------------------------
@@ -78,7 +107,16 @@ namespace moris
                 moris::Cell< MSI::Dof_Type > & aDofType,
                 Matrix< DDRMat >             & adQIdu )
         {
-            MORIS_ERROR(false, "IQI_Stabilization::compute_dQIdu - not implemented\n.");
+            // get the stabilization parameter
+            const std::shared_ptr< Stabilization_Parameter > & tSP =
+                    mStabilizationParam( static_cast< uint >( IQI_Stabilization_Type::STABILIZATION ) );
+
+            // Dof dependency
+            if ( tSP != nullptr && tSP->check_dof_dependency( aDofType ) )
+            {
+                // compute dQIdu
+                adQIdu = trans( tSP->dSPdMasterDOF( aDofType ) );
+            }
         }
 
         //------------------------------------------------------------------------------
