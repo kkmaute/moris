6648ef1ed07aafdf12243c11e6a539fa23efa9e6
maute <maute@colorado.edu>
2022-02-25 14:08:22 -0700

    added fn_clip_value.hpp

--- projects/ALG/src/algorithms.hpp
+++ projects/ALG/src/algorithms.hpp
@@ -2,8 +2,9 @@
 #define MORIS_ALGORITHMS_HPP_
 
 // algorithms header files.
-#include "fn_equal_to.hpp" // ALG/src
-#include "fn_hash_value.hpp" // ALG/src
-#include "fn_num_digits.hpp" // ALG/src
+#include "fn_equal_to.hpp"      // ALG/src
+#include "fn_hash_value.hpp"    // ALG/src
+#include "fn_num_digits.hpp"    // ALG/src
+#include "fn_clip_value.hpp"    // ALG/src
 
 #endif /* MORIS_ALGORITHMS_HPP_ */
