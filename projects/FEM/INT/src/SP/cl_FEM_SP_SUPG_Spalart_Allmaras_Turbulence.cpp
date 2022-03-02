b4b78b416ff52d050446e8abca5fb15d792d0bac
maute <maute@colorado.edu>
2022-03-01 20:23:20 -0700

    changed stabilization such it is based on linearized version of modified velocity and destruction term

--- projects/FEM/INT/src/SP/cl_FEM_SP_SUPG_Spalart_Allmaras_Turbulence.cpp
+++ projects/FEM/INT/src/SP/cl_FEM_SP_SUPG_Spalart_Allmaras_Turbulence.cpp
@@ -44,21 +44,21 @@ namespace moris
                     "SP_SUPG_Spalart_Allmaras_Turbulence::set_parameters - max 2 constant parameters can to be set." );
 
             // if exponent specification
-            if( tParamSize > 0 )
+            if ( tParamSize > 0 )
             {
                 // set exponent
                 mExponent = aParameters( 0 )( 0 );
             }
 
             // if including reaction term specification
-            if( tParamSize > 1 )
+            if ( tParamSize > 1 )
             {
                 // error unknown dof string
                 MORIS_ERROR( aParameters( 1 )( 0 ) == 0.0 || aParameters( 1 )( 0 ) == 1.0,
                         "SP_SUPG_Spalart_Allmaras_Turbulence::set_parameters - invalid mHasReaction parameter \n" );
 
                 // set exponent
-                mHasReaction = aParameters( 1 )( 0 );
+                mHasReaction = aParameters( 1 )( 0 ) > 0 ? true : false;
             }
         }
 
@@ -175,8 +175,8 @@ namespace moris
             // compute the length scale
             real tHugn = this->length_scale();
 
-            // compute uTilde = u - cb2/sigma gradv
-            Matrix< DDRMat > tModVelocity = tCMSATurbulence->modified_velocity();
+            // compute linearized version of uTilde = u - cb2/sigma gradv
+            Matrix< DDRMat > tModVelocity = tCMSATurbulence->modified_velocity_linearized();
 
             // compute norm( uTilde ) and threshold tNormA (for consistency with derivative computation)
             real tNormA = std::max( norm( tModVelocity ), mEpsilon );
@@ -191,12 +191,15 @@ namespace moris
             real tTau = std::pow( tTauA, mExponent ) + std::pow( tTauK, mExponent );
 
             // if use reaction term
-            if( mHasReaction )
+            if ( mHasReaction )
             {
-                // compute tau S
+                // compute tau S with linearized version of destruction term
                 real tTauS =
-                        tCMSATurbulence->wall_destruction_coefficient()( 0 )
+                        2.0 * tCMSATurbulence->wall_destruction_coefficient()( 0 )
                         - tCMSATurbulence->production_coefficient()( 0 );
+
+                // compute the regularized absolute of source term
+                tTauS = std::sqrt( tTauS * tTauS + mEpsilon ) - std::sqrt( mEpsilon );
 
                 // add contribution of reaction term
                 tTau += std::pow( tTauS, mExponent );
@@ -232,8 +235,8 @@ namespace moris
             // compute the length scale
             real tHugn = this->length_scale();
 
-            // evaluate uTilde = u - 1/sigma cb2 gradv
-            Matrix< DDRMat > tModVelocity = tCMSATurbulence->modified_velocity();
+            // evaluate linearized version of uTilde = u - 2/sigma cb2 gradv
+            Matrix< DDRMat > tModVelocity = tCMSATurbulence->modified_velocity_linearized();
 
             // compute norm( uTilde ) and threshold tNormA (for consistency with derivative computation)
             real tNormA = std::max( norm( tModVelocity ), mEpsilon );
@@ -249,15 +252,18 @@ namespace moris
 
             // if use reaction term
             real tTauS = 0;
-            if( mHasReaction )
+            if ( mHasReaction )
             {
-                // compute tau S
+                // compute tau S with linearized version of destruction term
                 tTauS =
-                        tCMSATurbulence->wall_destruction_coefficient()( 0 )
+                        2.0 * tCMSATurbulence->wall_destruction_coefficient()( 0 )
                         - tCMSATurbulence->production_coefficient()( 0 );
 
+                // compute the regularized absolute of source term
+                tTauS = std::sqrt( tTauS * tTauS + mEpsilon ) - std::sqrt( mEpsilon );
+
                 // add contribution of reaction term
-                tTau += std::pow( tTauS, mExponent );
+                tTau += std::pow( std::abs( tTauS ), mExponent );
             }
 
             // threshold tau
@@ -267,7 +273,7 @@ namespace moris
             if ( tTau > mEpsilon )
             {
                 // add contribution from the length scale derivative
-                mdPPdMasterDof( tDofIndex ) = - this->dlengthscaledmasteru( aDofTypes )
+                mdPPdMasterDof( tDofIndex ) = -this->dlengthscaledmasteru( aDofTypes )
                                             * ( std::pow( tTauA, mExponent - 1.0 ) * 2.0 * tNormA / std::pow( tHugn, 2.0 )
                                                     + std::pow( tTauK, mExponent - 1.0 ) * 8.0 * tCMSATurbulence->diffusion_coefficient()( 0 ) / std::pow( tHugn, 3.0 ) );
 
@@ -277,9 +283,10 @@ namespace moris
                     // if normA greater than threshold
                     if ( tNormA > mEpsilon )
                     {
-                        // add contribution to mdPPdMasterDof from modified velocity
+                        // add contribution to mdPPdMasterDof from linearized version of modified velocity
                         mdPPdMasterDof( tDofIndex ) +=
-                                std::pow( tTauA, mExponent - 1.0 ) * 2.0 * trans( tModVelocity ) * tCMSATurbulence->dmodvelocitydu( aDofTypes ) / ( tHugn * tNormA );
+                                std::pow( tTauA, mExponent - 1.0 ) * 2.0
+                                * trans( tModVelocity ) * tCMSATurbulence->dmodvelocitylinearizeddu( aDofTypes ) / ( tHugn * tNormA );
                     }
 
                     // compute tdtauKdu
@@ -287,16 +294,18 @@ namespace moris
                             std::pow( tTauK, mExponent - 1.0 ) * 4.0 * tCMSATurbulence->ddiffusioncoeffdu( aDofTypes ) / std::pow( tHugn, 2.0 );
 
                     // if use reaction term
-                    if( mHasReaction )
+                    if ( mHasReaction )
                     {
                         // compute tdtauSdu
                         mdPPdMasterDof( tDofIndex ) +=
-                                std::pow( tTauS, mExponent - 1.0 ) * ( tCMSATurbulence->dwalldestructioncoeffdu( aDofTypes ) - tCMSATurbulence->dproductioncoeffdu( aDofTypes ) );
+                                std::pow( tTauS, mExponent - 1.0 )
+                                * tTauS / std::sqrt( tTauS * tTauS + mEpsilon )
+                                * ( 2.0 * tCMSATurbulence->dwalldestructioncoeffdu( aDofTypes ) - tCMSATurbulence->dproductioncoeffdu( aDofTypes ) );
                     }
                 }
 
                 // scale
-                mdPPdMasterDof( tDofIndex ) = - std::pow( tTau, - ( mExponent + 1.0 ) / mExponent ) * mdPPdMasterDof( tDofIndex );
+                mdPPdMasterDof( tDofIndex ) = -std::pow( tTau, -( mExponent + 1.0 ) / mExponent ) * mdPPdMasterDof( tDofIndex );
             }
             else
             {
@@ -330,7 +339,7 @@ namespace moris
                     mMasterCM( static_cast< uint >( IWG_Constitutive_Type::SPALART_ALLMARAS_TURBULENCE ) );
 
             // compute and threshold the velocity norm (thresholding for consistency with derivatives)
-            const real tNorm = std::max( norm( tCMSATurbulence->modified_velocity() ), mEpsilon );
+            const real tNorm = std::max( norm( tCMSATurbulence->modified_velocity_linearized() ), mEpsilon );
 
             // get the velocity type FI
             Field_Interpolator* tVelocityFI =
@@ -342,7 +351,7 @@ namespace moris
 
             for ( uint iNode = 0; iNode < tNumNodes; iNode++ )
             {
-                tAbs += std::abs( dot( tCMSATurbulence->modified_velocity(), tVelocityFI->dnNdxn( 1 ).get_column( iNode ) ) );
+                tAbs += std::abs( dot( tCMSATurbulence->modified_velocity_linearized(), tVelocityFI->dnNdxn( 1 ).get_column( iNode ) ) );
             }
 
             // threshold tAbs
@@ -409,13 +418,13 @@ namespace moris
                         mMasterFIManager->get_field_interpolators_for_type( mMasterDofVelocity );
 
                 // compute and threshold the velocity norm (thresholding for consistency with derivatives)
-                const real tNorm = std::max( norm( tCMSATurbulence->modified_velocity() ), mEpsilon );
+                const real tNorm = std::max( norm( tCMSATurbulence->modified_velocity_linearized() ), mEpsilon );
 
                 // compute derivative of the modified velocity norm (compute only derivative if not thresholded)
                 Matrix< DDRMat > tdNormdu( 1, tFIDer->get_number_of_space_time_coefficients(), 0.0 );
                 if ( tNorm > mEpsilon && tCMSATurbulence->check_dof_dependency( aDofTypes ) )
                 {
-                    tdNormdu += trans( tCMSATurbulence->modified_velocity() ) * tCMSATurbulence->dmodvelocitydu( aDofTypes ) / tNorm;
+                    tdNormdu += trans( tCMSATurbulence->modified_velocity_linearized() ) * tCMSATurbulence->dmodvelocitylinearizeddu( aDofTypes ) / tNorm;
                 }
 
                 // get the abs term
@@ -423,7 +432,7 @@ namespace moris
                 real       tAbs      = 0.0;
                 for ( uint iNode = 0; iNode < tNumNodes; iNode++ )
                 {
-                    tAbs += std::abs( dot( tCMSATurbulence->modified_velocity(), tVelocityFI->dnNdxn( 1 ).get_column( iNode ) ) );
+                    tAbs += std::abs( dot( tCMSATurbulence->modified_velocity_linearized(), tVelocityFI->dnNdxn( 1 ).get_column( iNode ) ) );
                 }
 
                 // threshold tAbs
@@ -436,13 +445,13 @@ namespace moris
                     uint tNumNodes = tVelocityFI->dnNdxn( 1 ).n_cols();
                     for ( uint iNode = 0; iNode < tNumNodes; iNode++ )
                     {
-                        real tAdd = dot( tCMSATurbulence->modified_velocity(), tVelocityFI->dnNdxn( 1 ).get_column( iNode ) );
+                        real tAdd = dot( tCMSATurbulence->modified_velocity_linearized(), tVelocityFI->dnNdxn( 1 ).get_column( iNode ) );
 
                         // handle case that tAdd is smaller than threshold
                         if ( std::abs( tAdd ) > mEpsilon )
                         {
                             tdAbsdu += tAdd * trans( tVelocityFI->dnNdxn( 1 ).get_column( iNode ) )
-                            * tCMSATurbulence->dmodvelocitydu( aDofTypes ) / std::abs( tAdd );
+                                     * tCMSATurbulence->dmodvelocitylinearizeddu( aDofTypes ) / std::abs( tAdd );
                         }
                     }
                 }
--- projects/FEM/INT/src/SP/cl_FEM_SP_SUPG_Spalart_Allmaras_Turbulence.hpp
+++ projects/FEM/INT/src/SP/cl_FEM_SP_SUPG_Spalart_Allmaras_Turbulence.hpp
@@ -9,13 +9,13 @@
 #define SRC_FEM_CL_FEM_SP_SUPG_SPALART_ALLMARAS_TURBULENCE_HPP_
 
 #include <map>
-//MRS/COR/src
+// MRS/COR/src
 #include "typedefs.hpp"
 #include "cl_Cell.hpp"
-//LINALG/src
+// LINALG/src
 #include "cl_Matrix.hpp"
 #include "linalg_typedefs.hpp"
-//FEM/INT/src
+// FEM/INT/src
 #include "cl_FEM_Field_Interpolator.hpp"
 #include "cl_FEM_Constitutive_Model.hpp"
 #include "cl_FEM_Stabilization_Parameter.hpp"
@@ -30,161 +30,160 @@ namespace moris
         class SP_SUPG_Spalart_Allmaras_Turbulence : public Stabilization_Parameter
         {
 
-                //------------------------------------------------------------------------------
-            private :
+            //------------------------------------------------------------------------------
 
-                // default dof type
-                MSI::Dof_Type mMasterDofViscosity = MSI::Dof_Type::VISCOSITY;
-                MSI::Dof_Type mMasterDofVelocity  = MSI::Dof_Type::VX;
+          private:
+            // default dof type
+            MSI::Dof_Type mMasterDofViscosity = MSI::Dof_Type::VISCOSITY;
+            MSI::Dof_Type mMasterDofVelocity  = MSI::Dof_Type::VX;
 
-                // internal threshold
-                const real mEpsilon = MORIS_REAL_EPS;
+            // internal threshold
+            const real mEpsilon = MORIS_REAL_EPS;
 
-                // local constitutive enums
-                enum class IWG_Constitutive_Type
-                {
-                        SPALART_ALLMARAS_TURBULENCE,
-                        MAX_ENUM
-                };
+            // local constitutive enums
+            enum class IWG_Constitutive_Type
+            {
+                SPALART_ALLMARAS_TURBULENCE,
+                MAX_ENUM
+            };
 
-                // flag for evaluation
-                bool mLengthScaleEval = true;
-                moris::Matrix< DDBMat > mdLengthScaledMasterDofEval;
+            // flag for evaluation
+            bool                    mLengthScaleEval = true;
+            moris::Matrix< DDBMat > mdLengthScaledMasterDofEval;
 
-                // storage
-                real mLengthScale;
-                moris::Cell< Matrix< DDRMat > > mdLengthScaledMasterDof;
+            // storage
+            real                            mLengthScale;
+            moris::Cell< Matrix< DDRMat > > mdLengthScaledMasterDof;
 
-                /*
-                 * Rem: mParameters( 0 ) -
-                 * 0 - exponent for (tauA^r + tauK^r + tauK^r)^(-1/r)
-                 * 1 - use reaction term 0.0 for no, 1.0 for yes
-                 */
+            /*
+             * Rem: mParameters( 0 ) -
+             * 0 - exponent for (tauA^r + tauK^r + tauK^r)^(-1/r)
+             * 1 - use reaction term 0.0 for no, 1.0 for yes
+             */
 
-                // parameters
-                real mExponent = 2.0;
-                real mHasReaction = 1.0;
+            // parameters
+            bool mHasReaction = true;
+            real mExponent    = 2.0;
 
-            public:
+          public:
+            //------------------------------------------------------------------------------
+            /*
+             * constructor
+             */
+            SP_SUPG_Spalart_Allmaras_Turbulence();
 
-                //------------------------------------------------------------------------------
-                /*
-                 * constructor
-                 */
-                SP_SUPG_Spalart_Allmaras_Turbulence();
+            //------------------------------------------------------------------------------
+            /**
+             * trivial destructor
+             */
+            ~SP_SUPG_Spalart_Allmaras_Turbulence(){};
 
-                //------------------------------------------------------------------------------
-                /**
-                 * trivial destructor
-                 */
-                ~SP_SUPG_Spalart_Allmaras_Turbulence(){};
+            //------------------------------------------------------------------------------
+            /**
+             * set parameters
+             * @param[ in ] aParameters a list of parameters
+             */
+            void set_parameters( moris::Cell< Matrix< DDRMat > > aParameters );
 
-                //------------------------------------------------------------------------------
-                /**
-                 * set parameters
-                 * @param[ in ] aParameters a list of parameters
-                 */
-                void set_parameters( moris::Cell< Matrix< DDRMat > > aParameters );
+            //------------------------------------------------------------------------------
+            /**
+             * set function pointers for evaluation
+             */
+            void set_function_pointers();
 
-                //------------------------------------------------------------------------------
-                /**
-                 * set function pointers for evaluation
-                 */
-                void set_function_pointers();
+            //------------------------------------------------------------------------------
+            /**
+             * reset evaluation flags
+             * child implementation
+             */
+            void reset_eval_flags();
 
-                //------------------------------------------------------------------------------
-                /**
-                 * reset evaluation flags
-                 * child implementation
-                 */
-                void reset_eval_flags();
+            //------------------------------------------------------------------------------
+            /**
+             * set dof types
+             * @param[ in ] aDofTypes a cell of cell of dof types
+             * @param[ in ] aDofStrings list of strings describing the dof types
+             * @param[ in ] aIsMaster enum for master or slave
+             */
+            void set_dof_type_list(
+                    moris::Cell< moris::Cell< MSI::Dof_Type > >& aDofTypes,
+                    moris::Cell< std::string >&                  aDofStrings,
+                    mtk::Master_Slave                            aIsMaster = mtk::Master_Slave::MASTER );
 
-                //------------------------------------------------------------------------------
-                /**
-                 * set dof types
-                 * @param[ in ] aDofTypes a cell of cell of dof types
-                 * @param[ in ] aDofStrings list of strings describing the dof types
-                 * @param[ in ] aIsMaster enum for master or slave
-                 */
-                void set_dof_type_list(
-                        moris::Cell< moris::Cell< MSI::Dof_Type > > & aDofTypes,
-                        moris::Cell< std::string >                  & aDofStrings,
-                        mtk::Master_Slave                             aIsMaster = mtk::Master_Slave::MASTER );
+            //------------------------------------------------------------------------------
+            /**
+             * create a global dof type list including constitutive and property dependencies
+             * child implementation
+             */
+            void build_global_dof_type_list();
 
-                //------------------------------------------------------------------------------
-                /**
-                 * create a global dof type list including constitutive and property dependencies
-                 * child implementation
-                 */
-                void build_global_dof_type_list();
+            //------------------------------------------------------------------------------
+            /**
+             * set dv types
+             * @param[ in ] aDvTypes   a cell of group of dv types
+             * @param[ in ] aDvStrings list of strings describing the dv types
+             * @param[ in ] aIsMaster enum for master or slave
+             */
+            void
+            set_dv_type_list(
+                    moris::Cell< moris::Cell< PDV_Type > >& aDvTypes,
+                    moris::Cell< std::string >&             aDvStrings,
+                    mtk::Master_Slave                       aIsMaster = mtk::Master_Slave::MASTER )
+            {
+                Stabilization_Parameter::set_dv_type_list( aDvTypes, aIsMaster );
+            }
 
-                //------------------------------------------------------------------------------
-                /**
-                 * set dv types
-                 * @param[ in ] aDvTypes   a cell of group of dv types
-                 * @param[ in ] aDvStrings list of strings describing the dv types
-                 * @param[ in ] aIsMaster enum for master or slave
-                 */
-                void set_dv_type_list(
-                        moris::Cell< moris::Cell< PDV_Type > > & aDvTypes,
-                        moris::Cell< std::string >             & aDvStrings,
-                        mtk::Master_Slave                        aIsMaster = mtk::Master_Slave::MASTER )
-                {
-                    Stabilization_Parameter::set_dv_type_list( aDvTypes, aIsMaster );
-                }
+            //------------------------------------------------------------------------------
+            /**
+             * evaluate the penalty parameter value
+             */
+            void eval_SP();
 
-                //------------------------------------------------------------------------------
-                /**
-                 * evaluate the penalty parameter value
-                 */
-                void eval_SP();
+            //------------------------------------------------------------------------------
+            /**
+             * evaluate the penalty parameter derivative wrt to a master dof type
+             * @param[ in ] aDofTypes a dof type wrt which the derivative is evaluated
+             */
+            void eval_dSPdMasterDOF( const moris::Cell< MSI::Dof_Type >& aDofTypes );
 
-                //------------------------------------------------------------------------------
-                /**
-                 * evaluate the penalty parameter derivative wrt to a master dof type
-                 * @param[ in ] aDofTypes a dof type wrt which the derivative is evaluated
-                 */
-                void eval_dSPdMasterDOF( const moris::Cell< MSI::Dof_Type > & aDofTypes );
+            //------------------------------------------------------------------------------
+            /**
+             * evaluate the penalty parameter derivative wrt to a master dv type
+             * @param[ in ] aDvTypes a dv type wrt which the derivative is evaluated
+             */
+            void
+            eval_dSPdMasterDV( const moris::Cell< PDV_Type >& aDvTypes )
+            {
+                MORIS_ERROR( false, "SP_SUPG_Spalart_Allmaras_Turbulence::eval_dSPdMasterDV - not implemented." );
+            }
 
-                //------------------------------------------------------------------------------
-                /**
-                 * evaluate the penalty parameter derivative wrt to a master dv type
-                 * @param[ in ] aDvTypes a dv type wrt which the derivative is evaluated
-                 */
-                void eval_dSPdMasterDV( const moris::Cell< PDV_Type > & aDvTypes )
-                {
-                    MORIS_ERROR( false, "SP_SUPG_Spalart_Allmaras_Turbulence::eval_dSPdMasterDV - not implemented." );
-                }
+            //------------------------------------------------------------------------------
 
-                //------------------------------------------------------------------------------
+          private:
+            //------------------------------------------------------------------------------
+            /**
+             * return the length scale
+             * @param[ out ] mLengthScale length scale for SUPG
+             */
+            real length_scale();
 
-            private:
+            /**
+             * evaluate the length scale parameter
+             */
+            void eval_length_scale();
 
-                //------------------------------------------------------------------------------
-                /**
-                 * return the length scale
-                 * @param[ out ] mLengthScale length scale for SUPG
-                 */
-                real length_scale();
+            //------------------------------------------------------------------------------
+            /**
+             * return the length scale derivative wrt to a master dof type
+             * @param[ in ] aDofTypes a dof type wrt which the derivative is evaluated
+             */
+            const Matrix< DDRMat >& dlengthscaledmasteru( const moris::Cell< MSI::Dof_Type >& aDofTypes );
 
-                /**
-                 * evaluate the length scale parameter
-                 */
-                void eval_length_scale();
-
-                //------------------------------------------------------------------------------
-                /**
-                 * return the length scale derivative wrt to a master dof type
-                 * @param[ in ] aDofTypes a dof type wrt which the derivative is evaluated
-                 */
-                const Matrix< DDRMat > & dlengthscaledmasteru( const moris::Cell< MSI::Dof_Type > & aDofTypes );
-
-                /**
-                 * evaluate the length scale derivative wrt to a master dof type
-                 * @param[ in ] aDofTypes a dof type wrt which the derivative is evaluated
-                 */
-                void eval_dlengthscaledmasteru( const moris::Cell< MSI::Dof_Type > & aDofTypes );
-
+            /**
+             * evaluate the length scale derivative wrt to a master dof type
+             * @param[ in ] aDofTypes a dof type wrt which the derivative is evaluated
+             */
+            void eval_dlengthscaledmasteru( const moris::Cell< MSI::Dof_Type >& aDofTypes );
         };
         //------------------------------------------------------------------------------
     } /* namespace fem */
