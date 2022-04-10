48fff3bac1096f52401ef3b9227a49979c575a0a
maute <maute@pegasus>
2022-04-10 11:35:50 -0600

    added property to ignore residual of time continuity IWG; used for inexact Newton in combination with pseudo transient continuation; if property is not set; residual is considered by default

--- projects/FEM/INT/src/IWG/cl_FEM_IWG_Time_Continuity_Dof.hpp
+++ projects/FEM/INT/src/IWG/cl_FEM_IWG_Time_Continuity_Dof.hpp
@@ -9,13 +9,13 @@
 #define SRC_FEM_CL_FEM_IWG_TIME_CONTINUITY_DOF_HPP_
 
 #include <map>
-#include "typedefs.hpp"                     //MRS/COR/src
-#include "cl_Cell.hpp"                      //MRS/CON/src
+#include "typedefs.hpp"    //MRS/COR/src
+#include "cl_Cell.hpp"     //MRS/CON/src
 
-#include "cl_Matrix.hpp"                    //LINALG/src
-#include "linalg_typedefs.hpp"              //LINALG/src
+#include "cl_Matrix.hpp"          //LINALG/src
+#include "linalg_typedefs.hpp"    //LINALG/src
 
-#include "cl_FEM_IWG.hpp"                   //FEM/INT/src
+#include "cl_FEM_IWG.hpp"    //FEM/INT/src
 
 namespace moris
 {
@@ -26,65 +26,66 @@ namespace moris
         class IWG_Time_Continuity_Dof : public IWG
         {
 
-                //------------------------------------------------------------------------------
-            public:
+            //------------------------------------------------------------------------------
 
-                enum class IWG_Property_Type
-                {
-                    WEIGHT_CURRENT,
-                    WEIGHT_PREVIOUS,
-                    INITIAL_CONDITION,
-                    MAX_ENUM
-                };
+          public:
+            enum class IWG_Property_Type
+            {
+                WEIGHT_CURRENT,
+                WEIGHT_PREVIOUS,
+                WEIGHT_RESIDUAL,
+                INITIAL_CONDITION,
+                MAX_ENUM
+            };
 
-                //------------------------------------------------------------------------------
-                /*
-                 *  constructor
-                 */
-                IWG_Time_Continuity_Dof();
+            //------------------------------------------------------------------------------
+            /*
+             *  constructor
+             */
+            IWG_Time_Continuity_Dof();
 
-                //------------------------------------------------------------------------------
-                /**
-                 * trivial destructor
-                 */
-                ~IWG_Time_Continuity_Dof(){};
+            //------------------------------------------------------------------------------
+            /**
+             * trivial destructor
+             */
+            ~IWG_Time_Continuity_Dof(){};
 
-                //------------------------------------------------------------------------------
-                /**
-                 * compute the residual
-                 * @param[ in ] aWStar weight associated to the evaluation point
-                 */
-                void compute_residual( real aWStar );
+            //------------------------------------------------------------------------------
+            /**
+             * compute the residual
+             * @param[ in ] aWStar weight associated to the evaluation point
+             */
+            void compute_residual( real aWStar );
 
-                //------------------------------------------------------------------------------
-                /**
-                 * compute the jacobian
-                 * @param[ in ] aWStar weight associated to the evaluation point
-                 */
-                void compute_jacobian( real aWStar );
+            //------------------------------------------------------------------------------
+            /**
+             * compute the jacobian
+             * @param[ in ] aWStar weight associated to the evaluation point
+             */
+            void compute_jacobian( real aWStar );
 
-                //------------------------------------------------------------------------------
-                /**
-                 * compute the residual and the jacobian
-                 * @param[ in ] aWStar weight associated to the evaluation point
-                 */
-                void compute_jacobian_and_residual( real aWStar );
+            //------------------------------------------------------------------------------
+            /**
+             * compute the residual and the jacobian
+             * @param[ in ] aWStar weight associated to the evaluation point
+             */
+            void compute_jacobian_and_residual( real aWStar );
 
-                //------------------------------------------------------------------------------
-                /**
-                 * compute dR(n)/du(n-1)
-                 * @param[ in ] aWStar weight associated to the evaluation point
-                 */
-                void compute_jacobian_previous( real aWStar );
+            //------------------------------------------------------------------------------
+            /**
+             * compute dR(n)/du(n-1)
+             * @param[ in ] aWStar weight associated to the evaluation point
+             */
+            void compute_jacobian_previous( real aWStar );
 
-                //------------------------------------------------------------------------------
-                /**
-                 * compute the derivative of the residual wrt design variables
-                 * @param[ in ] aWStar weight associated to the evaluation point
-                 */
-                void compute_dRdp( real aWStar );
+            //------------------------------------------------------------------------------
+            /**
+             * compute the derivative of the residual wrt design variables
+             * @param[ in ] aWStar weight associated to the evaluation point
+             */
+            void compute_dRdp( real aWStar );
 
-                //------------------------------------------------------------------------------
+            //------------------------------------------------------------------------------
         };
         //------------------------------------------------------------------------------
     } /* namespace fem */
