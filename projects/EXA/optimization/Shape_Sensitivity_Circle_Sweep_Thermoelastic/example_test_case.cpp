80f8a39050fe7e55a50d899d894711b87bf832e9
maute <maute@pegasus>
2022-04-14 14:01:45 -0600

    reformated with Clang

--- projects/EXA/optimization/Shape_Sensitivity_Circle_Sweep_Thermoelastic/example_test_case.cpp
+++ projects/EXA/optimization/Shape_Sensitivity_Circle_Sweep_Thermoelastic/example_test_case.cpp
@@ -1,24 +1,29 @@
-//
-// example specific interface to moris
-//
-
+/*
+ * Copyright (c) 2022 University of Colorado
+ * Licensed under the MIT license. See LICENSE.txt file in the MORIS root for details.
+ *
+ *------------------------------------------------------------------------------------
+ *
+ * example specific interface to moris
+ *
+ */
 #include <catch.hpp>
 
-#include "cl_Logger.hpp" // MRS/IOS/src
+#include "cl_Logger.hpp"    // MRS/IOS/src
 #include "HDF5_Tools.hpp"
 
 using namespace moris;
 
 //---------------------------------------------------------------
 
-int fn_WRK_Workflow_Main_Interface( int argc, char * argv[] );
+int fn_WRK_Workflow_Main_Interface( int argc, char* argv[] );
 
 //---------------------------------------------------------------
 
-TEST_CASE("Shape_Sensitivity_Circle_Sweep_Thermoelastic",
-        "[moris],[example],[optimization],[sweep],[sweep_thermoelastic]")
+TEST_CASE( "Shape_Sensitivity_Circle_Sweep_Thermoelastic",
+        "[moris],[example],[optimization],[sweep],[sweep_thermoelastic]" )
 {
-    if (par_size() == 1)
+    if ( par_size() == 1 )
     {
         // define command line call
         int argc = 2;
@@ -26,57 +31,57 @@ TEST_CASE("Shape_Sensitivity_Circle_Sweep_Thermoelastic",
         char tString1[] = "";
         char tString2[] = "Shape_Sensitivity_Circle_Sweep_Thermoelastic.so";
 
-        char * argv[2] = {tString1,tString2};
+        char* argv[ 2 ] = { tString1, tString2 };
 
         // call to performance manager main interface
         int tRet = fn_WRK_Workflow_Main_Interface( argc, argv );
 
         // catch test statements should follow
-        REQUIRE( tRet ==  0 );
+        REQUIRE( tRet == 0 );
 
         // Sweep HDF5 file
-        hid_t tFileID = open_hdf5_file( "shape_opt_test.hdf5" );
+        hid_t  tFileID = open_hdf5_file( "shape_opt_test.hdf5" );
         herr_t tStatus = 0;
 
         // Declare sensitivity matrices for comparison
-        Matrix<DDRMat> tObjectiveAnalytical;
-        Matrix<DDRMat> tConstraintsAnalytical;
-        Matrix<DDRMat> tObjectiveFD;
-        Matrix<DDRMat> tConstraintsFD;
+        Matrix< DDRMat > tObjectiveAnalytical;
+        Matrix< DDRMat > tConstraintsAnalytical;
+        Matrix< DDRMat > tObjectiveFD;
+        Matrix< DDRMat > tConstraintsFD;
 
         // Read analytical sensitivities
-        load_matrix_from_hdf5_file( tFileID, "objective_gradients eval_1-1 analytical", tObjectiveAnalytical, tStatus);
-        load_matrix_from_hdf5_file( tFileID, "constraint_gradients eval_1-1 analytical", tConstraintsAnalytical, tStatus);
-        REQUIRE(tObjectiveAnalytical.length() == tConstraintsAnalytical.length()); // one objective and one constraint for this problem only
+        load_matrix_from_hdf5_file( tFileID, "objective_gradients eval_1-1 analytical", tObjectiveAnalytical, tStatus );
+        load_matrix_from_hdf5_file( tFileID, "constraint_gradients eval_1-1 analytical", tConstraintsAnalytical, tStatus );
+        REQUIRE( tObjectiveAnalytical.length() == tConstraintsAnalytical.length() );    // one objective and one constraint for this problem only
 
         // Read FD sensitivities and compare
-        Cell<std::string> tFDTypes = {"fd_forward", "fd_backward", "fd_central"};
-        for (uint tFDIndex = 0; tFDIndex < tFDTypes.size(); tFDIndex++)
+        Cell< std::string > tFDTypes = { "fd_forward", "fd_backward", "fd_central" };
+        for ( uint tFDIndex = 0; tFDIndex < tFDTypes.size(); tFDIndex++ )
         {
-            load_matrix_from_hdf5_file( tFileID, "objective_gradients eval_1-1 epsilon_1-1 " + tFDTypes(tFDIndex), tObjectiveFD, tStatus);
-            load_matrix_from_hdf5_file( tFileID, "constraint_gradients eval_1-1 epsilon_1-1 " + tFDTypes(tFDIndex), tConstraintsFD, tStatus);
+            load_matrix_from_hdf5_file( tFileID, "objective_gradients eval_1-1 epsilon_1-1 " + tFDTypes( tFDIndex ), tObjectiveFD, tStatus );
+            load_matrix_from_hdf5_file( tFileID, "constraint_gradients eval_1-1 epsilon_1-1 " + tFDTypes( tFDIndex ), tConstraintsFD, tStatus );
 
-            REQUIRE(tObjectiveAnalytical.length() == tObjectiveFD.length());
-            REQUIRE(tConstraintsAnalytical.length() == tConstraintsFD.length());
+            REQUIRE( tObjectiveAnalytical.length() == tObjectiveFD.length() );
+            REQUIRE( tConstraintsAnalytical.length() == tConstraintsFD.length() );
 
-            for (uint tADVIndex = 0; tADVIndex < tObjectiveAnalytical.length(); tADVIndex++)
+            for ( uint tADVIndex = 0; tADVIndex < tObjectiveAnalytical.length(); tADVIndex++ )
             {
-                MORIS_LOG_INFO("Check derivative of objective  wrt. ADV(%i):  analytical  %12.5e, finite difference (%s) %12.5e, percent error %12.5e.",
+                MORIS_LOG_INFO( "Check derivative of objective  wrt. ADV(%i):  analytical  %12.5e, finite difference (%s) %12.5e, percent error %12.5e.",
                         tADVIndex,
-                        tObjectiveAnalytical(tADVIndex),
-                        tFDTypes(tFDIndex).c_str(),
-                        tObjectiveFD(tADVIndex),
-                        100*std::abs((tObjectiveAnalytical(tADVIndex)-tObjectiveFD(tADVIndex))/tObjectiveFD(tADVIndex)));
+                        tObjectiveAnalytical( tADVIndex ),
+                        tFDTypes( tFDIndex ).c_str(),
+                        tObjectiveFD( tADVIndex ),
+                        100 * std::abs( ( tObjectiveAnalytical( tADVIndex ) - tObjectiveFD( tADVIndex ) ) / tObjectiveFD( tADVIndex ) ) );
 
-                MORIS_LOG_INFO("Check derivative of constraint wrt. ADV(%i):  analytical  %12.5e, finite difference (%s) %12.5e, percent error %12.5e.",
+                MORIS_LOG_INFO( "Check derivative of constraint wrt. ADV(%i):  analytical  %12.5e, finite difference (%s) %12.5e, percent error %12.5e.",
                         tADVIndex,
-                        tConstraintsAnalytical(tADVIndex),
-                        tFDTypes(tFDIndex).c_str(),
-                        tConstraintsFD(tADVIndex),
-                        100*std::abs((tConstraintsAnalytical(tADVIndex)-tConstraintsFD(tADVIndex))/tConstraintsFD(tADVIndex)));
+                        tConstraintsAnalytical( tADVIndex ),
+                        tFDTypes( tFDIndex ).c_str(),
+                        tConstraintsFD( tADVIndex ),
+                        100 * std::abs( ( tConstraintsAnalytical( tADVIndex ) - tConstraintsFD( tADVIndex ) ) / tConstraintsFD( tADVIndex ) ) );
 
-                CHECK(tObjectiveAnalytical(tADVIndex)   == Approx(tObjectiveFD(tADVIndex)).scale(1000));
-                CHECK(tConstraintsAnalytical(tADVIndex) == Approx(tConstraintsFD(tADVIndex)).scale(1000));
+                CHECK( tObjectiveAnalytical( tADVIndex ) == Approx( tObjectiveFD( tADVIndex ) ).scale( 1000 ) );
+                CHECK( tConstraintsAnalytical( tADVIndex ) == Approx( tConstraintsFD( tADVIndex ) ).scale( 1000 ) );
             }
         }
 
@@ -85,10 +90,10 @@ TEST_CASE("Shape_Sensitivity_Circle_Sweep_Thermoelastic",
     }
 }
 
-TEST_CASE("Shape_Sensitivity_Circle_Sweep_Thermoelastic_Staggered",
-        "[moris],[example],[optimization],[sweep],[sweep_thermoelastic_staggered]")
+TEST_CASE( "Shape_Sensitivity_Circle_Sweep_Thermoelastic_Staggered",
+        "[moris],[example],[optimization],[sweep],[sweep_thermoelastic_staggered]" )
 {
-    if (par_size() == 1)
+    if ( par_size() == 1 )
     {
         // define command line call
         int argc = 2;
@@ -96,57 +101,57 @@ TEST_CASE("Shape_Sensitivity_Circle_Sweep_Thermoelastic_Staggered",
         char tString1[] = "";
         char tString2[] = "Shape_Sensitivity_Circle_Sweep_Thermoelastic_Staggered.so";
 
-        char * argv[2] = {tString1,tString2};
+        char* argv[ 2 ] = { tString1, tString2 };
 
         // call to performance manager main interface
         int tRet = fn_WRK_Workflow_Main_Interface( argc, argv );
 
         // catch test statements should follow
-        REQUIRE( tRet ==  0 );
+        REQUIRE( tRet == 0 );
 
         // Sweep HDF5 file
-        hid_t tFileID = open_hdf5_file( "shape_opt_test.hdf5" );
+        hid_t  tFileID = open_hdf5_file( "shape_opt_test.hdf5" );
         herr_t tStatus = 0;
 
         // Declare sensitivity matrices for comparison
-        Matrix<DDRMat> tObjectiveAnalytical;
-        Matrix<DDRMat> tConstraintsAnalytical;
-        Matrix<DDRMat> tObjectiveFD;
-        Matrix<DDRMat> tConstraintsFD;
+        Matrix< DDRMat > tObjectiveAnalytical;
+        Matrix< DDRMat > tConstraintsAnalytical;
+        Matrix< DDRMat > tObjectiveFD;
+        Matrix< DDRMat > tConstraintsFD;
 
         // Read analytical sensitivities
-        load_matrix_from_hdf5_file( tFileID, "objective_gradients eval_1-1 analytical", tObjectiveAnalytical, tStatus);
-        load_matrix_from_hdf5_file( tFileID, "constraint_gradients eval_1-1 analytical", tConstraintsAnalytical, tStatus);
-        REQUIRE(tObjectiveAnalytical.length() == tConstraintsAnalytical.length()); // one objective and one constraint for this problem only
+        load_matrix_from_hdf5_file( tFileID, "objective_gradients eval_1-1 analytical", tObjectiveAnalytical, tStatus );
+        load_matrix_from_hdf5_file( tFileID, "constraint_gradients eval_1-1 analytical", tConstraintsAnalytical, tStatus );
+        REQUIRE( tObjectiveAnalytical.length() == tConstraintsAnalytical.length() );    // one objective and one constraint for this problem only
 
         // Read FD sensitivities and compare
-        Cell<std::string> tFDTypes = {"fd_forward", "fd_backward", "fd_central"};
-        for (uint tFDIndex = 0; tFDIndex < tFDTypes.size(); tFDIndex++)
+        Cell< std::string > tFDTypes = { "fd_forward", "fd_backward", "fd_central" };
+        for ( uint tFDIndex = 0; tFDIndex < tFDTypes.size(); tFDIndex++ )
         {
-            load_matrix_from_hdf5_file( tFileID, "objective_gradients eval_1-1 epsilon_1-1 " + tFDTypes(tFDIndex), tObjectiveFD, tStatus);
-            load_matrix_from_hdf5_file( tFileID, "constraint_gradients eval_1-1 epsilon_1-1 " + tFDTypes(tFDIndex), tConstraintsFD, tStatus);
+            load_matrix_from_hdf5_file( tFileID, "objective_gradients eval_1-1 epsilon_1-1 " + tFDTypes( tFDIndex ), tObjectiveFD, tStatus );
+            load_matrix_from_hdf5_file( tFileID, "constraint_gradients eval_1-1 epsilon_1-1 " + tFDTypes( tFDIndex ), tConstraintsFD, tStatus );
 
-            REQUIRE(tObjectiveAnalytical.length() == tObjectiveFD.length());
-            REQUIRE(tConstraintsAnalytical.length() == tConstraintsFD.length());
+            REQUIRE( tObjectiveAnalytical.length() == tObjectiveFD.length() );
+            REQUIRE( tConstraintsAnalytical.length() == tConstraintsFD.length() );
 
-            for (uint tADVIndex = 0; tADVIndex < tObjectiveAnalytical.length(); tADVIndex++)
+            for ( uint tADVIndex = 0; tADVIndex < tObjectiveAnalytical.length(); tADVIndex++ )
             {
-                MORIS_LOG_INFO("Check derivative of objective  wrt. ADV(%i):  analytical  %12.5e, finite difference (%s) %12.5e, percent error %12.5e.",
+                MORIS_LOG_INFO( "Check derivative of objective  wrt. ADV(%i):  analytical  %12.5e, finite difference (%s) %12.5e, percent error %12.5e.",
                         tADVIndex,
-                        tObjectiveAnalytical(tADVIndex),
-                        tFDTypes(tFDIndex).c_str(),
-                        tObjectiveFD(tADVIndex),
-                        100*std::abs((tObjectiveAnalytical(tADVIndex)-tObjectiveFD(tADVIndex))/tObjectiveFD(tADVIndex)));
+                        tObjectiveAnalytical( tADVIndex ),
+                        tFDTypes( tFDIndex ).c_str(),
+                        tObjectiveFD( tADVIndex ),
+                        100 * std::abs( ( tObjectiveAnalytical( tADVIndex ) - tObjectiveFD( tADVIndex ) ) / tObjectiveFD( tADVIndex ) ) );
 
-                MORIS_LOG_INFO("Check derivative of constraint wrt. ADV(%i):  analytical  %12.5e, finite difference (%s) %12.5e, percent error %12.5e.",
+                MORIS_LOG_INFO( "Check derivative of constraint wrt. ADV(%i):  analytical  %12.5e, finite difference (%s) %12.5e, percent error %12.5e.",
                         tADVIndex,
-                        tConstraintsAnalytical(tADVIndex),
-                        tFDTypes(tFDIndex).c_str(),
-                        tConstraintsFD(tADVIndex),
-                        100*std::abs((tConstraintsAnalytical(tADVIndex)-tConstraintsFD(tADVIndex))/tConstraintsFD(tADVIndex)));
+                        tConstraintsAnalytical( tADVIndex ),
+                        tFDTypes( tFDIndex ).c_str(),
+                        tConstraintsFD( tADVIndex ),
+                        100 * std::abs( ( tConstraintsAnalytical( tADVIndex ) - tConstraintsFD( tADVIndex ) ) / tConstraintsFD( tADVIndex ) ) );
 
-                CHECK(tObjectiveAnalytical(tADVIndex) == Approx(tObjectiveFD(tADVIndex)).scale(1000));
-                CHECK(tConstraintsAnalytical(tADVIndex) == Approx(tConstraintsFD(tADVIndex)).scale(1000));
+                CHECK( tObjectiveAnalytical( tADVIndex ) == Approx( tObjectiveFD( tADVIndex ) ).scale( 1000 ) );
+                CHECK( tConstraintsAnalytical( tADVIndex ) == Approx( tConstraintsFD( tADVIndex ) ).scale( 1000 ) );
             }
         }
 
@@ -155,8 +160,8 @@ TEST_CASE("Shape_Sensitivity_Circle_Sweep_Thermoelastic_Staggered",
     }
 }
 
-TEST_CASE("Shape_Sensitivity_Circle_Sweep_Thermoelastic_GCMMA",
-        "[moris],[example],[optimization],[gcmma_thermoelastic]")
+TEST_CASE( "Shape_Sensitivity_Circle_Sweep_Thermoelastic_GCMMA",
+        "[moris],[example],[optimization],[gcmma_thermoelastic]" )
 {
     // define command line call
     int argc = 2;
@@ -164,12 +169,11 @@ TEST_CASE("Shape_Sensitivity_Circle_Sweep_Thermoelastic_GCMMA",
     char tString1[] = "";
     char tString2[] = "Shape_Sensitivity_Circle_Sweep_Thermoelastic_GCMMA.so";
 
-    char * argv[2] = {tString1,tString2};
+    char* argv[ 2 ] = { tString1, tString2 };
 
     // call to performance manager main interface
     int tRet = fn_WRK_Workflow_Main_Interface( argc, argv );
 
     // catch test statements should follow
-    REQUIRE( tRet ==  0 );
-
+    REQUIRE( tRet == 0 );
 }
