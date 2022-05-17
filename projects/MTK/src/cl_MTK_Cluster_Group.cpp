241c02a4e11e028e9e605493cdd15b231c54cf41
wunsch <wunsch@tomcat>
2022-05-17 10:41:38 -0600

    New mtk::Cluster_Group class

---
+++ projects/MTK/src/cl_MTK_Cluster_Group.cpp
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2022 University of Colorado 
+ * Licensed under the MIT license. See LICENSE.txt file in the MORIS root for details. 
+ * 
+ * ------------------------------------------------------------------------------------ 
+ * 
+ * cl_MTK_Cluster_Group.cpp  
+ * 
+ */
+
+#include "cl_MTK_Cluster_Group.hpp"
+
+namespace moris
+{
+    namespace mtk
+    {
+        //------------------------------------------------------------------------------
+
+        Cluster_Group::Cluster_Group(
+                moris::Cell< Cluster const * > aClusters,
+                const moris_index              aBsplineMeshListIndex )
+                : mClusters( aClusters )
+                , mBsplineMeshListIndex( aBsplineMeshListIndex )
+        {
+            // only initialize the cluster group with the list of clusters, do nothing else in constructor
+        }
+
+        //------------------------------------------------------------------------------
+
+        moris::Cell< Cluster const* > const& 
+        Cluster_Group::get_clusters_in_group() const
+        {
+            return mClusters;
+        }
+
+        //------------------------------------------------------------------------------
+
+        moris::real
+        Cluster_Group::compute_cluster_group_volume(
+                const mtk::Primary_Void aPrimaryOrVoid,
+                const mtk::Master_Slave aIsMaster ) const
+        {
+            // initialize volume measure
+            real tVolume = 0.0;
+
+            // sum up volume over all clusters 
+            for( uint iCluster = 0; iCluster < mClusters.size(); iCluster++ )
+            {
+                tVolume += mClusters( iCluster )->compute_cluster_cell_measure( aPrimaryOrVoid, aIsMaster );
+            }
+
+            // return sum
+            return tVolume;
+        }
+
+        //------------------------------------------------------------------------------
+        
+        moris::real
+        Cluster_Group::compute_cluster_group_volume_derivative(
+                const Matrix< DDRMat > & aPerturbedVertexCoords,
+                uint aDirection,
+                const mtk::Primary_Void aPrimaryOrVoid,
+                const mtk::Master_Slave aIsMaster ) const
+        {
+            MORIS_ERROR( false, 
+                "Cluster_Group::compute_cluster_group_volume_derivative() - not implemented yet." );
+
+            return 0.0;
+        }
+
+        //------------------------------------------------------------------------------
+        
+        moris::real
+        Cluster_Group::compute_cluster_group_side_measure(
+                const mtk::Primary_Void aPrimaryOrVoid,
+                const mtk::Master_Slave aIsMaster ) const
+        {
+            // initialize interface/boundary surface/length
+            real tInterfaceSize = 0.0;
+
+            // sum up interface/boundary surface/length over all clusters 
+            for( uint iCluster = 0; iCluster < mClusters.size(); iCluster++ )
+            {
+                tInterfaceSize += mClusters( iCluster )->compute_cluster_cell_side_measure( aPrimaryOrVoid, aIsMaster );
+            }
+
+            // return sum
+            return tInterfaceSize;
+        }
+
+        //------------------------------------------------------------------------------
+        
+        moris::real
+        Cluster_Group::compute_cluster_group_side_measure_derivative(
+                const Matrix< DDRMat > & aPerturbedVertexCoords,
+                uint aDirection,
+                const mtk::Primary_Void aPrimaryOrVoid,
+                const mtk::Master_Slave aIsMaster ) const
+        {
+            MORIS_ERROR( false, 
+                "Cluster_Group::compute_cluster_group_side_measure_derivative() - not implemented yet." );
+
+            return 0.0;
+        }
+
+
+        //------------------------------------------------------------------------------
+
+    } // namespace moris
+} // namespace mtk
---
+++ projects/MTK/src/cl_MTK_Cluster_Group.hpp
@@ -0,0 +1,125 @@
+/*
+ * Copyright (c) 2022 University of Colorado 
+ * Licensed under the MIT license. See LICENSE.txt file in the MORIS root for details. 
+ * 
+ * ------------------------------------------------------------------------------------ 
+ * 
+ * cl_MTK_Cluster_Group.hpp  
+ * 
+ */
+#ifndef SRC_cl_MTK_Cluster_Group
+#define SRC_cl_MTK_Cluster_Group
+
+#include "cl_Cell.hpp"
+#include "cl_MTK_Cluster.hpp"
+
+namespace moris
+{
+    namespace mtk
+    {
+
+        class Cluster_Group
+        {
+            //------------------------------------------------------------------------------
+
+          private:
+
+            // list of clusters in group
+            moris::Cell< moris::mtk::Cluster const* > mClusters;
+
+            // index on which B-spline mesh the cluster group is valid
+            // NOTE: this is the index of the B-spline mesh wrt. to the list of B-spline meshes enriched using the current Lagrange mesh. 
+            // NOTE: This corresponds to the mesh indices used by MSI.
+            moris_index mBsplineMeshListIndex;
+
+            //------------------------------------------------------------------------------
+
+          public:
+          
+            Cluster_Group(
+                    moris::Cell< moris::mtk::Cluster const * > aClusters,
+                    const moris_index                          aBsplineMeshListIndex );
+
+            ~Cluster_Group(){};
+
+            //------------------------------------------------------------------------------
+
+            /**
+             * @brief Get a the list of clusters in the cluster group
+             * 
+             * @return moris::Cell< mtk::Cluster const* > const& list of clusters in the cluster group
+             */
+            moris::Cell< Cluster const* > const& get_clusters_in_group() const;
+
+            //------------------------------------------------------------------------------
+
+            /**
+             * @brief Compute the sum of all cluster volumes within the cluster group
+             * 
+             * @param aPrimaryOrVoid 
+             * @param aIsMaster 
+             * @return moris::real volume of all clusters 
+             */
+            moris::real
+            compute_cluster_group_volume(
+                    const mtk::Primary_Void aPrimaryOrVoid = mtk::Primary_Void::PRIMARY,
+                    const mtk::Master_Slave aIsMaster      = mtk::Master_Slave::MASTER ) const;
+
+            //------------------------------------------------------------------------------
+
+            /**
+             * @brief compute the derivative of the total cluster group volume
+             * 
+             * @param aPerturbedVertexCoords 
+             * @param aDirection 
+             * @param aPrimaryOrVoid 
+             * @param aIsMaster 
+             * @return moris::real 
+             */            
+            moris::real
+            compute_cluster_group_volume_derivative(
+                    const Matrix< DDRMat > & aPerturbedVertexCoords,
+                    uint aDirection,
+                    const mtk::Primary_Void aPrimaryOrVoid = mtk::Primary_Void::PRIMARY,
+                    const mtk::Master_Slave aIsMaster      = mtk::Master_Slave::MASTER ) const;
+
+            //------------------------------------------------------------------------------
+            
+            /**
+             * @brief compute the total cluster group interface/boundary surface/length
+             * 
+             * @param aPrimaryOrVoid 
+             * @param aIsMaster 
+             * @return moris::real 
+             */
+            moris::real
+            compute_cluster_group_side_measure(
+                    const mtk::Primary_Void aPrimaryOrVoid = mtk::Primary_Void::PRIMARY,
+                    const mtk::Master_Slave aIsMaster      = mtk::Master_Slave::MASTER ) const;
+
+            //------------------------------------------------------------------------------
+            
+            /**
+             * @brief compute the derivative of the total cluster group interface/boundary surface/length
+             * 
+             * @param aPerturbedVertexCoords 
+             * @param aDirection 
+             * @param aPrimaryOrVoid 
+             * @param aIsMaster 
+             * @return moris::real 
+             */
+            moris::real
+            compute_cluster_group_side_measure_derivative(
+                    const Matrix< DDRMat > & aPerturbedVertexCoords,
+                    uint aDirection,
+                    const mtk::Primary_Void aPrimaryOrVoid = mtk::Primary_Void::PRIMARY,
+                    const mtk::Master_Slave aIsMaster      = mtk::Master_Slave::MASTER ) const;
+
+            //------------------------------------------------------------------------------
+
+        };    // class Cluster
+
+    }    // namespace mtk
+}    // namespace moris
+
+#endif /* cl_MTK_Cluster_Group.hpp */
