fe4fa765d6b28518c7194b4c36bec91eb4a917fa
momo@nighthawk.colorado.edu <momo@nighthawk>
2022-06-14 14:16:19 -0600

    added parameter list to include adv-reinitial parameters

---
+++ projects/PRM/src/fn_PRM_WRK_Parameters.hpp
@@ -0,0 +1,39 @@
+#ifndef SRC_fn_PRM_MIG_Parameters
+#define SRC_fn_PRM_MIG_Parameters
+
+#include <string>
+#include <cstdio>
+
+#include "assert.hpp"
+//#include "cl_Communication_Tools.hpp"
+#include "typedefs.hpp"
+#include "cl_Matrix.hpp"
+#include "linalg_typedefs.hpp"
+#include "cl_XML_Parser.hpp"
+
+#include "cl_Param_List.hpp"
+
+namespace moris::prm
+{
+
+    //------------------------------------------------------------------------------
+
+    // creates a parameter list with default inputs
+    inline ParameterList
+    create_wrk_parameter_list()
+    {
+        ParameterList tParameterList;
+
+        tParameterList.insert( "adv_field", "" );
+        tParameterList.insert( "dof_type", "" );
+        tParameterList.insert( "reinitialization_frequency", 1 );
+        tParameterList.insert( "output_mesh_file", "" );
+        tParameterList.insert( "time_offset", 0.0 );
+
+        return tParameterList;
+    }
+    //------------------------------------------------------------------------------
+
+}// namespace moris::prm
+
+#endif
