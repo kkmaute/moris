e6a29ac2d84fe304aa09c79f726b752813f0518a
maute <maute@colorado.edu>
2022-07-29 15:31:09 -0600

    formated with clang

--- projects/LINALG/src/op_times.hpp
+++ projects/LINALG/src/op_times.hpp
@@ -21,60 +21,58 @@
 
 namespace moris
 {
-/**
- * @brief Matrix multiplication
- *
- * @param[in] aA Input matrix
- * @param[in] aB Input matrix
- *
- * This function returns a matrix such that @f$ \mathbf{C}_{ij} = \mathbf{A}_{ik} \mathbf{B}_{kj} @f$
- * where C is the product of A and B. If A is an m-by-p and B is a p-by-n matrix, then C is an m-by-n matrix.
- *
- * Example:
- * @include LNA/src/op_times.inc
- *
- */
-template< typename Matrix_Type_A, typename Matrix_Type_B   >
-auto
-operator*( Matrix< Matrix_Type_A > & aA,
-           Matrix< Matrix_Type_B > & aB )
-->decltype( aA.matrix_data() * aB.matrix_data() )
-{
+    /**
+     * @brief Matrix multiplication
+     *
+     * @param[in] aA Input matrix
+     * @param[in] aB Input matrix
+     *
+     * This function returns a matrix such that @f$ \mathbf{C}_{ij} = \mathbf{A}_{ik} \mathbf{B}_{kj} @f$
+     * where C is the product of A and B. If A is an m-by-p and B is a p-by-n matrix, then C is an m-by-n matrix.
+     *
+     * Example:
+     * @include LNA/src/op_times.inc
+     *
+     */
+    template< typename Matrix_Type_A, typename Matrix_Type_B >
+    auto
+    operator*( Matrix< Matrix_Type_A >& aA,
+            Matrix< Matrix_Type_B >&    aB )
+            -> decltype( aA.matrix_data() * aB.matrix_data() )
+    {
 
-    MORIS_ASSERT(aA.n_cols() == aB.n_rows(),"Dimension mismatch in matrix multiplication. %-5i vs %-5i" , aA.n_cols(), aB.n_rows());
-    return  aA.matrix_data() * aB.matrix_data();
-}
+        MORIS_ASSERT( aA.n_cols() == aB.n_rows(), "Dimension mismatch in matrix multiplication. %-5i vs %-5i", aA.n_cols(), aB.n_rows() );
+        return aA.matrix_data() * aB.matrix_data();
+    }
 
-template< typename Matrix_Type >
-auto
-operator*( typename Matrix< Matrix_Type >::Data_Type & aA,
-           Matrix< Matrix_Type > &                     aB )
-->decltype( scalar_times( aA, aB.matrix_data() ) )
-{
-    return scalar_times( aA, aB.matrix_data() );
-}
+    template< typename Matrix_Type >
+    auto
+    operator*( typename Matrix< Matrix_Type >::Data_Type& aA,
+            Matrix< Matrix_Type >&                        aB )
+            -> decltype( scalar_times( aA, aB.matrix_data() ) )
+    {
+        return scalar_times( aA, aB.matrix_data() );
+    }
 
+    template< typename Matrix_Type_A, typename Matrix_Type_B >
+    auto
+    operator*( const Matrix< Matrix_Type_A >& aA,
+            const Matrix< Matrix_Type_B >&    aB )
+            -> decltype( aA.matrix_data() * aB.matrix_data() )
+    {
+        MORIS_ASSERT( aA.n_cols() == aB.n_rows(), "Dimension mismatch in matrix multiplication. %-5i vs %-5i", aA.n_cols(), aB.n_rows() );
+        return aA.matrix_data() * aB.matrix_data();
+    }
 
-template< typename Matrix_Type_A,typename Matrix_Type_B >
-auto
-operator*( const Matrix< Matrix_Type_A > & aA,
-           const Matrix< Matrix_Type_B > & aB )
-->decltype( aA.matrix_data() * aB.matrix_data() )
-{
-    MORIS_ASSERT(aA.n_cols() == aB.n_rows(),"Dimension mismatch in matrix multiplication. %-5i vs %-5i",aA.n_cols(), aB.n_rows());
-    return  aA.matrix_data() * aB.matrix_data();
-}
+    template< typename Matrix_Type >
+    auto
+    operator*( const typename Matrix< Matrix_Type >::Data_Type& aA,
+            const Matrix< Matrix_Type >&                        aB )
+            -> decltype( scalar_times( aA, aB.matrix_data() ) )
+    {
+        return scalar_times( aA, aB.matrix_data() );
+    }
 
-template< typename Matrix_Type >
-auto
-operator*( const typename Matrix< Matrix_Type >::Data_Type & aA,
-           const Matrix< Matrix_Type > & aB )
-->decltype( scalar_times(aA,aB.matrix_data()) )
-{
-    return  scalar_times(aA,aB.matrix_data());
-}
-
-}
-
+}    // namespace moris
 
 #endif /* PROJECTS_LINALG_OP_TIMES_HPP_ */
