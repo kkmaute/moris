1001b2d5223b8531ee0fbd6200ca90a19eaa5735
maute <maute@colorado.edu>
2022-08-14 15:33:45 -0600

    add "invalid" query option

--- projects/GEN/GEN_MAIN/src/cl_GEN_Geometric_Query_Interface.hpp
+++ projects/GEN/GEN_MAIN/src/cl_GEN_Geometric_Query_Interface.hpp
@@ -5,60 +5,59 @@
 
 namespace moris
 {
-   namespace ge
-   {
+    namespace ge
+    {
         enum class Query_Type
-        {   
+        {
             INTERSECTION_NO_LOCATION,
             INTERSECTION_LOCATION,
-            VALUE_AT_PARAMETRIC_LOCATION
-
+            VALUE_AT_PARAMETRIC_LOCATION,
+            INVALID
         };
 
-       class Geometric_Query_Interface
-       {
-       private:
-           
-       public:
-           Geometric_Query_Interface(/* args */);
+        class Geometric_Query_Interface
+        {
+          private:
 
-           ~Geometric_Query_Interface();
+          public:
+            Geometric_Query_Interface( /* args */ );
 
-           virtual enum Query_Type get_query_type() const = 0; 
+            ~Geometric_Query_Interface();
 
-           virtual moris_index get_geomtric_index() const  = 0;
+            virtual enum Query_Type get_query_type() const = 0;
 
-           virtual enum EntityRank get_query_entity_rank() const = 0;
+            virtual moris_index get_geomtric_index() const = 0;
 
-           virtual Matrix<IndexMat> const & get_query_entity_to_vertex_connectivity() const = 0;
+            virtual enum EntityRank get_query_entity_rank() const = 0;
 
-           virtual moris::Cell<std::shared_ptr<moris::Matrix<moris::DDRMat>>> * get_query_indexed_coordinates() const = 0;
+            virtual Matrix< IndexMat > const & get_query_entity_to_vertex_connectivity() const = 0;
 
-           virtual Matrix<DDRMat>  get_vertex_local_coord_wrt_parent_entity( moris_index aVertexIndex ) const = 0;
+            virtual moris::Cell< std::shared_ptr< moris::Matrix< moris::DDRMat > > >* get_query_indexed_coordinates() const = 0;
 
-           virtual enum EntityRank get_query_parent_entity_rank() const = 0;
+            virtual Matrix< DDRMat > get_vertex_local_coord_wrt_parent_entity( moris_index aVertexIndex ) const = 0;
 
-           virtual Matrix<IndexMat> get_query_parent_entity_connectivity() const = 0;
+            virtual enum EntityRank get_query_parent_entity_rank() const = 0;
 
-           virtual Matrix<DDRMat>   get_query_parent_coordinates() const = 0;
+            virtual Matrix< IndexMat > get_query_parent_entity_connectivity() const = 0;
 
-           virtual moris_index get_query_parent_entity_id() const = 0;
+            virtual Matrix< DDRMat > get_query_parent_coordinates() const = 0;
 
-           virtual moris_index max_query_entity_intersection() const = 0;
+            virtual moris_index get_query_parent_entity_id() const = 0;
 
-       };
-       
-       
-       Geometric_Query_Interface::Geometric_Query_Interface(/* args */)
-       {
-       }
-       
-       Geometric_Query_Interface::~Geometric_Query_Interface()
-       {
-       }
-       
-   } // namespace ge
-   
-} // namespace moris
+            virtual moris_index max_query_entity_intersection() const = 0;
+        };
+
+
+        Geometric_Query_Interface::Geometric_Query_Interface( /* args */ )
+        {
+        }
+
+        Geometric_Query_Interface::~Geometric_Query_Interface()
+        {
+        }
+
+    }    // namespace ge
+
+}    // namespace moris
 
 #endif
