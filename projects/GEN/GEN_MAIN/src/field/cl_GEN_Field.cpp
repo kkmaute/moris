53c33f55c528418e5111290d859ed4c116234e22
momo <momo1035@colorado.edu>
2022-09-10 12:38:35 -0600

    Initailize the primitive adv ids with max id
    Initializng with -1 causes a issue when computing sensetivites, since our fields for predefined gemotry are allocated for adv+ field variable, initailzing with max_id ignore the unewanted adv ids in the assembly process of the diqidadv
    
    T

--- projects/GEN/GEN_MAIN/src/field/cl_GEN_Field.cpp
+++ projects/GEN/GEN_MAIN/src/field/cl_GEN_Field.cpp
@@ -47,7 +47,7 @@ namespace moris
                 , mSensitivities( 1, aFieldVariableIndices.length() + aConstants.length() )
                 , mConstants( aConstants )
                 , mParameters( aParameters )
-                , mDeterminingADVIds( aFieldVariableIndices.length() + aConstants.length(), 1, -1 )
+                , mDeterminingADVIds( aFieldVariableIndices.length() + aConstants.length(), 1, MORIS_ID_MAX )
                 , mDependsOnADVs( aADVIndices.length() )
         {
             // Check that refinement information is correct
@@ -152,7 +152,7 @@ namespace moris
         {
             for ( uint tVariableIndex = 0; tVariableIndex < mDeterminingADVIds.length(); tVariableIndex++ )
             {
-                if ( mDeterminingADVIds( tVariableIndex ) > -1 )
+                if ( mDeterminingADVIds( tVariableIndex ) < MORIS_ID_MAX )
                 {
                     mFieldVariables( tVariableIndex ) = get_address( aADVs, mDeterminingADVIds( tVariableIndex ) );
                 }
