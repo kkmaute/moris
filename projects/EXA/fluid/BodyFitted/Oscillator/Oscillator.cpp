ca7df98094c4b31a2b73253af7093c2bf032aa01
bhavsar <bhavsar@mirage>
2022-09-19 15:03:20 -0600

    Change to input file for mesh file path

--- projects/EXA/fluid/BodyFitted/Oscillator/Oscillator.cpp
+++ projects/EXA/fluid/BodyFitted/Oscillator/Oscillator.cpp
@@ -129,7 +129,7 @@ namespace moris
     std::string tExoFile = tName + ".exo";
     std::string tSoFile  = tName + ".so";
 
-    std::string tMeshFileName = "$MORISROOT/projects/EXA/fluid/BodyFitted/Oscillator/Oscillator.g";
+    std::string tMeshFileName = "./Oscillator.g";
 
     /* ------------------------------------------------------------------------ */
     // Output Config
