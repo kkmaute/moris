fcd42840ab7d180ed811b7a0114931bf2d025403
maute <maute@colorado.edu>
2022-10-03 13:46:55 -0600

    update checks to work with Catch2 V2

--- projects/MRS/ASR/test/assert.cpp
+++ projects/MRS/ASR/test/assert.cpp
@@ -16,41 +16,42 @@ extern moris::Logger gLogger;
 
 namespace moris
 {
+
+void test_moris_error( bool aCheck )
+{
+    MORIS_ERROR( aCheck , "Test output message for MORIS ERROR throw" );
+}
+
+void test_moris_assert( bool aCheck)
+{
+    MORIS_ASSERT( aCheck, "Test output message for MORIS ASSERT throw" );
+}
+
 TEST_CASE(
         "MORIS_ASSERT and MORIS_ERROR Tests",
         "[MORIS],[assert],[error]"      )
-                                {
+{
 
     SECTION("CHECK FOR THROWS")
     {
-        // Verify that MORIS_ERROR throws. Otherwise all CHECK_THROW tests will fail.
-        CHECK_THROWS(MORIS_ERROR( 1 == 0, "Test output message for MORIS ERROR throw" ));
+	// Verify that MORIS_ERROR throws. Otherwise all CHECK_THROW tests will fail.
+        CHECK_THROWS( test_moris_error( false ) );
 
         // Verify that MORIS_ERROR throws only in debug mode. Otherwise all CHECK_THROW tests will fail.
 #ifdef MORIS_HAVE_DEBUG
-        CHECK_THROWS(MORIS_ASSERT( 1 == 0, "Test output message for MORIS ASSERT throw" ));
+        CHECK_THROWS(test_moris_assert( false ));
 #endif
     }
 
     SECTION("CHECK FOR NO THROWS")
     {
         // Verify that MORIS_ERROR does not throw.
-        CHECK_NOTHROW(MORIS_ERROR( 1 == 1, "Test output message for MORIS ERROR throw" ));
+        CHECK_NOTHROW( test_moris_error( true ) );
 
         // Verify that MORIS_ASSERT does not throw.
-        CHECK_NOTHROW(MORIS_ASSERT( 1 == 1, "Test output message for MORIS ASSERT throw" ));
+        CHECK_NOTHROW( test_moris_assert( true  ) );
     }
 
-    SECTION("CHECK FOR THROWS WITH MULTIPLE ARGUMENTS")
-    {
-        // Verify that MORIS_ERROR throws. Otherwise all CHECK_THROW tests will fail.
-        CHECK_THROWS(MORIS_ERROR( false, "Test output message for MORIS ERROR throw %-5i   ||   %-5.2e", 124, 0.00456789 ));
+  }
 
-        // Verify that MORIS_ERROR throws only in debug mode. Otherwise all CHECK_THROW tests will fail.
-#ifdef MORIS_HAVE_DEBUG
-        CHECK_THROWS(MORIS_ASSERT( false, "Test output message for MORIS ERROR throw %-5i   ||   %-5.2e", 124, 0.00456789 ));
-#endif
-    }
-                                }
-}
-
+ }
