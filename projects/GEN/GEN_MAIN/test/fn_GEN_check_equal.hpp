931f6bbaebc0c559fb71a8d8e786786b75b28d42
maute <maute@colorado.edu>
2022-10-03 13:57:45 -0600

    fixed Catch2 test

--- projects/GEN/GEN_MAIN/test/fn_GEN_check_equal.hpp
+++ projects/GEN/GEN_MAIN/test/fn_GEN_check_equal.hpp
@@ -26,24 +26,25 @@ namespace moris
          * @param aMatrix2 Comparison matrix 2
          */
         template< typename Matrix_Type >
-        void check_equal(Matrix<Matrix_Type> aMatrix1, Matrix<Matrix_Type> aMatrix2)
+        void
+        check_equal( Matrix< Matrix_Type > aMatrix1, Matrix< Matrix_Type > aMatrix2 )
         {
-            REQUIRE(aMatrix1.n_rows() == aMatrix2.n_rows());
-            REQUIRE(aMatrix1.n_cols() == aMatrix2.n_cols());
-            for (uint tRowIndex = 0; tRowIndex < aMatrix1.n_rows(); tRowIndex++)
+            REQUIRE( aMatrix1.n_rows() == aMatrix2.n_rows() );
+            REQUIRE( aMatrix1.n_cols() == aMatrix2.n_cols() );
+            for ( uint tRowIndex = 0; tRowIndex < aMatrix1.n_rows(); tRowIndex++ )
             {
-                for (uint tColumnIndex = 0; tColumnIndex < aMatrix1.n_cols(); tColumnIndex++)
+                for ( uint tColumnIndex = 0; tColumnIndex < aMatrix1.n_cols(); tColumnIndex++ )
                 {
-                    CHECK(aMatrix1(tRowIndex, tColumnIndex) == Approx(aMatrix2(tRowIndex, tColumnIndex)));
-                    if (aMatrix1(tRowIndex, tColumnIndex) != Approx(aMatrix2(tRowIndex, tColumnIndex)))
+                    CHECK( aMatrix1( tRowIndex, tColumnIndex ) == Approx( aMatrix2( tRowIndex, tColumnIndex ) ).margin( MORIS_REAL_EPS ) );
+
+                    if ( aMatrix1( tRowIndex, tColumnIndex ) != Approx( aMatrix2( tRowIndex, tColumnIndex ) ).margin( MORIS_REAL_EPS ) )
                     {
-                        std::cout << aMatrix1(tRowIndex, tColumnIndex) << " =/= " << aMatrix2(tRowIndex, tColumnIndex) << std::endl;
+                        std::cout << aMatrix1( tRowIndex, tColumnIndex ) << " =/= " << aMatrix2( tRowIndex, tColumnIndex ) << std::endl;
                     }
                 }
             }
         }
-    }
-}
+    }    // namespace ge
+}    // namespace moris
 
-#endif //MORIS_FN_GEN_CHECK_EQUAL_HPP
-
+#endif    // MORIS_FN_GEN_CHECK_EQUAL_HPP
