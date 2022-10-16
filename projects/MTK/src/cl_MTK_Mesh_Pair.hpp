5b1ddf356959b33c85cc6b905278ce097ace81eb
maute <maute@colorado.edu>
2022-10-16 11:55:54 -0600

    added explict definition of = operator

--- projects/MTK/src/cl_MTK_Mesh_Pair.hpp
+++ projects/MTK/src/cl_MTK_Mesh_Pair.hpp
@@ -23,13 +23,12 @@ namespace moris
         class Mesh_Pair
         {
 
-        private:
+          private:
             Interpolation_Mesh* mInterpolationMesh;
             Integration_Mesh*   mIntegrationMesh;
             bool                mIsOwned;
 
-        public:
-
+          public:
             /**
              * Constructor
              *
@@ -40,14 +39,14 @@ namespace moris
             Mesh_Pair(
                     Interpolation_Mesh* aInterpolationMesh,
                     Integration_Mesh*   aIntegrationMesh,
-                    bool                aIsOwned = false);
+                    bool                aIsOwned = false );
 
             /**
              * Explicit copy constructor to ensure pointers are only owned once.
              *
              * @param aMeshPair Input mesh pair for copying
              */
-            Mesh_Pair(const Mesh_Pair& aMeshPair);
+            Mesh_Pair( const Mesh_Pair& aMeshPair );
 
             /**
              * Destructor (deletes mesh pointers if owned)
@@ -69,19 +68,30 @@ namespace moris
             Integration_Mesh* get_integration_mesh() const;
 
             /**
+             * Copy constructor
+             */
+            const Mesh_Pair&
+            operator=( const Mesh_Pair& aMeshPair )
+            {
+                mInterpolationMesh = aMeshPair.get_interpolation_mesh();
+                mIntegrationMesh   = aMeshPair.get_integration_mesh();
+                mIsOwned           = aMeshPair.mIsOwned;
+
+                return *this;
+            }
+
+            /**
              * Friend functions in the mesh manager so it can properly transfer pointer ownership, without revealing
              * this to the outside.
              */
-            friend uint Mesh_Manager::register_mesh_pair(Mesh_Pair& aMeshPair);
+            friend uint Mesh_Manager::register_mesh_pair( Mesh_Pair& aMeshPair );
             friend uint Mesh_Manager::register_mesh_pair(
                     Interpolation_Mesh* aInterpolationMesh,
                     Integration_Mesh*   aIntegrationMesh,
                     bool                aIsOwned,
-                    std::string const & aMeshPairName);
-
+                    std::string const & aMeshPairName );
         };
-    }
-}
+    }    // namespace mtk
+}    // namespace moris
 
-#endif //MORIS_CL_MTK_MESH_PAIR_HPP
-
+#endif    // MORIS_CL_MTK_MESH_PAIR_HPP
