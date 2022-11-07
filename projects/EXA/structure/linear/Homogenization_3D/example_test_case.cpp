d14adf912f9157cfd3eb5ee61d481f26d02b0762
maute <maute@colorado.edu>
2022-11-07 09:07:35 -0700

    reformated with clang

--- projects/EXA/structure/linear/Homogenization_3D/example_test_case.cpp
+++ projects/EXA/structure/linear/Homogenization_3D/example_test_case.cpp
@@ -10,9 +10,9 @@
 
 #include <catch.hpp>
 
-#include "cl_Logger.hpp" // MRS/IOS/src
-#include "cl_MTK_Exodus_IO_Helper.hpp"  // MTK/src
-#include "cl_Communication_Tools.hpp"   // MRS/COM/src
+#include "cl_Logger.hpp"                  // MRS/IOS/src
+#include "cl_MTK_Exodus_IO_Helper.hpp"    // MTK/src
+#include "cl_Communication_Tools.hpp"     // MRS/COM/src
 
 #include "cl_Matrix.hpp"
 #include "fn_norm.hpp"
@@ -24,25 +24,27 @@ bool gPrintReferenceValues = true;
 
 //---------------------------------------------------------------
 
-int fn_WRK_Workflow_Main_Interface( int argc, char * argv[] );
+int fn_WRK_Workflow_Main_Interface( int argc, char* argv[] );
 
 //---------------------------------------------------------------
 
-extern "C"
-void check_linear_results(moris::mtk::Exodus_IO_Helper & aExoIO,uint aInnerNodeId, uint aOuterNodeId)
+extern "C" void
+check_linear_results(
+        moris::mtk::Exodus_IO_Helper& aExoIO,
+        uint                          aInnerNodeId,
+        uint                          aOuterNodeId )
 {
+    std::cout << "E_Inner " << aExoIO.get_nodal_field_value( aInnerNodeId, 5, 0 ) << std::endl;
+    std::cout << "E_Outer " << aExoIO.get_nodal_field_value( aOuterNodeId, 4, 0 ) << std::endl;
 
-    std::cout<<"E_Inner "<<             aExoIO.get_nodal_field_value( aInnerNodeId, 5, 0 ) << std::endl;
-    std::cout<<"E_Outer "<<             aExoIO.get_nodal_field_value( aOuterNodeId, 4, 0 ) << std::endl;
-
-    if (gPrintReferenceValues)
+    if ( gPrintReferenceValues )
     {
         // coordinates of reference point
-        moris::print( aExoIO.get_nodal_coordinate( aInnerNodeId ), "Coordinates of reference point");
-        moris::print( aExoIO.get_nodal_coordinate( aOuterNodeId ), "Coordinates of reference point");
+        moris::print( aExoIO.get_nodal_coordinate( aInnerNodeId ), "Coordinates of reference point" );
+        moris::print( aExoIO.get_nodal_coordinate( aOuterNodeId ), "Coordinates of reference point" );
 
-        std::cout<<"E_Inner: "<<               aExoIO.get_nodal_field_value(aInnerNodeId, 5, 0 ) << std::endl;
-        std::cout<<"E_Outer: "<<               aExoIO.get_nodal_field_value(aOuterNodeId, 4, 0 ) << std::endl;
+        std::cout << "E_Inner: " << aExoIO.get_nodal_field_value( aInnerNodeId, 5, 0 ) << std::endl;
+        std::cout << "E_Outer: " << aExoIO.get_nodal_field_value( aOuterNodeId, 4, 0 ) << std::endl;
         return;
     }
 
@@ -50,63 +52,64 @@ void check_linear_results(moris::mtk::Exodus_IO_Helper & aExoIO,uint aInnerNodeI
     real tEpsilon = 1.0e-2;
 
     // define reference coordinates for node aNodeId
-    Matrix< DDRMat > tReferenceCoordinateInner = { {0.4},{0.6}, {0.4} };
-    Matrix< DDRMat > tReferenceCoordinateOuter = { {0.2},{0.8}, {1.0} };
+    Matrix< DDRMat > tReferenceCoordinateInner = { { 0.4 }, { 0.6 }, { 0.4 } };
+    Matrix< DDRMat > tReferenceCoordinateOuter = { { 0.2 }, { 0.8 }, { 1.0 } };
 
     // check nodal coordinates
-    real tRelDiffNormInner = moris::norm( aExoIO.get_nodal_coordinate( aInnerNodeId ) - tReferenceCoordinateInner )/ moris::norm(tReferenceCoordinateInner);
-    real tRelDiffNormOuter = moris::norm( aExoIO.get_nodal_coordinate( aOuterNodeId ) - tReferenceCoordinateOuter )/ moris::norm(tReferenceCoordinateOuter);
-    REQUIRE( tRelDiffNormInner <  tEpsilon );
-    REQUIRE( tRelDiffNormOuter <  tEpsilon );
+    real tRelDiffNormInner = moris::norm( aExoIO.get_nodal_coordinate( aInnerNodeId ) - tReferenceCoordinateInner ) / moris::norm( tReferenceCoordinateInner );
+    real tRelDiffNormOuter = moris::norm( aExoIO.get_nodal_coordinate( aOuterNodeId ) - tReferenceCoordinateOuter ) / moris::norm( tReferenceCoordinateOuter );
+    REQUIRE( tRelDiffNormInner < tEpsilon );
+    REQUIRE( tRelDiffNormOuter < tEpsilon );
 
     // check reference displacements
     real tReferenceHomogenizedC1111 = 1.069;
-    real tHomogenizedC1111 = aExoIO.get_global_variable(0, 0) + aExoIO.get_global_variable(1, 0);
-    REQUIRE( std::abs( tHomogenizedC1111 - tReferenceHomogenizedC1111 ) <  tEpsilon );
 
+    real tHomogenizedC1111 = aExoIO.get_global_variable( 0, 0 ) + aExoIO.get_global_variable( 1, 0 );
+
+    REQUIRE( std::abs( tHomogenizedC1111 - tReferenceHomogenizedC1111 ) < tEpsilon );
 }
 
 //---------------------------------------------------------------
 
-extern "C"
-void check_linear_results_serial()
+extern "C" void
+check_linear_results_serial()
 {
     // open and query exodus output file (set verbose to true to get basic mesh information)
-    moris::mtk::Exodus_IO_Helper tExoIO("Homogenization_3D.exo",0,false,false);
+    moris::mtk::Exodus_IO_Helper tExoIO( "Homogenization_3D.exo", 0, false, false );
 
     // check dimension, number of nodes and number of elements
     uint tNumDims  = tExoIO.get_number_of_dimensions();
     uint tNumNodes = tExoIO.get_number_of_nodes();
     uint tNumElems = tExoIO.get_number_of_elements();
 
-        std::cout << "Number of dimensions: " << tNumDims  << std::endl;
-        std::cout << "Number of nodes     : " << tNumNodes << std::endl;
-        std::cout << "Number of elements  : " << tNumElems << std::endl;
+    std::cout << "Number of dimensions: " << tNumDims << std::endl;
+    std::cout << "Number of nodes     : " << tNumNodes << std::endl;
+    std::cout << "Number of elements  : " << tNumElems << std::endl;
 
-    if (gPrintReferenceValues)
+    if ( gPrintReferenceValues )
     {
-        std::cout << "Number of dimensions: " << tNumDims  << std::endl;
+        std::cout << "Number of dimensions: " << tNumDims << std::endl;
         std::cout << "Number of nodes     : " << tNumNodes << std::endl;
         std::cout << "Number of elements  : " << tNumElems << std::endl;
     }
     else
     {
-        REQUIRE( tNumDims  ==  3   );
-        REQUIRE( tNumNodes ==  976 );
-        REQUIRE( tNumElems ==  1731 );
+        REQUIRE( tNumDims == 3 );
+        REQUIRE( tNumNodes == 976 );
+        REQUIRE( tNumElems == 1731 );
     }
 
     // check results at node with most deformation
     uint tInnerNodeId = 33;
     uint tOuterNodeId = 584;
 
-    check_linear_results(tExoIO,tInnerNodeId,tOuterNodeId);
+    check_linear_results( tExoIO, tInnerNodeId, tOuterNodeId );
 }
 
 //---------------------------------------------------------------
 
-TEST_CASE("Axisymmetric_Problem_Linear",
-        "[moris],[example],[structure],[homogenization]")
+TEST_CASE( "Axisymmetric_Problem_Linear",
+        "[moris],[example],[structure],[homogenization]" )
 {
     // temporary
     gLogger.initialize( "Log.log" );
@@ -117,13 +120,13 @@ TEST_CASE("Axisymmetric_Problem_Linear",
     char tString1[] = "";
     char tString2[] = "./Homogenization_3D.so";
 
-    char * argv[2] = {tString1,tString2};
+    char* argv[ 2 ] = { tString1, tString2 };
 
     // call to performance manager main interface
     int tRet = fn_WRK_Workflow_Main_Interface( argc, argv );
 
     // catch test statements should follow
-    REQUIRE( tRet ==  0 );
+    REQUIRE( tRet == 0 );
 
     // check results
     switch ( par_size() )
@@ -135,8 +138,7 @@ TEST_CASE("Axisymmetric_Problem_Linear",
         }
         default:
         {
-            MORIS_ERROR(false,"This 2D Example can only be run in serial.",par_size());
+            MORIS_ERROR( false, "This 3D Example can only be run in serial.", par_size() );
         }
     }
 }
-
