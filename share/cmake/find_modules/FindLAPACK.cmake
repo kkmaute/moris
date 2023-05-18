ac3b92451bbad1d0fe549627e6142ca525303e2f
maute <maute@colorado.edu>
2023-05-18 17:41:24 -0600

    minor reformatting; does not resolved issue with using lapack with trilinos

--- share/cmake/find_modules/FindLAPACK.cmake
+++ share/cmake/find_modules/FindLAPACK.cmake
@@ -49,6 +49,4 @@ add_library(LAPACK::lapack INTERFACE IMPORTED GLOBAL)
 target_link_libraries(LAPACK::lapack INTERFACE ${LAPACK_LIBRARY_TARGETS})
 
 add_library(LAPACK::all_libs INTERFACE IMPORTED)
-target_link_libraries(LAPACK::all_libs
-  INTERFACE LAPACK::lapack
-  )
+target_link_libraries(LAPACK::all_libs INTERFACE ${LAPACK_LIBRARY_TARGETS})
