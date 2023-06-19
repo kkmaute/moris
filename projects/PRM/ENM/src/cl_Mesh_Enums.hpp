ddca9fde34f3ca403116c124297651ed4d6b3354
Adam <adam.christopherson@colorado.edu>
2023-06-19 15:52:55 -0600

    Split declaration/definition for mesh enum functions and added function for getting order

--- projects/PRM/ENM/src/cl_Mesh_Enums.hpp
+++ projects/PRM/ENM/src/cl_Mesh_Enums.hpp
@@ -18,7 +18,6 @@
 
 enum class MeshType
 {
-
     MTK,    //< Wrapper around STK mesh database
     STK,    //< Wrapper around STK mesh database
     HMR,    //  Wrapper around HMR mesh database
@@ -62,39 +61,21 @@ enum class CellShape
     END_ENUM
 };
 
-inline const std::string
-get_enum_str( enum CellTopology aCellTopoEnum )
-{
-    switch ( aCellTopoEnum )
-    {
-        case CellTopology::TRI3:
-            return "TRI3";
-        case CellTopology::QUAD4:
-            return "QUAD4";
-        case CellTopology::QUAD8:
-            return "QUAD8";
-        case CellTopology::TET4:
-            return "TET4";
-        case CellTopology::TET10:
-            return "TET10";
-        case CellTopology::HEX8:
-            return "HEX8";
-        case CellTopology::HEX20:
-            return "HEX20";
-        case CellTopology::HEX27:
-            return "HEX27";
-        case CellTopology::HEX64:
-            return "HEX64";
-        case CellTopology::PRISM6:
-            return "PRISM6";
-        case CellTopology::INVALID:
-            return "INVALID";
-        case CellTopology::END_ENUM:
-            return "END_ENUM";
-        default:
-            return "invalid cell topology enum provided";
-    }
-}
+/**
+ * Gets a string representing the given CellTopology enum
+ *
+ * @param aCellTopology CellTopology enum
+ * @return String with enum name
+ */
+std::string get_enum_str( CellTopology ACellTopology );
+
+/**
+ * Gets just the order from a cell topology
+ *
+ * @param aCellTopology CellTopology enum
+ * @return Polynomial order
+ */
+uint get_order_from_topology( CellTopology aCellTopology );
 
 namespace moris
 {
@@ -119,8 +100,6 @@ namespace moris
         END_ENUM                 //
     };
 
-    //------------------------------------------------------------------------------
-
     enum class ClusterType
     {
         CELL_CLUSTER,            // Cell Cluster
@@ -129,122 +108,29 @@ namespace moris
         END_ENUM
     };
 
-    //------------------------------------------------------------------------------
-    inline const std::string
-    get_cluster_type_str( enum ClusterType aClusterType )
-    {
-        switch ( aClusterType )
-        {
+    /**
+     * Gets a string representing the given EntityRank enum
+     *
+     * @param aEntityRank EntityRank enum
+     * @return String with enum name
+     */
+    std::string get_enum_str( EntityRank aEntityRank );
 
-            case ClusterType::CELL_CLUSTER:
-                return "CELL_CLUSTER";
-                break;
-            case ClusterType::SIDE_CLUSTER:
-                return "SIDE_CLUSTER";
-                break;
-            case ClusterType::DOUBLE_SIDED_CLUSTER:
-                return "DOUBLE_SIDED_CLUSTER";
-                break;
-            default:
-                return "Unrecognized Enum provided to get_enum_str";
-        }
-    }
+    /**
+     * Gets an EntityRank enum from a given string
+     *
+     * @param aString String with enum name
+     * @return EntityRank enum
+     */
+    EntityRank get_entity_rank_from_str( std::string const & aString );
 
-    inline const std::string
-    get_enum_str( enum EntityRank aCellTopoEnum )
-    {
-        switch ( aCellTopoEnum )
-        {
-
-            case EntityRank::NODE:
-                return "NODE";
-                break;
-            case EntityRank::EDGE:
-                return "EDGE";
-                break;
-            case EntityRank::FACE:
-                return "FACE";
-                break;
-            case EntityRank::ELEMENT:
-                return "ELEMENT";
-                break;
-            case EntityRank::BSPLINE:
-                return "BSPLINE";
-                break;
-            case EntityRank::BSPLINE_2:
-                return "BSPLINE_2";
-                break;
-            case EntityRank::BSPLINE_3:
-                return "BSPLINE_3";
-                break;
-            case EntityRank::INVALID:
-                return "INVALID";
-                break;
-            default:
-                return "Unrecognized Enum provided to get_enum_str";
-        }
-    }
-    inline enum EntityRank
-    get_entity_rank_from_str( std::string const & aString )
-    {
-        if ( aString.compare( "NODE" ) == 0 || aString.compare( "node" ) == 0 ) { return EntityRank::NODE; }
-        else if ( aString.compare( "EDGE" ) == 0 || aString.compare( "edge" ) == 0 )
-        {
-            return EntityRank::EDGE;
-        }
-        else if ( aString.compare( "FACE" ) == 0 || aString.compare( "face" ) == 0 )
-        {
-            return EntityRank::FACE;
-        }
-        else if ( aString.compare( "ELEMENT" ) == 0 || aString.compare( "element" ) == 0 )
-        {
-            return EntityRank::ELEMENT;
-        }
-        else if ( aString.compare( "BSPLINE" ) == 0 || aString.compare( "bspline" ) == 0 )
-        {
-            return EntityRank::BSPLINE;
-        }
-        else if ( aString.compare( "BSPLINE_2" ) == 0 || aString.compare( "bspline_2" ) == 0 )
-        {
-            return EntityRank::BSPLINE_2;
-        }
-        else if ( aString.compare( "BSPLINE_3" ) == 0 || aString.compare( "bspline_3" ) == 0 )
-        {
-            return EntityRank::BSPLINE_3;
-        }
-        else if ( aString.compare( "INVALID" ) == 0 || aString.compare( "invalid" ) == 0 )
-        {
-            return EntityRank::INVALID;
-        }
-        else
-        {
-            MORIS_ERROR( 0, "Invliad entity rank string" );
-            return EntityRank::INVALID;
-        };
-    }
-    inline enum EntityRank
-    get_entity_rank_from_index( moris_index aEntityRankIndex )
-    {
-        if ( aEntityRankIndex == 0 ) { return EntityRank::NODE; }
-        else if ( aEntityRankIndex == 1 )
-        {
-            return EntityRank::EDGE;
-        }
-        else if ( aEntityRankIndex == 2 )
-        {
-            return EntityRank::FACE;
-        }
-        else if ( aEntityRankIndex == 3 )
-        {
-            return EntityRank::ELEMENT;
-        }
-        else
-        {
-            MORIS_ERROR( 0, "Invalid index entity rank string" );
-            return EntityRank::INVALID;
-        };
-    }
-}    // namespace moris
+    /**
+     * Gets the entity rank from a given index
+     * @param aEntityRankIndex
+     * @return
+     */
+    EntityRank get_entity_rank_from_index( moris_index aEntityRankIndex );
+}
 
 #endif /* MORIS_MESH_CL_MESH_ENUMS_HPP_ */
 
