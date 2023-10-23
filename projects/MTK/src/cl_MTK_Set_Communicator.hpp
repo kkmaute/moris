b03e27d98bc1cfd39375443dab7db8edced79523
wunsch <nils.wunsch@colorado.edu>
2023-10-23 17:38:16 -0600

    Set_Communicator for bulk comm. of set info

---
+++ projects/MTK/src/cl_MTK_Set_Communicator.hpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2022 University of Colorado
+ * Licensed under the MIT license. See LICENSE.txt file in the MORIS root for details.
+ *
+ * ------------------------------------------------------------------------------------
+ *
+ * cl_MTK_Set_Communicator.hpp
+ *
+ */
+#pragma once
+
+#include "containers.hpp"
+
+namespace moris
+{
+    namespace mtk
+    {
+        // forward declare the mtk::Set
+        class Set;
+
+        //------------------------------------------------------------------------------
+
+        /**
+         * @brief This class handles communication of basic set information (e.g. element type, interpolation order, etc.)
+         * for a list of sets. It has access to protected information of the mtk::Set's. This class is needed to perform
+         * communication of set information in bulk rather than a set-by-set basis to improve parallel performance.
+         */
+        class Set_Communicator
+        {
+            //------------------------------------------------------------------------------
+
+          public:
+            //------------------------------------------------------------------------------
+
+            Set_Communicator( moris::Cell< mtk::Set* >& aSetsToCommunicate );
+
+            //------------------------------------------------------------------------------
+
+            ~Set_Communicator() {}
+
+            //------------------------------------------------------------------------------
+
+            bool check_sets( moris::Cell< mtk::Set* >& aSetsToCommunicate );
+
+            //------------------------------------------------------------------------------
+
+        };    // end class: mtk::Set_Communicator
+
+        //------------------------------------------------------------------------------
+
+    }    // namespace mtk
+
+}    // namespace moris
