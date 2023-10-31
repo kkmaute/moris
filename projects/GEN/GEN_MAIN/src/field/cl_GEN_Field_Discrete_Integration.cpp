e6a73eff0995b964094271e4b888f3434cb4e689
wunsch <nils.wunsch@colorado.edu>
2023-10-31 14:30:42 -0600

    fixed bug in GEN
    bug occurs when using multi-linear intersection
    along side higher-order LS interpolations

--- projects/GEN/GEN_MAIN/src/field/cl_GEN_Field_Discrete_Integration.cpp
+++ projects/GEN/GEN_MAIN/src/field/cl_GEN_Field_Discrete_Integration.cpp
@@ -17,7 +17,7 @@ namespace moris
 
         //--------------------------------------------------------------------------------------------------------------
 
-        Field_Discrete_Integration::Field_Discrete_Integration(uint aNumOriginalNodes)
+        Field_Discrete_Integration::Field_Discrete_Integration( uint aNumOriginalNodes )
         {
             mNumOriginalNodes = aNumOriginalNodes;
         }
@@ -25,100 +25,102 @@ namespace moris
         //--------------------------------------------------------------------------------------------------------------
 
         real Field_Discrete_Integration::get_field_value(
-                uint                  aNodeIndex,
-                const Matrix<DDRMat>& aCoordinates)
+                uint                    aNodeIndex,
+                const Matrix< DDRMat >& aCoordinates )
         {
-            if (aNodeIndex < mNumOriginalNodes)
+            if ( aNodeIndex < mNumOriginalNodes )
             {
-                return this->get_field_value(aNodeIndex);
+                return this->get_field_value( aNodeIndex );
             }
             else
             {
-                MORIS_ASSERT((aNodeIndex - mNumOriginalNodes) < mChildNodes.size(),
+                MORIS_ASSERT( 
+                        ( aNodeIndex - mNumOriginalNodes ) < mChildNodes.size(),
+                        "Field_Discrete_Integration::get_field_value() - "
                         "A discrete field value was requested from a node that this field doesn't know. "
-                        "Perhaps a child node was not added to this field?");
-                return mChildNodes(aNodeIndex - mNumOriginalNodes)->interpolate_field_value(this);
+                        "Perhaps a child node was not added to this field?" );
+
+                return mChildNodes( aNodeIndex - mNumOriginalNodes )->interpolate_field_value( this );
             }
         }
 
         //--------------------------------------------------------------------------------------------------------------
 
-        const Matrix<DDRMat>& Field_Discrete_Integration::get_dfield_dadvs(
-                uint                  aNodeIndex,
-                const Matrix<DDRMat>& aCoordinates)
+        const Matrix< DDRMat >& Field_Discrete_Integration::get_dfield_dadvs(
+                uint                    aNodeIndex,
+                const Matrix< DDRMat >& aCoordinates )
         {
-            if (aNodeIndex < mNumOriginalNodes)
+            if ( aNodeIndex < mNumOriginalNodes )
             {
-                return this->get_dfield_dadvs(aNodeIndex);
+                return this->get_dfield_dadvs( aNodeIndex );
             }
             else
             {
-                MORIS_ASSERT((aNodeIndex - mNumOriginalNodes) < mChildNodes.size(),
+                MORIS_ASSERT( ( aNodeIndex - mNumOriginalNodes ) < mChildNodes.size(),
                         "A discrete field dfield_dadvs was requested from a node that this field doesn't know. "
-                        "Perhaps a child node was not added to this field?");
-                return mChildNodes(aNodeIndex - mNumOriginalNodes)->join_field_sensitivities(this);
+                        "Perhaps a child node was not added to this field?" );
+                return mChildNodes( aNodeIndex - mNumOriginalNodes )->join_field_sensitivities( this );
             }
         }
 
         //--------------------------------------------------------------------------------------------------------------
 
-        Matrix<DDSMat> Field_Discrete_Integration::get_determining_adv_ids(
-                uint                  aNodeIndex,
-                const Matrix<DDRMat>& aCoordinates)
+        Matrix< DDSMat > Field_Discrete_Integration::get_determining_adv_ids(
+                uint                    aNodeIndex,
+                const Matrix< DDRMat >& aCoordinates )
         {
-            if (aNodeIndex < mNumOriginalNodes)
+            if ( aNodeIndex < mNumOriginalNodes )
             {
-                return this->get_determining_adv_ids(aNodeIndex);
+                return this->get_determining_adv_ids( aNodeIndex );
             }
             else
             {
-                MORIS_ASSERT((aNodeIndex - mNumOriginalNodes) < mChildNodes.size(),
-                             "Determining ADV IDs were requested from a node that this discrete field doesn't know. "
-                             "Perhaps a child node was not added to this field?");
-                return mChildNodes(aNodeIndex - mNumOriginalNodes)->join_determining_adv_ids(this);
+                MORIS_ASSERT( ( aNodeIndex - mNumOriginalNodes ) < mChildNodes.size(),
+                        "Determining ADV IDs were requested from a node that this discrete field doesn't know. "
+                        "Perhaps a child node was not added to this field?" );
+                return mChildNodes( aNodeIndex - mNumOriginalNodes )->join_determining_adv_ids( this );
             }
         }
 
         //--------------------------------------------------------------------------------------------------------------
 
         void Field_Discrete_Integration::get_dfield_dcoordinates(
-                uint                  aNodeIndex,
-                const Matrix<DDRMat>& aCoordinates,
-                Matrix<DDRMat>&       aSensitivities)
+                uint                    aNodeIndex,
+                const Matrix< DDRMat >& aCoordinates,
+                Matrix< DDRMat >&       aSensitivities )
         {
-            MORIS_ASSERT(aNodeIndex >= mNumOriginalNodes,
-                        "Discrete field dfield_dcoordinates is only valid for intersection node indices.");
-            MORIS_ASSERT((aNodeIndex - mNumOriginalNodes) < mChildNodes.size(),
-                         "A discrete field dfield_dcoordinates was requested from a node that this field doesn't know. "
-                         "Perhaps a child node was not added to this field?");
-            mChildNodes(aNodeIndex - mNumOriginalNodes)->get_dfield_dcoordinates(this, aSensitivities);
+            MORIS_ASSERT( aNodeIndex >= mNumOriginalNodes,
+                    "Discrete field dfield_dcoordinates is only valid for intersection node indices." );
+            MORIS_ASSERT( ( aNodeIndex - mNumOriginalNodes ) < mChildNodes.size(),
+                    "A discrete field dfield_dcoordinates was requested from a node that this field doesn't know. "
+                    "Perhaps a child node was not added to this field?" );
+            mChildNodes( aNodeIndex - mNumOriginalNodes )->get_dfield_dcoordinates( this, aSensitivities );
         }
 
         //--------------------------------------------------------------------------------------------------------------
 
-        Matrix<DDSMat> Field_Discrete_Integration::get_determining_adv_ids(uint aNodeIndex)
+        Matrix< DDSMat > Field_Discrete_Integration::get_determining_adv_ids( uint aNodeIndex )
         {
-            return Field::get_determining_adv_ids(aNodeIndex, {{}});
+            return Field::get_determining_adv_ids( aNodeIndex, { {} } );
         }
 
         //--------------------------------------------------------------------------------------------------------------
 
-        void Field_Discrete_Integration::add_child_node(uint aNodeIndex, std::shared_ptr<Child_Node> aChildNode)
+        void Field_Discrete_Integration::add_child_node( uint aNodeIndex, std::shared_ptr< Child_Node > aChildNode )
         {
-            MORIS_ASSERT(aNodeIndex == mNumOriginalNodes + mChildNodes.size(),
-                    "Child nodes must be added to a level set field in order by node index.");
-            mChildNodes.push_back(aChildNode);
+            MORIS_ASSERT( aNodeIndex == mNumOriginalNodes + mChildNodes.size(),
+                    "Child nodes must be added to a level set field in order by node index." );
+            mChildNodes.push_back( aChildNode );
         }
 
         //--------------------------------------------------------------------------------------------------------------
 
         void Field_Discrete_Integration::reset_nodal_data()
         {
-            mChildNodes.resize(0);
+            mChildNodes.resize( 0 );
         }
 
         //--------------------------------------------------------------------------------------------------------------
 
-    }
-}
-
+    }    // namespace ge
+}    // namespace moris
