b4f306913823a7ccdab1fe506acfdc4ead30e67d
Adam Christopherson <adam.christopherson@colorado.edu>
2023-12-06 15:27:22 -0700

    Scaled field inherits directly from field

--- projects/GEN/src/ADV/fields/cl_GEN_Scaled_Field.hpp
+++ projects/GEN/src/ADV/fields/cl_GEN_Scaled_Field.hpp
@@ -15,8 +15,7 @@
 
 namespace moris::ge
 {
-    // TODO a scaled field should not have this inheritance to begin with
-    class Scaled_Field : public Field_Discrete_Interpolation
+    class Scaled_Field : public Field
     {
 
     private:
@@ -36,7 +35,7 @@ namespace moris::ge
         Scaled_Field(
                 std::shared_ptr< Field > aField,
                 ADV_ARG_TYPES )
-                : Field_Discrete_Interpolation( mtk::Mesh_Pair( nullptr, nullptr ), ADV_ARGS )
+                : Field( ADV_ARGS )
                 , mField( aField )
         {
             VARIABLE_CHECK( 1 );
@@ -50,9 +49,17 @@ namespace moris::ge
          * @param aCoordinates Node coordinates
          * @return Field value
          */
-        real get_base_field_value(
-                uint                  aNodeIndex,
-                const Matrix<DDRMat>& aCoordinates);
+        real get_field_value(
+                uint                    aNodeIndex,
+                const Matrix< DDRMat >& aCoordinates ) override;
+
+        /**
+         * Gets a field value of a derived node.
+         *
+         * @param aDerivedNode Derived node
+         * @return Field value
+         */
+        real get_field_value( Derived_Node* aDerivedNode ) override;
 
         /**
          * Given a node index, evaluates the sensitivity of the field with respect to all of the
@@ -62,9 +69,17 @@ namespace moris::ge
          * @param aCoordinates Node coordinates
          * @return Vector of sensitivities
          */
-        const Matrix<DDRMat>& get_base_dfield_dadvs(
-                uint                  aNodeIndex,
-                const Matrix<DDRMat>& aCoordinates);
+        const Matrix<DDRMat>& get_dfield_dadvs(
+                uint                    aNodeIndex,
+                const Matrix< DDRMat >& aCoordinates ) override;
+
+        /**
+         * Gets a vector of the field derivatives with respect to ADVs of a derived node.
+         *
+         * @param aDerivedNode Derived node
+         * @return d(field value)/d(ADV_j)
+         */
+        const Matrix< DDRMat >& get_dfield_dadvs( Derived_Node* aDerivedNode ) override;
 
         /**
          * Gets the IDs of ADVs which this field depends on for evaluations.
@@ -73,16 +88,44 @@ namespace moris::ge
          * @param aCoordinates Node coordinates
          * @return Determining ADV IDs at this node
          */
-        Matrix<DDSMat> get_base_determining_adv_ids(
-                uint                  aNodeIndex,
-                const Matrix<DDRMat>& aCoordinates);
+        Matrix<DDSMat> get_determining_adv_ids(
+                uint                    aNodeIndex,
+                const Matrix< DDRMat >& aCoordinates ) override;
+
+        /**
+         * Gets the IDs of ADVs that this field depends on for evaluations at a derived node.
+         *
+         * @param aDerivedNode Derived node
+         * @return Determining ADV IDs at this node
+         */
+        Matrix< DDSMat > get_determining_adv_ids( Derived_Node* aDerivedNode ) override;
+
+        /**
+         * Given a node index or coordinates, returns a vector of the field derivatives with respect to the nodal
+         * coordinates.
+         *
+         * @param aNodeIndex Node index
+         * @param aCoordinates Vector of coordinate values
+         * @param aSensitivities Sensitivities to be filled with d(field value)/d(coordinate_j)
+         */
+        virtual void get_dfield_dcoordinates(
+                uint                    aNodeIndex,
+                const Matrix< DDRMat >& aCoordinates,
+                Matrix< DDRMat >&       aSensitivities ) override;
 
         /**
          * Sets the dependencies of this field after they have been found by update_dependencies().
          *
          * @param aDependencyFields Fields that this field depends on.
          */
-        void set_dependencies( Cell< std::shared_ptr< Field > > aDependencyFields );
+        void set_dependencies( Cell< std::shared_ptr< Field > > aDependencyFields ) override;
+
+        /**
+         * Gets an MTK field, if this field needs to be remapped to a new mesh
+         *
+         * @return MTK field
+         */
+        std::shared_ptr< mtk::Field > get_mtk_field() override;
 
     };
 }
