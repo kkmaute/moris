07690d1a3c02ac55dc63b8399a72a1d1ed58bd81
Brendan Chong <brendan.chong@colorado.edu>
2024-04-30 10:32:49 -0600

    updated shape sensitivity example to reflect merge changes

--- projects/EXA/optimization/Shape_Sensitivity_Bspline/Shape_Sensitivity_Bspline_Surface_Mesh_2D.cpp
+++ projects/EXA/optimization/Shape_Sensitivity_Bspline/Shape_Sensitivity_Bspline_Surface_Mesh_2D.cpp
@@ -160,7 +160,7 @@ namespace moris
     //--------------------------------------------------------------------------------------------------------------
 
     void
-    HMRParameterList( Vector< Vector< ParameterList > >& tParameterlist )
+    HMRParameterList( Vector< Vector< Parameter_List > >& tParameterlist )
     {
         tParameterlist.resize( 1 );
         tParameterlist( 0 ).resize( 1 );
@@ -195,7 +195,7 @@ namespace moris
     //--------------------------------------------------------------------------------------------------------------
 
     void
-    XTKParameterList( Vector< Vector< ParameterList > >& tParameterlist )
+    XTKParameterList( Vector< Vector< Parameter_List > >& tParameterlist )
     {
         tParameterlist.resize( 1 );
         tParameterlist( 0 ).resize( 1 );
@@ -217,7 +217,7 @@ namespace moris
     //--------------------------------------------------------------------------------------------------------------
 
     void
-    GENParameterList( Vector< Vector< ParameterList > >& tParameterlist )
+    GENParameterList( Vector< Vector< Parameter_List > >& tParameterlist )
     {
         tParameterlist.resize( 3 );
         tParameterlist( 0 ).resize( 1 );
@@ -266,7 +266,7 @@ namespace moris
     //--------------------------------------------------------------------------------------------------------------
 
     void
-    FEMParameterList( Vector< Vector< ParameterList > >& tParameterList )
+    FEMParameterList( Vector< Vector< Parameter_List > >& tParameterList )
     {
         // create a cell of cell of parameter list for fem
         tParameterList.resize( 8 );
@@ -413,7 +413,7 @@ namespace moris
     //--------------------------------------------------------------------------------------------------------------
 
     void
-    SOLParameterList( Vector< Vector< ParameterList > >& tParameterlist )
+    SOLParameterList( Vector< Vector< Parameter_List > >& tParameterlist )
     {
         tParameterlist.resize( 8 );
         for ( uint Ik = 0; Ik < 8; Ik++ )
@@ -449,7 +449,7 @@ namespace moris
     //--------------------------------------------------------------------------------------------------------------
 
     void
-    MSIParameterList( Vector< Vector< ParameterList > >& tParameterlist )
+    MSIParameterList( Vector< Vector< Parameter_List > >& tParameterlist )
     {
         tParameterlist.resize( 1 );
         tParameterlist( 0 ).resize( 1 );
@@ -460,7 +460,7 @@ namespace moris
     //--------------------------------------------------------------------------------------------------------------
 
     void
-    VISParameterList( Vector< Vector< ParameterList > >& tParameterlist )
+    VISParameterList( Vector< Vector< Parameter_List > >& tParameterlist )
     {
         tParameterlist.resize( 1 );
         tParameterlist( 0 ).resize( 1 );
@@ -478,7 +478,7 @@ namespace moris
     //--------------------------------------------------------------------------------------------------------------
 
     void
-    OPTParameterList( Vector< Vector< ParameterList > >& tParameterlist )
+    OPTParameterList( Vector< Vector< Parameter_List > >& tParameterlist )
     {
         tParameterlist.resize( 3 );
         tParameterlist( 0 ).resize( 1 );
@@ -501,7 +501,7 @@ namespace moris
     //--------------------------------------------------------------------------------------------------------------
 
     void
-    MORISGENERALParameterList( Vector< Vector< ParameterList > >& tParameterlist )
+    MORISGENERALParameterList( Vector< Vector< Parameter_List > >& tParameterlist )
     {
     }
 
