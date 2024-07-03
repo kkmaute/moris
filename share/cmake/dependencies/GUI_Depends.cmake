9b6b59e7b60133b7119dbd37a2514924e9397ec2
kkmaute <maute@colorado.edu>
2024-07-03 11:45:10 -0600

    moved GUI to its own project

--- share/cmake/dependencies/GUI_Depends.cmake
+++ share/cmake/dependencies/GUI_Depends.cmake
@@ -16,7 +16,7 @@ endif()
 set(GUI_CONFIGURED_ONCE "YES")
 
 # Add GUI to the source directory list
-list(APPEND MORIS_SOURCE_DIRS ${PRM}/${GUI})
+list(APPEND MORIS_SOURCE_DIRS ${GUI})
 
 # Third party libraries needed by GUI library
 set(GUI_TPL_DEPENDENCIES
@@ -25,3 +25,4 @@ set(GUI_TPL_DEPENDENCIES
 
 # Moris packages needed by GUI
 include(${MORIS_DEPENDS_DIR}/ENM_Depends.cmake)
+include(${MORIS_DEPENDS_DIR}/FEM_Depends.cmake)
