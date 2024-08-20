0be6fd0c67053eb7b9d46d1bdb4b4856bc2e68f0
Kurt Maute <maute@colorado.edu>
2024-08-19 21:31:52 -0600

    fixed isse with checking equal on true using equal_to function

--- projects/LINALG/test/fn_isvector.cpp
+++ projects/LINALG/test/fn_isvector.cpp
@@ -9,7 +9,7 @@
  */
 
 #include <catch.hpp>
-#include "fn_equal_to.hpp" // ALG/src
+#include "fn_equal_to.hpp"    // ALG/src
 #include "cl_Matrix.hpp"
 #include "linalg_typedefs.hpp"
 #include "moris_typedefs.hpp"
@@ -17,22 +17,21 @@
 
 namespace moris
 {
-TEST_CASE( "moris::isvector", "[linalgebra],[isvector]" )
+    TEST_CASE( "moris::isvector", "[linalgebra],[isvector]" )
     {
-    Matrix< DDRMat > a( 3, 3 );
-    Matrix< DDRMat > b( 1, 3 );
-    Matrix< DDRMat > c( 1, 1 );
-    Matrix< DDRMat > d;
+        Matrix< DDRMat > a( 3, 3 );
+        Matrix< DDRMat > b( 1, 3 );
+        Matrix< DDRMat > c( 1, 1 );
+        Matrix< DDRMat > d;
 
-    bool tIsVector_1 = isvector( a );
-    bool tIsVector_2 = isvector( b );
-    bool tIsVector_3 = isvector( b );
-    bool tIsVector_4 = isvector( d );
+        bool tIsVector_1 = isvector( a );
+        bool tIsVector_2 = isvector( b );
+        bool tIsVector_3 = isvector( b );
+        bool tIsVector_4 = isvector( d );
 
-    CHECK( equal_to( tIsVector_1, false ) );
-    CHECK( equal_to( tIsVector_2, true ) );
-    CHECK( equal_to( tIsVector_3, true ) );
-    CHECK( equal_to( tIsVector_4, false ) );
+        REQUIRE( !tIsVector_1 );
+        REQUIRE( tIsVector_2 );
+        REQUIRE( tIsVector_3 );
+        REQUIRE( !tIsVector_4 );
     }
-}
-
+}    // namespace moris
