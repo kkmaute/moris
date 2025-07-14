1838462adbba2ca4eecb6493bf7c2ada53fd7d1e
Kurt Maute <maute@colorado.edu>
2025-07-14 10:14:54 -0600

    added check for proper matrix dimension

--- projects/LINALG/src/fn_join_horiz.hpp
+++ projects/LINALG/src/fn_join_horiz.hpp
@@ -24,26 +24,28 @@
 
 namespace moris
 {
-/**
- * @brief horizontal concatenation
- *
- * @param[in] aA The First Matrix
- * @param[in] aB The Second Matrix
- *
- * @return join the corresponding rows of the given matrices
- *
- * @note The given matrices must have the same number of rows
- */
+    /**
+     * @brief horizontal concatenation
+     *
+     * @param[in] aA The First Matrix
+     * @param[in] aB The Second Matrix
+     *
+     * @return join the corresponding rows of the given matrices
+     *
+     * @note The given matrices must have the same number of rows
+     */
     template< typename Matrix_Type >
     auto
-    join_horiz( Matrix< Matrix_Type > const & aA,
-           Matrix< Matrix_Type > const & aB )
-    -> decltype( join_horiz( aA.matrix_data(), aB.matrix_data() ) )
+    join_horiz(
+            Matrix< Matrix_Type > const &aA,
+            Matrix< Matrix_Type > const &aB )
+            -> decltype( join_horiz( aA.matrix_data(), aB.matrix_data() ) )
     {
+        MORIS_ASSERT( aA.n_rows() == 0 || aB.n_rows() == 0 || aA.n_rows() == aB.n_rows(),
+                "moris::join_horiz - The number of rows in the two matrices must be equal." );
 
         return join_horiz( aA.matrix_data(), aB.matrix_data() );
     }
-}
+}    // namespace moris
 
 #endif /* PROJECTS_LINALG_SRC_FN_JOIN_HORIZ_HPP_ */
-
