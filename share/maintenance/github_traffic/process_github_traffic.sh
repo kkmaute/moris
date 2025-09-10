01ffd8d38b9d75575f3140db0413a60b8a2ae310
Kurt Maute <maute@colorado.edu>
2025-09-09 18:54:25 -0600

    new github traffic files

--- share/maintenance/github_traffic/process_github_traffic.sh
+++ share/maintenance/github_traffic/process_github_traffic.sh
@@ -9,8 +9,8 @@ startmonth=05
 startyear=23
 
 endday=01
-endmonth=06
-endyear=24
+endmonth=08
+endyear=25
 
 list=`ls *-*-*` 
 
@@ -18,13 +18,29 @@ nclones=0
 nunique=0
 nsets=0
 
+mkdir tmp
+
+for set in $list;do
+    # Extract day, month, year from filename
+    day=$(echo "$set" | cut -d'-' -f1)
+    month=$(echo "$set" | cut -d'-' -f2)
+    year=$(echo "$set" | cut -d'-' -f3)
+
+    # Create sortable format: YYYY-MM-DD and store mapping
+    cp $set tmp/"$year-$month-$day" $file
+done
+
+cd tmp
+
+list=`ls * | sort`
+
 for set in $list;do
 
    n=`echo $set | awk -F '-' -v sm=$startmonth -v sd=$startday -v sy=$startyear -v em=$endmonth -v ed=$endday -v ey=$endyear '{ 
           n=0;
           shash=sd+12*sm+365*sy
           ehash=ed+12*em+365*ey
-          chash=$1+12*$2+365*$3
+          chash=$3+12*$2+365*$1
           if ( chash >= shash ) {
              if ( chash <= ehash ) { n=1 }
           };
@@ -43,8 +59,12 @@ for set in $list;do
 
    nunique=`expr $nunique + $uniques`
    
-   echo "$set $nsets  $nclones  $nunique"
+   echo "$set      $nsets       $nclones       $nunique"
    
    fi
    
 done   
+
+cd ..
+
+rm -r tmp
